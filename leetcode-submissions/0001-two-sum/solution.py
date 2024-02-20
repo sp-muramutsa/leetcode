1b11a24ad5d054ee47298d70ada08c665bffb002
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-02-20 21:15:15 +0000

    [LeetCode Sync] Sandrin - Two Sum - Runtime - 38 ms (49.48%), Memory - 12.6 MB (99.94%)

--- leetcode-submissions/0001-two-sum/solution.py
+++ leetcode-submissions/0001-two-sum/solution.py
@@ -5,13 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        length = len(nums)
-        i = 0
-        while i < length:
-            j = 0
-            while j < length:
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
-                j += 1
-            i += 1
+
+        previous = {}
+
+        for index, number in enumerate(nums):
+            difference = target - number
+            if difference in previous:
+                return [previous[difference], index]
+            previous[number] = index
         
+        return 
+
