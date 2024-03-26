0d7cdfea60c28ce0400adbdaba17db88f5e6d5e6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-03-26 21:31:04 +0000

    [LeetCode Sync] Sandrin - Maximum Number of Balloons - Runtime - 38 ms (5.85%), Memory - 16.6 MB (100.00%)

--- leetcode-submissions/1297-maximum-number-of-balloons/solution.py
+++ leetcode-submissions/1297-maximum-number-of-balloons/solution.py
@@ -1,30 +1,13 @@
+from collections import defaultdict
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         
-        balloon_count = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+        count = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
         for char in text:
-            if char in balloon_count: 
-                balloon_count[char] += 1
-            
-        occurences = list(balloon_count.values())
-
-        for number in occurences:
-            if number < 1:
-                return 0
+            if char in count:
+                count[char] += 1
         
-        for number in occurences[2:4]:
-            if number < 2:
-                return 0
-
-        ones = occurences[:2]
-        ones.append(occurences[4]) 
-        twos = occurences[2:4]
-
-
-        result_ones = min(ones)
-        result_twos = min(twos) // 2
-        result = min(result_ones, result_twos)
-    
-       
-        return result
+        return min(count['b'], count['a'], count['l']//2 , count['o']//2, count['n'])
+        
+         
         
