a76ef6522d48e3a4d22cf86d670b996af603d8c3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-03-28 00:58:49 +0000

    [LeetCode Sync] Sandrin - Valid Palindrome - Runtime - 39 ms (6.68%), Memory - 21.7 MB (19.96%)

--- leetcode-submissions/0125-valid-palindrome/solution.py
+++ leetcode-submissions/0125-valid-palindrome/solution.py
@@ -1,27 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        # upper to lower and remove non alphanums
-        s = s.lower()
-        length = len(s)
-        new_s = ""
+        # Convert to lowercase and remove non-alphanumeric characters
+        s = "".join(char.lower() for char in s if char.isalnum())
+        
+        return s == s[::-1]  # Special notation for reversing a string
 
-        for i in range(length):
-            if s[i].isalnum():
-                new_s += s[i]
+    
         
-        
-        length_s = len(new_s)
-        i = 0
-        j = length_s - 1
-        while i < length_s and j > 0:
-            if new_s[i] == new_s[j]:
-                pass
-            else:
-                return False
-            i += 1
-            j -= 1 
-        
-        return True
+
+
         
         
