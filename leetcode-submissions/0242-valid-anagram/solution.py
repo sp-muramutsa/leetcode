725b6db5fc8e876ae466d0f12c50a939811d0614
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-08 06:25:54 +0000

    [LeetCode Sync] Sandrin - Valid Anagram - Runtime - 43 ms (5.15%), Memory - 17.1 MB (100.00%)

--- leetcode-submissions/0242-valid-anagram/solution.py
+++ leetcode-submissions/0242-valid-anagram/solution.py
@@ -1,4 +1,11 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
+        
+    
+
+        
+        
+        
         
