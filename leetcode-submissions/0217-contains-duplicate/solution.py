9f15735dfe7fe040d2120afc691b56c7cff69c67
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-08 06:04:07 +0000

    [LeetCode Sync] Sandrin - Contains Duplicate - Runtime - 426 ms (5.36%), Memory - 31.9 MB (20.64%)

--- leetcode-submissions/0217-contains-duplicate/solution.py
+++ leetcode-submissions/0217-contains-duplicate/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-
         hashset = set()
 
         for number in nums:
-            if number not in hashset:
-                hashset.add(number)
-            else:
+            if number in hashset:
                 return True
+            else:
+                hashset.add(number)
         
         return False
 
