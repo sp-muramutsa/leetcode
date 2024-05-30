7f60172cedd237c7fead1666615833802cad6926
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-30 08:10:00 +0000

    [LeetCode Sync] Sandrin - Two Sum - Runtime - 76 ms (35.60%), Memory - 18.8 MB (50.51%)

--- leetcode-submissions/0001-two-sum/solution.py
+++ leetcode-submissions/0001-two-sum/solution.py
@@ -1,20 +1,32 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        Time: O(logn)
         """
-        
+
         hashmap = {}
-        length = len(nums)
 
         for index, number in enumerate(nums):
-            hashmap[number] = index
-        
-        for i in range(length):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]]
-        
+            hashmap[index] = number
+
+        hashmap = dict(sorted(hashmap.items(), key=lambda x:x[1]))  
+        hashmap_values = list(hashmap.values())
+        hashmap_keys = list(hashmap.keys())
+        print(hashmap)
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            print(l, r)
+            if hashmap_values[l] + hashmap_values[r] < target: 
+                l += 1
+
+            elif hashmap_values[l] + hashmap_values[r] > target:
+                r -= 1
             
+            else:
+                return [hashmap_keys[l], hashmap_keys[r]]
+
+
+
+        
