1dc5e7a2183c73e6b0a0ff0950fd4555b81a65c3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-30 07:10:10 +0000

    [LeetCode Sync] Sandrin - Move Zeroes - Runtime - 44 ms (7.64%), Memory - 45.7 MB (97.23%)

---
+++ leetcode-submissions/0283-move-zeroes/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int j = i + 1;
+                while(j < nums.length) {
+                    if (nums[j] != 0) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                    j++;
+                }
+            }
+        }
+    }
+}
