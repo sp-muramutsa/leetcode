4a87e69f304588d3949d990c082d9c05312f141e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-30 06:41:15 +0000

    [LeetCode Sync] Sandrin - Is Subsequence - Runtime - 35 ms (3.76%), Memory - 16.7 MB (100.00%)

--- leetcode-submissions/0392-is-subsequence/solution.py
+++ leetcode-submissions/0392-is-subsequence/solution.py
@@ -1,31 +1,30 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # fix a char in s
-        # iterate t till we find the character. 
-        # if we do. we fix the next character in s. and so on
-        # if we don't find the s character in all of t we return false
 
-        length_s = len(s)
-        length_t = len(t)
-       
-        s_pointer = 0
-        t_pointer = 0
-
-        while t_pointer < length_t and s_pointer < length_s:
-            checker = s_pointer
-            if s[s_pointer] == t[t_pointer]:
-                s_pointer += 1
-            else:
-                pass
-            t_pointer += 1
+        """
+        Time: O(n) where n in the length of t
+        Space: O(1)
+        Intuition:
+        Use two pointers to traverse both strings. The idea is to check if all characters of `s` appear in `t` in the same order.
+        - Initialize two pointers: `s_pointer` for string `s` and `t_pointer` for string `t`.
+        - Traverse `t` with `t_pointer` and whenever a character in `t` matches the current character in `s` (pointed to by `s_pointer`), move the `s_pointer` to the next character.
+        - If `s_pointer` reaches the end of `s`, it means all characters of `s` have been found in `t` in the correct order, so return True.
+        - If the loop finishes and `s_pointer` has not reached the end of `s`, return False.
         
-        return s_pointer == length_s
-
-        """ 
-        Time O(n + m)
-        Space O(1)
         """
 
+        s_pointer, t_pointer = 0, 0
+        s_length, t_length = len(s), len(t)
+
+        while s_pointer < s_length and t_pointer < t_length:
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+            t_pointer += 1  
+        
+        if s_pointer == s_length:
+            return True
+        else:
+            return False
     
 
 
