211d78d2c30655848a0bf9a989b4e7a3d05951c1
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-31 09:27:04 +0000

    [LeetCode Sync] Sandrin - Valid Parentheses - Runtime - 1 ms (99.47%), Memory - 41.5 MB (96.85%)

---
+++ leetcode-submissions/0020-valid-parentheses/solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> a = new Stack<Character>();
+
+        for(char c: chars) {
+            if (c == '(') {
+                a.push(')');
+            } else if (c == '{') {
+                a.push('}');
+            } else if (c == '[') {
+                a.push(']');
+            } else if (a.isEmpty() || a.pop() != c){
+                return false;
+            }
+        }
+        return a.isEmpty();
+    }
+
+    //     public boolean isValid(String s) {
+    //     char[] chars = s.toCharArray();
+    //     Stack<Integer> a = new Stack<Integer>();
+    //     Stack<Integer> b = new Stack<Integer>();
+    //     Stack<Integer> d = new Stack<Integer>();
+
+    //     for(char c: chars) {
+    //         if (c == '(') {
+    //             a.push(1);
+    //         } else if (c == ')') {
+    //             if(a.isEmpty()) { return false;}
+    //             a.pop();
+    //         } else if (c == '{') {
+    //             b.push(1);
+    //         } else if (c == '}') {
+    //             if(b.isEmpty()) { return false;}
+    //             b.pop();
+    //         } else if (c == '[') {
+    //             d.push(1);
+    //         } else if (c == ']') {
+    //             if(d.isEmpty()) { return false;}
+    //             d.pop();
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    //     return a.isEmpty() && b.isEmpty() && d.isEmpty();
+    // }
+}
