cc9f7a3d042b1bd48f23dc202f268a5f3357af3f
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-01 08:22:30 +0000

    [LeetCode Sync] Sandrin - Remove Element - Runtime - 35 ms (0.39%), Memory - 16.4 MB (100.00%)

--- leetcode-submissions/0027-remove-element/solution.py
+++ leetcode-submissions/0027-remove-element/solution.py
@@ -1,5 +1,35 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        nums[:] = [i for i in nums if i != val]
-        return len(nums)
-        
+
+        """
+        Two pointer solution
+        The tripping solution was returning the wrong k!!!!!
+        """
+
+        length = len(nums)
+        l, r = 0, length - 1
+        while l < r:
+            if nums[l] == val and nums[r] == val:
+                nums[r] = val
+                r -= 1
+ 
+            
+            elif nums[l] == val and nums[r] != val:
+                nums[l] = nums[r]
+                nums[r] = val
+                l += 1
+       
+            
+            elif nums[l] != val:
+                l += 1
+
+        x = 0
+        for j in range(length - 1, -1, -1):
+            if nums[j] == val:
+                x += 1
+                nums[j] = "_"
+            else:
+                break
+
+        k = length - x
+        return k
