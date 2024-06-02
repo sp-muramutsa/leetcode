d760a8731a17943e9ca46899d74db5bc08d248df
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-02 21:00:53 +0000

    [LeetCode Sync] Sandrin - Isomorphic Strings - Runtime - 31 ms (5.81%), Memory - 19 MB (5.49%)

--- leetcode-submissions/0205-isomorphic-strings/solution.py
+++ leetcode-submissions/0205-isomorphic-strings/solution.py
@@ -1,22 +1,19 @@
+from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
 
-        s_hashmap = {}
-        t_hashmap = {}
+        length_s =  len(s)
+        length_t =  len(t)
 
-        length_s = len(s)
-        length_t = len(t)
+        if length_s != length_t:
+            return False
 
+        s_hashmap = defaultdict(list)
         for i in range(length_s):
-            if s[i] not in s_hashmap:
-                s_hashmap[s[i]] = [i]
-            else:
-                s_hashmap[s[i]].append(i)
-
-        for j in range(length_t):
-            if t[j] not in t_hashmap:
-                t_hashmap[t[j]] = [j]
-            else:
-                t_hashmap[t[j]].append(j)
+            s_hashmap[s[i]].append(i)
         
-        return list(s_hashmap.values()) == list(t_hashmap.values())
+        t_hashmap = defaultdict(list)
+        for j in range(length_t):
+            t_hashmap[t[j]].append(j)
+        
+        return list(s_hashmap.values())== list(t_hashmap.values()) 
