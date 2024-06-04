18912cb4d21ad3081a288d9178194a7c643b94c4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-04 17:21:47 +0000

    [LeetCode Sync] Sandrin - First Letter to Appear Twice - Runtime - 0 ms (100.00%), Memory - 41.4 MB (92.07%)

---
+++ leetcode-submissions/2427-first-letter-to-appear-twice/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Set<Character> chars = new HashSet();
+        for (int i = 0; i < s.length(); i++) {
+            if (chars.contains(s.charAt(i))) {
+                return s.charAt(i);
+            }
+            chars.add(s.charAt(i));
+        } 
+        return ' ';
+    }
+}
