045e5d53b42254879393a1b107755a65b8f03094
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-05 17:54:53 +0000

    [LeetCode Sync] Sandrin - Count Number of Nice Subarrays - Runtime - 42 ms (17.43%), Memory - 56.2 MB (5.80%)

---
+++ leetcode-submissions/1370-count-number-of-nice-subarrays/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        /**
+            k odd numbers in a subarray
+        */
+
+        Map<Integer, Integer> freqs = new HashMap<>();
+        freqs.put(0, 1);
+        int ans = 0;
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curr += nums[i] % 2;
+            ans += freqs.getOrDefault(curr - k, 0);
+            freqs.put(curr, freqs.getOrDefault(curr, 0) + 1);
+        }
+        return ans;
+    }
+}
