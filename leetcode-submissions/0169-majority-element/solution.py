968ce3fde84f734ad13e01fe9fedea67c64b99a9
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-05 09:21:47 +0000

    [LeetCode Sync] Sandrin - Majority Element - Runtime - 174 ms (5.62%), Memory - 17.6 MB (100.00%)

--- leetcode-submissions/0169-majority-element/solution.py
+++ leetcode-submissions/0169-majority-element/solution.py
@@ -1,23 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        """"
-        Time O(n)
-        Space O(n)
         """
-        length = len(nums)
-        majority_length = length // 2
+        Time: O(n)
+        Space: O(1)
+        Intuition:
+        The algorithm uses the Boyer-Moore Voting Algorithm to find the majority element, which is an element that appears more than half the time in the list.
+        - Initialize a counter and a majority candidate.
+        - Iterate through the list:
+            - If the counter is zero, set the current element as the majority candidate.
+            - If the current element is the same as the majority candidate, increment the counter.
+            - If the current element is different, decrement the counter.
+        - The majority candidate at the end of the iteration is the majority element because it appears more than half the time in the list.
+        """
 
-        hashmap = {}
+        count, majority = 0, 0
 
         for number in nums:
-            if number not in hashmap:
-                hashmap[number] = 1
-            else:
-                hashmap[number] += 1
-        
-        for number, frequency in hashmap.items():
-            if frequency > majority_length:
-                return number
+            if count == 0:
+                majority = number
 
+            count += 1 if number == majority else -1
 
-        
+        return majority
+
