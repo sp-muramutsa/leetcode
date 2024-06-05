30c37a7a839c057ceade732078343c0ab2afc883
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-05 11:25:42 +0000

    [LeetCode Sync] Sandrin - Merge Strings Alternately - Runtime - 33 ms (84.81%), Memory - 16.4 MB (100.00%)

--- leetcode-submissions/1894-merge-strings-alternately/solution.py
+++ leetcode-submissions/1894-merge-strings-alternately/solution.py
@@ -1,25 +1,23 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        # Add letter from word one, add letter from word two. 
-        # Repeat until one exhaust
-        # In case one exhaust, add the remaining letters of the other
+        """
+        """
 
-        length1, length2 = len(word1), len(word2)
-        min_length = min(length1, length2)
+        n1 = len(word1)
+        n2 = len(word2)
+        n = min(n1, n2)
+        merged_string = ""
+        a, b = 0, 0
 
-        merged_word = ""
 
-        for i in range(min_length):
-            merged_word += word1[i] + word2[i]
+        while a < n:
+            merged_string += word1[a] + word2[a] 
+            a += 1
 
-        difference = length1 - length2
+        difference = abs(n1 - n2)
         if difference > 0:
-            merged_word += word1[-difference:]
-        elif difference < 0:
-            merged_word += word2[difference:]
-            
-        return merged_word
-        
+            merged_string += (word1[-difference:] if n1 > n2 else word2[-difference:])
+        return merged_string 
 
 
 
