6c0d36cee78ca09ca29fc1feb941121ac2db2c83
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-05 09:23:23 +0000

    [LeetCode Sync] Sandrin - Contains Duplicate II - Runtime - 495 ms (5.18%), Memory - 29.8 MB (99.19%)

--- leetcode-submissions/0219-contains-duplicate-ii/solution.py
+++ leetcode-submissions/0219-contains-duplicate-ii/solution.py
@@ -1,23 +1,45 @@
+from collections import Counter
+
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-
         """
-        Time O(n)
-        Space O(n)
+        Time: O(n)
+        Space: O(n)
+        Intuition:
+        1. Use a Counter to count occurrences of each number.
+        2. Identify numbers that have duplicates by checking counts greater than one.
+        3. Create a hashmap to store the indices of these duplicate numbers.
+        4. Check the differences between the indices of each duplicate number to see if any are within k distance.
         """
-  
-        nums_to_i = {} 
-        length = len(nums)
 
-        for i in range(length):
-            if nums[i] in nums_to_i:
-               if abs(i - nums_to_i[nums[i]]) <= k:
+        counter = Counter(nums)
+        n = len(nums)
+        duplicates = set()
+
+        # Find elements that appear more than once
+        for key, value in counter.items():
+            if value > 1:
+                duplicates.add(key)
+
+        # Store indices of duplicate elements in a hashmap
+        hashmap = {}
+        for i in range(n):
+            if nums[i] in duplicates:
+                if nums[i] not in hashmap:
+                    hashmap[nums[i]] = [i]
+                else:
+                    hashmap[nums[i]].append(i)
+
+        # Check if any two indices of the same element are within distance k
+        for indexes in hashmap.values():
+            m = len(indexes)
+            l, r = 0, 1
+            while r < m:
+                if indexes[r] - indexes[l] <= k:
                     return True
-            nums_to_i[nums[i]] = i
-      
-        return False
-        
-        
-       
+                l += 1
+                r += 1
 
-        
+        return False
+
