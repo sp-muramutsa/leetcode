1a38e51a5ab6d86f4f2cd3fab3d49418b968debf
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-08 22:42:41 +0000

    [LeetCode Sync] Sandrin - Linked List Cycle - Runtime - 0 ms (100.00%), Memory - 44.8 MB (27.75%)

---
+++ leetcode-submissions/0141-linked-list-cycle/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //are they individual numbers? because we can store in a set and if a value is already there return alse
+        //the fast and slow pointers... on the second round they will definitely meet
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
