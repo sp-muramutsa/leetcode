01316b03d36f8bb708a6b7f6efde092d09385088
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-10 06:24:32 +0000

    [LeetCode Sync] Sandrin - Palindrome Linked List - Runtime - 3 ms (99.72%), Memory - 58.7 MB (75.45%)

---
+++ leetcode-submissions/0234-palindrome-linked-list/solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        //get halfway
+        //reverse one side
+        //compare them
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //at this point slow is halfway
+
+        //reverse the second half
+        ListNode prev = null;
+
+        while(slow != null) {
+
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+
+        }
+
+        fast = head;
+        slow = prev;
+        while(slow != null && fast != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
