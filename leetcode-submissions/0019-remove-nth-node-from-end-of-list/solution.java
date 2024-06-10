c4cbeca38b47d07d7cfca67eacd846e04643201d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-10 01:38:29 +0000

    [LeetCode Sync] Sandrin - Remove Nth Node From End of List - Runtime - 0 ms (100.00%), Memory - 41.8 MB (59.57%)

--- leetcode-submissions/0019-remove-nth-node-from-end-of-list/solution.java
+++ leetcode-submissions/0019-remove-nth-node-from-end-of-list/solution.java
@@ -10,20 +10,22 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
         for (int i = 0; i < n; i++) {
             fast = fast.next;
         }
-        if (fast == null) {
-            return head.next;
-        }
+        // if (fast == null) {
+        //     return head.next;
+        // }
 
         while(fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }            
         slow.next = slow.next.next;
-        return head;
+        return dummy.next;
     }
 }
