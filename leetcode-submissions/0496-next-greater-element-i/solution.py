310306fcd2ea9610a7e6527d67cc34d14150d0c0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-24 08:07:39 +0000

    [LeetCode Sync] Sandrin - Next Greater Element I - Runtime - 40 ms (10.95%), Memory - 16.8 MB (100.00%)

--- leetcode-submissions/0496-next-greater-element-i/solution.py
+++ leetcode-submissions/0496-next-greater-element-i/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """ 
+        Time: O(length1 + length2) i.e. O(n)
+        Space: O(n)
+        Intuition: Iterate from left to right while stacking elements from nums2. Check and see if the current element is greater than what's on the top of the stack, we hash whatever is on the top with that number and keep popping while doing the same(this is where the sugar is. it means that number(from nums2) is the next greater element of the ones we are popping). After we are done iterating, we hash the remaining elements with -1(i.e. they have no next greater element).
+        The hardest part is over, now we go in the hashmap to look for the numbers in nums1 and their next elements.
         """
-        Time O(length_1 * length_2)
-        Space O(1)
-        """
-        length_1 = len(nums1)
-        length_2 = len(nums2)
+        length2 = len(nums2)
+        stack = []
+        hashmap = {}
 
-        for i in range(length_1):
-            index = nums2.index(nums1[i])
-            for j in range(index, length_2):
-                flag = 1 # to check swaps
-                if nums2[j] > nums1[i]:
-                    nums1[i] = nums2[j]
-                    flag *= 0 # show that swap was made
-                    break # leave the loop imediately since we only want the first greater element
-            if flag == 1: # check if a swap was made
-                nums1[i] = -1
-        
+        for i in range(length2):
+            while stack and nums2[i] > stack[-1]:
+                hashmap[stack.pop()] = nums2[i]
+            stack.append(nums2[i])
+
+        while stack:
+            hashmap[stack.pop()] = -1
+
+        length1 = len(nums1)
+
+        for i in range(length1):
+            nums1[i] = hashmap[nums1[i]]
+
         return nums1
 
-
-        
-        
-        
