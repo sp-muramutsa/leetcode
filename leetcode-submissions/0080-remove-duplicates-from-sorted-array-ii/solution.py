e394500c32d96b53e56c621f2194ccbb050c2f82
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-27 06:56:18 +0000

    [LeetCode Sync] Sandrin - Remove Duplicates from Sorted Array II - Runtime - 45 ms (99.80%), Memory - 16.6 MB (100.00%)

--- leetcode-submissions/0080-remove-duplicates-from-sorted-array-ii/solution.py
+++ leetcode-submissions/0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -1,18 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        """
-        Time: O()
-        Space: O(1)
-        Intuition: Start from the third element comparing each element with the second element to its left. If they are equal, that means we have more than three instances of the same integer: delete the one on the right. If not, then move by a step and continue the comparisons.
-        """
-        i, k = 2, len(nums)
-
+        i = 2
         while i < len(nums):
             if nums[i] == nums[i - 2]:
-                nums.pop(i)  # pop, remove, and del can all work here
-                k -= 1
+                nums.remove(nums[i])
             else:
                 i += 1
 
-        return k
+        return len(nums)
+        
 
+        
