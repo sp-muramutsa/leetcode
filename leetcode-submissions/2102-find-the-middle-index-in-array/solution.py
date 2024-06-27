8f1c91032980e0d3cc8ea7b6e40bc46a1ebfe416
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-27 06:44:08 +0000

    [LeetCode Sync] Sandrin - Find the Middle Index in Array - Runtime - 51 ms (5.30%), Memory - 16.5 MB (100.00%)

---
+++ leetcode-submissions/2102-find-the-middle-index-in-array/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        """
+        Time: O(n)
+        Space: O(n)
+        Intuition: Iterate while hashing each index with its right and left sums. Find an index whose sums are equal.
+        """
+
+        left_hash, right_hash = {}, {}
+        length = len(nums)
+        left_sum, right_sum = 0, 0
+
+        i = 0
+        j = length - 1
+        while i < length and j >= 0:
+
+            left_hash[i] = left_sum
+            left_sum += nums[i]
+
+            right_hash[j] = right_sum
+            right_sum += nums[j]
+
+            i += 1
+            j -= 1
+
+        for k in range(length):
+            if left_hash[k] == right_hash[k]:
+                return k
+
+        return -1
+
