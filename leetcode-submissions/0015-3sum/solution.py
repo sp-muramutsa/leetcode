5b1ddce9d12304c7891c56c6407e40e159751680
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-06-28 09:35:23 +0000

    [LeetCode Sync] Sandrin - 3Sum - Runtime - 1381 ms (11.98%), Memory - 20.3 MB (91.37%)

--- leetcode-submissions/0015-3sum/solution.py
+++ leetcode-submissions/0015-3sum/solution.py
@@ -1,30 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
         """
-        Time O(n^2)
-        Space O(n)
+        Time: O(n^2)
+        Space: O(n) - for the result set and input space if sorting in-place is not considered extra space.
+        Intuition:
+        1. Sort the array to use a two-pointer technique efficiently.
+        2. Iterate through the array, and for each element, use two pointers to find the remaining two elements that sum up to zero.
+        3. Skip duplicates to ensure all triplets are unique.
         """
 
-        nums.sort()
-        print(nums)
-        k = 0
-        length = len(nums)
-        threesum = []
+        nums.sort()  # Sort the input list to apply two-pointer technique
+        i = 0
+        n = len(nums)
+        result = set()  # Use a set to avoid duplicate triplets
 
-        while k < length:
-            i, j = k + 1, length - 1
-            while i < j:
-                if nums[i] + nums[j] + nums[k] == 0:
-                    threesum.append((nums[k], nums[i], nums[j]))
-                    i += 1
-                    j -= 1 
-                elif nums[i] + nums[j] + nums[k] > 0:
-                    j -= 1
-                else:
-                    i += 1           
-            k += 1
-        
-        return list(set(threesum))
-        
-        
+        while (
+            i < n and nums[i] <= 0
+        ):  # Since the array is sorted, no need to continue if the current number is greater than 0
+
+            # Use this loop to apply a two sum for numbers on the right of i (cfr Two Sum II)
+            l, r = i + 1, n - 1
+            while l < r:
+                threesum = nums[l] + nums[r] + nums[i]
+
+                if threesum == 0:
+                    result.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+
+                elif threesum < 0:
+                    l += 1
+
+                elif threesum > 0:
+                    r -= 1
+
+            i += 1
+
+        return result
+
