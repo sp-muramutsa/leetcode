a5a289f297d8de23102f107a265275f28a0bdb33
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-07-02 12:06:08 +0000

    [LeetCode Sync] Sandrin - Word Pattern - Runtime - 41 ms (0.08%), Memory - 16.5 MB (100.00%)

--- leetcode-submissions/0290-word-pattern/solution.py
+++ leetcode-submissions/0290-word-pattern/solution.py
@@ -1,41 +1,24 @@
+from collections import defaultdict
+
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
 
-        s_list = list(s.split(" "))
-        pattern_count = len(pattern)
-        s_count = s.count(" ") + 1
-        
-        
-        pattern_hashmap = {}
-        s_hashmap = {}
-
-        for i in range(pattern_count): 
-            if pattern[i] not in pattern_hashmap:
-                pattern_hashmap[pattern[i]] = [i]
-            else:
-                pattern_hashmap[pattern[i]].append(i)
-        
-        for j in range(s_count):
-            if s_list[j] not in s_hashmap:
-                s_hashmap[s_list[j]] = [j]
-            else:
-                s_hashmap[s_list[j]].append(j)
+        pattern_hashmap = defaultdict(list)
+        i = 0
+        length_pattern = len(pattern)
+        for i in range(length_pattern):
+            pattern_hashmap[pattern[i]].append(i)
        
-        
-        print(pattern_hashmap)
-        print(s_hashmap)
+        s_hashmap = defaultdict(list)
+        j = 0
+        s_list = s.split(" ")
+        length_s = len(s_list)
+        for j in range(length_s):
+            s_hashmap[s_list[j]].append(j)
         
         return list(pattern_hashmap.values()) == list(s_hashmap.values())
-        
-        
-
        
-
-        
-
-
-
-
         
 
         
+
