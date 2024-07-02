1fcf17d9739c012596357024db708645dd0a89a2
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-07-02 11:01:15 +0000

    [LeetCode Sync] Sandrin - Find All Numbers Disappeared in an Array - Runtime - 274 ms (5.13%), Memory - 24.7 MB (100.00%)

--- leetcode-submissions/0448-find-all-numbers-disappeared-in-an-array/solution.py
+++ leetcode-submissions/0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -1,15 +1,20 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-
         """
-        Time O(n)
-        Space O(n)
+        Time: O(n)
+        Space: O(1)
+        Intuition: We'll mark the presence of each number in the list by negating the value at the index corresponding to each number (adjusted for 0-based indexing). By iterating through the list, for each number 'number', we mark 'nums[abs(number) - 1]' as negative. After this marking step, any index that remains positive indicates that the number 'index + 1' is missing from the list. This allows us to find all missing numbers while only modifying the input list and using constant extra space.
         """
-
         n = len(nums)
-        nums = set(nums)
-        all_in_range = (i for i in range(1, n + 1))
 
-        return [x for x in all_in_range if x not in nums]
+        for number in nums:
+            number = abs(number)
+            nums[number - 1] = -abs(nums[number - 1])
 
-        
+        result = []
+        for index, num in enumerate(nums):
+            if num > 0:
+                result.append(index + 1)
+
+        return result
+
