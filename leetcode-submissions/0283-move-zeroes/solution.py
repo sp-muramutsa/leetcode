62e510a31e80d00999d342dc75b2bc76b8e49320
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-10-12 05:27:09 +0000

    [LeetCode Sync] Sandrin - Move Zeroes - Runtime - 116 ms (15.47%), Memory - 12.8 MB (100.00%)

--- leetcode-submissions/0283-move-zeroes/solution.py
+++ leetcode-submissions/0283-move-zeroes/solution.py
@@ -1,29 +1,28 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+class Solution(object):
+    def moveZeroes(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
-        Time: O(n) 
-        Space: O(n)
-        Intuition: Move left to right pushing non-zero elements to the start. Pushing zeros to the end wasn't quite working!
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        Intuition: Move non-zeros then move zeros after
         """
+
+        n = len(nums)
 
         i = 0
-        n = len(nums)
-        j = 1
-
-        if n <= 1:
-            return 
-
-
-        while j < n:
-            if nums[i] != 0:
+        for number in nums:
+            if number != 0:
+                nums[i] = number
                 i += 1
-            if nums[j] != 0 and nums[i] == 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-           
-            j += 1
         
+        j = n - i
+        for k in range(1, j+1):
+            nums[-k] = 0
 
         
-             
+        
+        
+
+
+
+
+
