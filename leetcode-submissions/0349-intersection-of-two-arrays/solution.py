eee14dc3070fead7a47f76ca65e6f95bf245e7e4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-10-13 20:38:07 +0000

    [LeetCode Sync] Sandrin - Intersection of Two Arrays - Runtime - 28 ms (8.90%), Memory - 11.7 MB (100.00%)

--- leetcode-submissions/0349-intersection-of-two-arrays/solution.py
+++ leetcode-submissions/0349-intersection-of-two-arrays/solution.py
@@ -1,4 +1,19 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1).intersection(set(nums2)))
-            
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        Intuition: Hash one list and iterate another one checking if its elements are in the hashmap
+        """
+        intersec = set()
+        length1 = len(nums1)
+        hashmap2 = {}
+        for index2, number2 in enumerate(nums2):
+            hashmap2[number2] = index2
+        
+
+        for number1 in nums1:
+            if number1 in hashmap2:
+                intersec.add(number1)
+        return intersec
