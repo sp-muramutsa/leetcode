6e0994cc42d4c862e79d3e010d980098b220dca3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-10-13 14:48:11 +0000

    [LeetCode Sync] Sandrin - Longest Common Prefix - Runtime - 18 ms (7.26%), Memory - 11.9 MB (100.00%)

--- leetcode-submissions/0014-longest-common-prefix/solution.py
+++ leetcode-submissions/0014-longest-common-prefix/solution.py
@@ -1,27 +1,16 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-
-        def prefix(word1 : str, word2: str):
-            length = min(len(word1), len(word2))
-            common_prefix = ""
-
-            for i in range(length):
-                if word1[i] == word2[i]:
-                    print("Yes")
-                    common_prefix = common_prefix + word1[i]
-                else:
-                    break
-                    
-            return common_prefix
-            
-
-        length = len(strs)
-        common_prefix = strs[0]
-        for i in range(1, length):
-            common_prefix = prefix(common_prefix, strs[i])
-        
-        return common_prefix
-
-
-        
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        Intuition: 
+        """
+        if strs == None or len(strs) == 0:
+            return ""
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != c:
+                    return strs[0][0:i]
+        return strs[0]
     
