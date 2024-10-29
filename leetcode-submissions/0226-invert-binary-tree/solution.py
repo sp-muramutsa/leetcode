8d9c449f1e9729ad2b8852eb0e4d95d29d5672a0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-10-29 00:26:22 +0000

    [LeetCode Sync] Sandrin - Invert Binary Tree - Runtime - 0 ms (100.00%), Memory - 16.6 MB (100.00%)

---
+++ leetcode-submissions/0226-invert-binary-tree/solution.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        # Iterative approach
+        if not root:
+            return None
+
+        queue = collections.deque([root])
+
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left:
+                queue.append(current.left)
+            
+            if current.right:
+                queue.append(current.right)
+            
+
+        return root
+
