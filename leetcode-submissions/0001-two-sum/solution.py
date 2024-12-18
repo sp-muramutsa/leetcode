82ac46153da5dcbdcee346234c3be810dc5d54bc
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-12-18 18:36:55 +0000

    [LeetCode Sync] Sandrin - Two Sum - Runtime - 0 ms (100.00%), Memory - 19.2 MB (5.75%)

--- leetcode-submissions/0001-two-sum/solution.py
+++ leetcode-submissions/0001-two-sum/solution.py
@@ -1,20 +1,21 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
         """
 
+        length = len(nums)
         hashmap = {}
-        n = len(nums)
+        
+        
+        for i in  range(length):
+            hashmap[target - nums[i]] = i 
+        print(hashmap)
+        
+        for key, value in enumerate(nums):
+            if value in hashmap and hashmap[value] != key:
+                return [hashmap[value], key]
 
-        for index, num in enumerate(nums):
-            hashmap[num] = index
 
-        for i in range(n):
-            diff = target - nums[i]
-            if diff in hashmap and i != hashmap[diff]:
-                return [i, hashmap[diff]]
 
-       
+
+        
