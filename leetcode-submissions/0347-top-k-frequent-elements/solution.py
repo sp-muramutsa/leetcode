b2cbd8d3199fc4301f608a55d8e4657bddae106c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-12-18 22:14:41 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 7 ms (84.12%), Memory - 14.4 MB (44.13%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,22 +1,22 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:      
-        # Map each element with it's frequency
-        element_frequency = {}
-        for element in nums:
-            if element not in element_frequency:
-                element_frequency[element] = 1
-                    
-            else:
-                element_frequency[element] += 1
-        
-        # sort dictionary based on frequency
-        element_frequency = dict(sorted(element_frequency.items(), key=lambda x:x[1], reverse=True))
-
-        # return k first ones
-        return list(element_frequency.keys())[:k]
-
-    
-
-
-        
-        
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if len(nums) == 1:
+            return [nums[0]]
+        if not nums:
+            return False
+        answ = []
+        hashmap = defaultdict(int)
+        for i in nums:
+            hashmap[i] += 1
+        hashmap = sorted(hashmap.items(), key= lambda x: x[1], reverse=True)
+        answ = []
+        for i in range(k):
+            answ.append(hashmap[i][0])
+        return answ
+       
+                
