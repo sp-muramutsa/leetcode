0b89778893ac13b10d4764ee109dfeb4ee1f2272
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-12-28 20:19:51 +0000

    [LeetCode Sync] Sandrin - Valid Parentheses - Runtime - 4 ms (8.78%), Memory - 17.8 MB (80.69%)

--- leetcode-submissions/0020-valid-parentheses/solution.py
+++ leetcode-submissions/0020-valid-parentheses/solution.py
@@ -1,29 +1,20 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        hashmap = {")" : "(", "}": "{", "]": "["}
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
         stack = []
-        n = len(s)
-        i = 0
-
-        while i < n:
-            # opening bracket: push it to the top of the stack
-            if s[i] in ["(", "{", "["]:
-                stack.append(s[i])
-            # closing bracket: check if the top of the stack is its opening if yes, pop. else, return false.
-            else:
-                if stack and hashmap[s[i]] == stack[-1]:
-                    stack.pop()
-                else:
+        for i in s:
+            if stack and i in hashmap:
+                if stack[-1] != hashmap[i]:
                     return False
-
-            i += 1
-           
-        return not stack
-
-
-
+                else:
+                    stack.pop()
+            else:
+                stack.append(i)
+        if not stack:
+            return True
+        else:
+            return False
