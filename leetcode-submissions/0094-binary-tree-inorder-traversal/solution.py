dd29fbb2ee205944662a2c35d13865cafe486f1c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-12-30 23:19:29 +0000

    [LeetCode Sync] Sandrin - Binary Tree Inorder Traversal - Runtime - 0 ms (100.00%), Memory - 17.7 MB (53.89%)

--- leetcode-submissions/0094-binary-tree-inorder-traversal/solution.py
+++ leetcode-submissions/0094-binary-tree-inorder-traversal/solution.py
@@ -6,21 +6,14 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        #so we need a node that points to the node we are at, and it starts at root.
-        node = root
-        #we use a stacl to store the nodes we have visited the same way the recursion works.
+        res = []
         stack = []
-        #the list for the nodes values.
-        result = []
-        while node or stack:
+        node = root
+        while stack or node:
             while node:
                 stack.append(node)
                 node = node.left
-            #this means we are at the leaf or node without left
-            #we set the node pointer to the previous node as it is Null at the moment
             node = stack.pop()
-            #we add the value to the result
-            result.append(node.val)
+            res.append(node.val)
             node = node.right
-        return result
-
+        return res
