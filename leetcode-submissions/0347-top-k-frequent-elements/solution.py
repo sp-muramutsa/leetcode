4f19b0d72916fa6c35fc1661f7a1b9359b926e69
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-01-27 21:35:45 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 7 ms (51.79%), Memory - 24.4 MB (7.81%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,18 +1,29 @@
+from collections import Counter
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
-        for i in nums:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        l = [[] for i in range(len(nums) +  1)]
-        for key, value in hashmap.items():
-            l[value].append(key)
-        ans = []
-        for i in range(len(l)-1, -1, -1):
-            for j in l[i]:
-                ans.append(j)
-                if len(ans) == k:
-                    break
-            if len(ans) == k:
-                break
-        return ans
+        """ """
+        # Step 1: Counter hashmap: O(n)
+        counter = Counter(nums)
+
+        # Step 2: Build buckets based on frequency i.e. map of: frequency -> [list of values appearing that many times]
+        n = len(nums)
+        buckets = [[] for _ in range(n)]
+
+        for value, frequency in counter.items():
+            buckets[frequency - 1].append(value)
+
+        # Step 3: Now we have buckets in a big bucket i.e. list of lists of the map. Iterate in it from back to front picking up the elements. Here the keys lies in the fact that we know the length of the original input list. And therefore the most frequent element can't have a frequency greater than that length. This effectively let's us have an O(n) runtime.
+        result = []
+        j = 0
+        for i in range(n - 1, -1, -1):
+            for number in buckets[i]:
+                if j < k:
+                    result.append(number)
+                    j += 1
+                else:
+                    return result
+
+        return result
 
