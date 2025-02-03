640acaf1b1ff8fa9e254a90a37481f8664c1dedd
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-02-03 21:03:57 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 5 ms (62.01%), Memory - 21.2 MB (61.57%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,29 +1,18 @@
 from collections import Counter
-
-
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """ """
-        # Step 1: Counter hashmap: O(n)
+        
+        # 1: Map
         counter = Counter(nums)
 
-        # Step 2: Build buckets based on frequency i.e. map of: frequency -> [list of values appearing that many times]
-        n = len(nums)
-        buckets = [[] for _ in range(n)]
-
+        heap = []
+        
         for value, frequency in counter.items():
-            buckets[frequency - 1].append(value)
+            heapq.heappush(heap, (frequency, value))
 
-        # Step 3: Now we have buckets in a big bucket i.e. list of lists of the map. Iterate in it from back to front picking up the elements. Here the keys lies in the fact that we know the length of the original input list. And therefore the most frequent element can't have a frequency greater than that length. This effectively let's us have an O(n) runtime.
-        result = []
-        j = 0
-        for i in range(n - 1, -1, -1):
-            for number in buckets[i]:
-                if j < k:
-                    result.append(number)
-                    j += 1
-                else:
-                    return result
-
-        return result
-
+            if len(heap) > k:
+                heapq.heappop(heap)
+               
+        
+        return [pair[1] for pair in heap]
