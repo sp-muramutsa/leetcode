a534ab545af077fc3e6fa1292784c72658df6228
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-02-05 15:47:20 +0000

    [LeetCode Sync] Sandrin - Product of Array Except Self - Runtime - 28 ms (39.10%), Memory - 26.2 MB (26.12%)

--- leetcode-submissions/0238-product-of-array-except-self/solution.py
+++ leetcode-submissions/0238-product-of-array-except-self/solution.py
@@ -1,26 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
-        """
-        Time O(n)
-        Space O(n)
-        """
-
-        l_mult, r_mult = 1, 1
-        length = len(nums)
-        left, right = [0] * length, [0] * length
-
-        for i in range(length):
-            j = -i- 1
-            left[i], right[j] = l_mult, r_mult
-            l_mult *= nums[i]
-            r_mult *= nums[j]
         
-        return [a * b for a, b in zip(left, right)]
+        left_multiplier, right_multiplier = 1, 1
 
+        n = len(nums)
         
+        # left
+        left = [0] * n
+        left[0] = 1
+        for i in range(1, n):
+            left[i] = nums[i-1] * left_multiplier
+            left_multiplier = left[i]
         
-
-
+        # right
+        right = [0] * n
+        right[-1] = 1
+        for i in range(n-2, -1, -1):
+            right[i] = nums[i+1] * right_multiplier
+            right_multiplier = right[i]
         
+        # result
+        for i in range(n):
+            nums[i] = left[i] * right[i]
         
+        return nums
