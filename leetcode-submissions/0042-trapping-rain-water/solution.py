67e6ba05d2664ea1d601928b56ff8de77b83af13
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-02-24 00:24:50 +0000

    [LeetCode Sync] Sandrin - Trapping Rain Water - Runtime - 7 ms (87.79%), Memory - 19.3 MB (38.19%)

--- leetcode-submissions/0042-trapping-rain-water/solution.py
+++ leetcode-submissions/0042-trapping-rain-water/solution.py
@@ -1,45 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """
-        Time: O(n)
-        Space: O(1)
-        
-        Intuition: The amount of water trapped at any given position depends on the minimum of the maximum heights 
-        to its left and right. Instead of using extra space to store these maximum heights, we can use two pointers 
-        to traverse the height array from both ends towards the center. By maintaining the maximum heights encountered 
-        so far from both the left and the right, we can directly compute the trapped water.
+        l, r = 0, len(height) - 1
 
-        1. Initialize two pointers, `l` and `r`, pointing to the beginning and end of the height array, respectively.
-        2. Initialize `max_left` and `max_right` to keep track of the maximum heights encountered so far from the left 
-           and right.
-        3. Traverse the array using the two pointers:
-           - If `max_left` is less than `max_right`, move the left pointer (`l`) one step to the right, update `max_left`, 
-             and calculate the water trapped at the new position.
-           - Otherwise, move the right pointer (`r`) one step to the left, update `max_right`, and calculate the water 
-             trapped at the new position.
-        4. Continue this process until the two pointers meet.
-        5. The total trapped water is the sum of the water trapped at each position.
-        """
-
-        length = len(height)
-        if length == 0:
-            return 0
-        
-        l, r = 0, length - 1
         max_left, max_right = height[l], height[r]
 
-        trapped = 0
+        water = 0
         while l < r:
-            if max_left < max_right: # We move the minimum one
+            if (
+                max_left < max_right
+            ):  # Move the minimum one: This mirrors the behavior of min(max_left, max_right) that the O(n) space solution was using.
                 l += 1
-                max_left = max(max_left, height[l]) 
-                trapped += max_left - height[l] # Don't need to check if it's negative
+                max_left = max(max_left, height[l])
+                water += (
+                    max_left - height[l]
+                )  # Pay attention how this can never be negative because we updated max_left first
+
             else:
                 r -= 1
                 max_right = max(max_right, height[r])
-                trapped += max_right - height[r] # Don't need to check if it's negative
-        
-        return trapped
+                water += max_right - height[r]  # Idem au max_left
 
-            
+        return water
 
