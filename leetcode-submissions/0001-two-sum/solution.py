9a2aba5fbe550c1e1656f04013500f7e73ed5423
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-03-20 06:32:01 +0000

    [LeetCode Sync] Sandrin - Two Sum - Runtime - 1 ms (58.51%), Memory - 19.1 MB (10.44%)

--- leetcode-submissions/0001-two-sum/solution.py
+++ leetcode-submissions/0001-two-sum/solution.py
@@ -1,19 +1,9 @@
-from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
-        for index, number in enumerate(nums):
-            hashmap[number] = index
-        
-        n = len(nums)
-        print(hashmap)
-
-        for i in range(n):
-            diff = target - nums[i]
-
-            if diff in hashmap and i != hashmap[diff]:
-                return [i, hashmap[diff]]
-                
-            
-
-        
+        for index, num in enumerate(nums):
+            digit = target - num
+            if digit in hashmap:
+                return [hashmap[digit], index]
+            hashmap[num] = index
+        return -1
