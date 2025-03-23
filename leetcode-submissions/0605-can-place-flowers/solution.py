9a3424e35c4a5953a02f237fe8eff7c71aafaf3c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-03-23 17:43:08 +0000

    [LeetCode Sync] Sandrin - Can Place Flowers - Runtime - 4 ms (86.85%), Memory - 18.1 MB (54.51%)

--- leetcode-submissions/0605-can-place-flowers/solution.py
+++ leetcode-submissions/0605-can-place-flowers/solution.py
@@ -1,26 +1,9 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        """
-        Time: O(n)
-        Space: O(n)
-        Intuition: Put two dummy zeroes at the start and the end of the array. Then iterate while checking if an index is viable for flower planting. Count the viable indexes and compare them to n. Remember to skip 2 places when you find a viable index.
-        """
-        length = len(flowerbed) + 1 
+        count = 0
         flowerbed = [0] + flowerbed + [0]
-        k = 0
-        i = 1
-        while i < length:
+        for i in range(1, len(flowerbed) - 1, 1):
             if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
-                k += 1
-                i += 1
-            i += 1
-        return k >= n
-
-
-
-
-       
-
-        
-
-        
+                count += 1
+                flowerbed[i] = 1
+        return n <= count
