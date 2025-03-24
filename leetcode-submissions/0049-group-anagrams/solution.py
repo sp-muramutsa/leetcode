8ef55859d99d2266c67efdd6af464337b066f98e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-03-24 16:37:13 +0000

    [LeetCode Sync] Sandrin - Group Anagrams - Runtime - 20 ms (25.83%), Memory - 22.8 MB (13.02%)

--- leetcode-submissions/0049-group-anagrams/solution.py
+++ leetcode-submissions/0049-group-anagrams/solution.py
@@ -2,6 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
         for i in strs:
-            sorted_word = sorted(i)
-            hashmap[tuple(sorted_word)].append(i)
+            pattern = [0] * 26
+            for j in i:
+                num = ord(j) - ord('a')
+                pattern[num] += 1
+            hashmap[tuple(pattern)].append(i)
         return list(hashmap.values())
