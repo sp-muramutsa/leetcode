0dcadeb476879a966efaba288e4e201efe815ba8
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-03-30 21:20:33 +0000

    [LeetCode Sync] Sandrin - Time Based Key-Value Store - Runtime - 209 ms (5.40%), Memory - 74.9 MB (42.54%)

--- leetcode-submissions/1023-time-based-key-value-store/solution.py
+++ leetcode-submissions/1023-time-based-key-value-store/solution.py
@@ -1,4 +1,3 @@
-from sortedcontainers import SortedDict
 class TimeMap:
 
     def __init__(self):
@@ -6,17 +5,30 @@ class TimeMap:
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.map:
-            self.map[key] = SortedDict({timestamp: value})
+            self.map[key] = [(timestamp, value)]
         else:
-            self.map[key][timestamp] = value
+            idx = self.bisect_right(self.map[key], timestamp)
+            self.map[key].insert(idx, (timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
         if not key in self.map:
             return ""
 
-        idx = self.map[key].bisect_right(timestamp)
+        idx = self.bisect_right(self.map[key], timestamp) - 1
 
-        return self.map[key].peekitem(idx - 1)[1] if idx > 0 else ""
+        return self.map[key][idx][1] if idx >= 0 else ""
+    
+    def bisect_right(self, arr, target):
+        l, r = 0, len(arr) 
+
+        while l < r:
+            mid = (l+r) // 2
+            if arr[mid][0] <= target:
+                l = mid + 1
+            else:
+                r = mid 
+        
+        return l 
 
 
 # Your TimeMap object will be instantiated and called as such:
