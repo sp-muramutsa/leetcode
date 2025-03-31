01db1fe60455594547304ee53de46265e8e15ede
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-03-31 17:55:56 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 10 ms (26.90%), Memory - 24.6 MB (7.81%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #we create a hashmap
         hashmap = {}
+        #we get the counts for number 0(n) Time 
         for i in nums:
             hashmap[i] = hashmap.get(i, 0) + 1
-        nums_list = [[] for i in range(len(nums) + 1)]
+        #create a list with index as number of occurences
+        res = [[] for i in range(len(nums)+1)]
         for key, value in hashmap.items():
-            nums_list[value].append(key)
-        res = []
-        for i in nums_list[::-1]:
+            res[value].append(key)
+        ans = []
+        for i in res[::-1]:
             for j in i:
-                res.append(j)
-                if len(res) == k:
-                    return res
-            
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
