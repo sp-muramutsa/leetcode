a3dcd26918ca68ab7d4f40084e5b758cc7f8879c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-04-01 13:45:37 +0000

    [LeetCode Sync] Sandrin - Valid Sudoku - Runtime - 6 ms (42.50%), Memory - 17.8 MB (69.42%)

--- leetcode-submissions/0036-valid-sudoku/solution.py
+++ leetcode-submissions/0036-valid-sudoku/solution.py
@@ -1,36 +1,31 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        for i in range(9):
-
-            # Horizontal
-            horizontal = set()
-            vertical = set()
-            for j in range(9):
-
-                if board[i][j] != ".":
-                    if board[i][j] in horizontal:
-                        return False
-                    horizontal.add(board[i][j])
-
-                # Vertical
-                if board[j][i] != ".":
-                    if board[j][i] in vertical:
-                        return False
-                    vertical.add(board[j][i])
-
-        # 3x3
-        for row in range(0, 9, 3):
-            for col in range(0, 9, 3):
-
-                box = set()
-                for a in range(row, row + 3):
-                    for b in range(col, col + 3):
-
-                        if board[a][b] != ".":
-                            if board[a][b] in box:
-
-                                return False
-                            box.add(board[a][b])
+        for row in range(9):
+            seen = set()
+            for column in range(9):
+                if board[row][column] == ".":
+                    continue
+                if board[row][column] in seen:
+                    return False
+                seen.add(board[row][column])
+        
+        for column in range(9):
+            seen = set()
+            for row in range(9):
+                if board[row][column] == ".":
+                    continue
+                if board[row][column] in seen:
+                    return False
+                seen.add(board[row][column])
+            
+        for box_row in range(0, 9, 3):
+            for box_column in range(0, 9, 3):
+                seen = set()
+                for i in range(box_row, box_row + 3):
+                    for j in range(box_column, box_column + 3):
+                        if board[i][j] == ".":
+                            continue
+                        if board[i][j] in seen:
+                            return False
+                        seen.add(board[i][j])
         return True
-
