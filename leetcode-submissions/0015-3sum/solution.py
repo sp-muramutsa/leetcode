e590387faab14df5fdcfc8ae23d47cccc28135f0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-04-01 17:21:39 +0000

    [LeetCode Sync] Sandrin - 3Sum - Runtime - 1507 ms (9.43%), Memory - 20.2 MB (96.67%)

--- leetcode-submissions/0015-3sum/solution.py
+++ leetcode-submissions/0015-3sum/solution.py
@@ -1,34 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        """
-        Time: O(n^2)
-        Space: O(n) - for the result set and input space if sorting in-place is not considered extra space.
-        Intuition:
-        1. Sort the array to use a two-pointer technique efficiently.
-        2. Iterate through the array, and for each element, use two pointers to find the remaining two elements that sum up to zero.
-        3. Skip duplicates to ensure all triplets are unique.
-        """
-
-        nums.sort()  # Sort the input list to apply two-pointer technique
-        i, n = 0, len(nums)
-        result = set()  # Use a set to avoid duplicate triplets
-
-        while i < n:
-            l, r = i + 1, n - 1
-
+        #using set to remove duplicates
+        nums.sort()
+        res = set()
+        for i in range(len(nums)):
+            l, r = i + 1, len(nums) - 1
             while l < r:
-                if nums[i] + nums[l] + nums[r] < 0:
+                ans = nums[i] + nums[l] + nums[r]
+                if ans < 0:
                     l += 1
-
-                elif nums[i] + nums[l] + nums[r] > 0:
+                elif ans > 0:
                     r -= 1
-
                 else:
-                    result.add((nums[i], nums[l], nums[r]))
+                    res.add((nums[i], nums[l], nums[r]))
                     l += 1
                     r -= 1
-
-            i += 1
-
-        return list(result)
-
+        return list(res)
