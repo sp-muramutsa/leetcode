f31d42a3b87c48680998a2dc72d6ce710f7d77d4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-04-01 17:48:08 +0000

    [LeetCode Sync] Sandrin - Container With Most Water - Runtime - 112 ms (16.13%), Memory - 28.5 MB (67.22%)

--- leetcode-submissions/0011-container-with-most-water/solution.py
+++ leetcode-submissions/0011-container-with-most-water/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)
-        l, r = 0, n - 1
-        area, max_area = 0, 0
-
+        #we first set the pointers
+        l, r = 0, len(height) - 1
+        area = 0
         while l < r:
-            w = r - l
-            h = min(height[l], height[r])
-            area = w * h
-            max_area = max(max_area, area)
-
-            if height[l] < height[r]:
+            width = r - l
+            curr_height = min(height[l], height[r])
+            area = max(area, (width * curr_height))
+            if height[l] > height[r]:
+                r -= 1
+            elif height[l] < height[r]:
                 l += 1
             else:
-                r -= 1
-
-        return max_area
-
+                l += 1
+        return area
