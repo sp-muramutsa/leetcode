927d2bcb7579c9f0de7bea4d181e497956c5fb51
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-04-05 21:04:16 +0000

    [LeetCode Sync] Sandrin - Longest Substring Without Repeating Characters - Runtime - 16 ms (67.65%), Memory - 18 MB (34.08%)

--- leetcode-submissions/0003-longest-substring-without-repeating-characters/solution.py
+++ leetcode-submissions/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,30 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        """
-        Time: O(n)
-        Space: O(n)
-        Intuition: Slide a window through the string while enlarging it by unique characters. When you meet a character you already added, start afresh with your set. Update the longest substring as you go.
-        """
-
-        length = len(s)
-
-        if length <= 1:
-            return length
-
-        l, r = 0, 0
-        seen = set()
-        longest = 0
-        count = 0
-
-        while r < length:
-            if s[r] not in seen:
-                seen.add(s[r])
-                r += 1
-            else:
+        if not s:
+            return 0
+        
+        hashset = set()
+        answer = 0
+        l = 0
+        
+        for r in range(len(s)):
+            while s[r] in hashset:
+                hashset.remove(s[l])
                 l += 1
-                r = l
-                seen = set()
-            longest = max(longest, len(seen))
-
-        return longest
-
+            
+            hashset.add(s[r])
+            answer = max(answer, len(hashset))
+        
+        return answer
