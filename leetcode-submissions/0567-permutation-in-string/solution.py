099aea48bfde9b688aaffb8cd3d8e39867521935
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-04-06 04:00:43 +0000

    [LeetCode Sync] Sandrin - Permutation in String - Runtime - 1085 ms (17.25%), Memory - 17.8 MB (62.84%)

--- leetcode-submissions/0567-permutation-in-string/solution.py
+++ leetcode-submissions/0567-permutation-in-string/solution.py
@@ -1,28 +1,14 @@
-from collections import Counter
-
-
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        """
-        Time: O(n)
-        Space: O(n)
-        Intuition: See comments.
-        """
-
-        # Make permutations using a hashtable. Pretty smart.
-
-        hashtable = Counter(s1)
-        length1, length2 = len(s1), len(s2)
-        l, r = (
-            0,
-            length1,
-        )  # Move a window of size length1 accross s2 and check if the current substring in the window is a permutation of s1
-
-        while r <= length2:
-            if Counter(s2[l:r]) == hashtable:
+        hashmap = {}
+        for i in s1:
+            hashmap[i] = hashmap.get(i, 0) + 1
+        l, r = 0, len(s1) - 1
+        while r < len(s2):
+            count = Counter(s2[l:r+1])
+            if count == hashmap:
                 return True
-            r += 1
-            l += 1
-
+            else:
+                r += 1
+                l += 1
         return False
-
