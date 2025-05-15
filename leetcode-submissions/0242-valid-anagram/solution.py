175b5edcfe4b27f05860fa2b49d1d397c0fb2f8d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-15 00:51:57 +0000

    [LeetCode Sync] Sandrin - Valid Anagram - Runtime - 7 ms (89.21%), Memory - 17.8 MB (63.93%)

--- leetcode-submissions/0242-valid-anagram/solution.py
+++ leetcode-submissions/0242-valid-anagram/solution.py
@@ -1,15 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        counter1 = Counter(s)
+        counter2 = Counter(t)
+
+        if counter1 != counter2:
             return False
-        hash_s = {}
-        hash_t = {}
-        for i in range(len(s)):
-            hash_s[s[i]] = hash_s.get(s[i], 0) + 1
-        for j in range(len(t)):
-            hash_t[t[j]] = hash_t.get(t[j], 0) + 1
-        for item in hash_s:
-            if hash_s[item] != hash_t.get(item, 0):
-                return False
+
         return True
-        
