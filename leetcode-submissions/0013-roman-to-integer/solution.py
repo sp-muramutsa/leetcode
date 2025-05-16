ff57e28da304f0cb7a926c5138ea8432e12cd527
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-16 04:08:06 +0000

    [LeetCode Sync] Sandrin - Roman to Integer - Runtime - 3 ms (79.38%), Memory - 17.6 MB (90.06%)

--- leetcode-submissions/0013-roman-to-integer/solution.py
+++ leetcode-submissions/0013-roman-to-integer/solution.py
@@ -1,35 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Symbol       Value
-        #   I             1
-        #   V             5
-        #   X             10
-        #   L             50
-        #   C             100
-        #   D             500
-        #   M             1000
-        res = 0
         hashmap = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+            "I" :      1,
+            "V" :       5,
+            "X" :       10,
+            "L" :       50,
+            "C" :       100,
+            "D" :       500,
+            "M" :       1000
         }
+        tot = 0
         i = 0
         while i < len(s):
-            if i < len(s) - 1:
-                if hashmap[s[i]] < hashmap[s[i+1]]:
-                    val = hashmap[s[i+1]] - hashmap[s[i]]
-                    res += val
-                    i += 2
-                else:
-                    res += hashmap[s[i]]
-                    i += 1
+            if i <= (len(s) - 2) and hashmap[s[i]] < hashmap[s[i+1]]:
+                tot += hashmap[s[i+1]] - hashmap[s[i]]
+                i += 2
             else:
-                res += hashmap[s[i]]
+                tot += hashmap[s[i]]
                 i += 1
-        return res
-
+        return tot
