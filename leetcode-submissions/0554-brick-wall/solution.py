43238725421f6f79d63fff5cd286bd2b2bdd3fff
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-16 02:43:41 +0000

    [LeetCode Sync] Sandrin - Brick Wall - Runtime - 1 ms (97.37%), Memory - 21.6 MB (87.13%)

--- leetcode-submissions/0554-brick-wall/solution.py
+++ leetcode-submissions/0554-brick-wall/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        hashmap = {}
+        #We create a hashmap and add one element just in case the bricks are of sam size and fill both ends, well the idea bhind is that we will create a hashmap and keep the key as the position and value as the number of slices at that position, later we will find the number of cut bricks by len(wall) - slices.
+        hashmap = {0 : 0}
         for i in wall:
-            count = 0
+            position = 0
             for j in i[:-1]:
-                count += j
-                hashmap[count] = hashmap.get(count, 0) + 1
-        val = 0
-        for value in hashmap.values():
-            val = max(val,value)
-        return (len(wall) - val)
+                position += j
+                hashmap[position] = hashmap.get(position, 0) + 1
+        slices = max(hashmap.values())
+        return len(wall) - slices
+
