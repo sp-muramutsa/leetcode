c525c9c477c81a5d116eb45f00a3558ba89bd51d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-20 17:25:45 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 11 ms (24.10%), Memory - 24.6 MB (7.81%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,22 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #we create a hashmap that counts the number of times a number repeats itself
-        #we create another hashmap that keeps a list of times numbers were repeated
-        #we sort the hashmap 
-        #we return the values
-
-        hashmap = {}
-        for i in nums:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        l = [[] for i in range(len(nums)+1)]
-        for i, n in hashmap.items():
-            l[n].append(i)
+        counter = Counter(nums)
+        bucket = [[] for i in range(len(nums)+1)]
+        for key, items in counter.items():
+            bucket[items].append(key)
+        bucket = bucket[::-1]
         res = []
-        for i in l[::-1]:
-            for j in i:
-                if len(res) < k:
+        for i in bucket:
+            if len(res) == k:
+                return res
+            else:
+                for j in i:
                     res.append(j)
-                else:
-                    break
-        return res
-        
