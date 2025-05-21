c39bbf9cb32df81be2375d66c48de5daaf5e47d4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-21 22:03:22 +0000

    [LeetCode Sync] Sandrin - Search a 2D Matrix - Runtime - 0 ms (100.00%), Memory - 18.1 MB (92.74%)

--- leetcode-submissions/0074-search-a-2d-matrix/solution.py
+++ leetcode-submissions/0074-search-a-2d-matrix/solution.py
@@ -1,39 +1,13 @@
 class Solution:
-    # O(log(m * n))
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        n = len(matrix)
-        U, D = 0, n - 1
-
-        # Up and Down
-        while U <= D:
-            MID = U + (D - U) // 2
-            if self.binary_search(MID, matrix, target):
-                return True
-
-            if matrix[MID][-1] < target:
-                U = MID + 1
-
-            elif matrix[MID][-1] > target:
-                D = MID - 1
-
+        for i in range(len(matrix)):
+            l, r = 0, len(matrix[i]) - 1
+            while l <= r:
+                mid = (l+r)//2
+                if matrix[i][mid] == target:
+                    return True
+                elif matrix[i][mid] > target:
+                    r = mid - 1
+                elif matrix[i][mid] < target:
+                    l = mid + 1
         return False
-
-    # Right to Left in a Row
-    def binary_search(self, MID, matrix, target):
-        print(matrix[MID])
-        l, r = 0, len(matrix[MID]) - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-
-            if matrix[MID][mid] == target:
-                return True
-
-            elif matrix[MID][mid] > target:
-                r = mid - 1
-
-            elif matrix[MID][mid] < target:
-                l = mid + 1
-
-        return False
-
