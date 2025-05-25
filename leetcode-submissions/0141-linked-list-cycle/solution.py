799b605e3589a6bd1aa49ae75c1e756d0036a318
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-25 00:10:27 +0000

    [LeetCode Sync] Sandrin - Linked List Cycle - Runtime - 50 ms (46.28%), Memory - 19.9 MB (29.75%)

--- leetcode-submissions/0141-linked-list-cycle/solution.py
+++ leetcode-submissions/0141-linked-list-cycle/solution.py
@@ -4,16 +4,20 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        #Better way to perform this problem for O(1) space is using slow and fast pointers, once the fast pointer catches up with the slow then that means there is a cycle in our linked list.
-        if head is None:
-            return head
-        slow = head
-        fast = head.next
-        while fast and fast.next:
-            if fast == slow:
-                return True
-            fast = fast.next.next
+
+        if not head or not head.next:
+            return False
+
+        slow, fast = head, head
+
+        while fast and fast.next is not None:
             slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
         return False
+
