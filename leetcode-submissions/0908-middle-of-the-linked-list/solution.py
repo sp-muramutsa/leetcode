8fde2f67b1bbfc0178bc2e7b66acbb69ca0ac90e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-25 03:33:14 +0000

    [LeetCode Sync] Sandrin - Middle of the Linked List - Runtime - 0 ms (100.00%), Memory - 17.7 MB (48.03%)

--- leetcode-submissions/0908-middle-of-the-linked-list/solution.py
+++ leetcode-submissions/0908-middle-of-the-linked-list/solution.py
@@ -5,10 +5,12 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #we set the fast and slow pointer and move them accordingly, the slowe pointer will land in the middle of the linked list. O(n) time and O(1) space
-        slow = head
-        fast = head
+
+        fast, slow = head, head
+
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
+
         return slow
+
