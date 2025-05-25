e75084a1655083ef9d36776a92952f13984c864d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-25 21:26:10 +0000

    [LeetCode Sync] Sandrin - Remove Nth Node From End of List - Runtime - 0 ms (100.00%), Memory - 17.8 MB (34.68%)

--- leetcode-submissions/0019-remove-nth-node-from-end-of-list/solution.py
+++ leetcode-submissions/0019-remove-nth-node-from-end-of-list/solution.py
@@ -5,21 +5,21 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        #we first get the lenght of the linked list
-        curr = head
-        count = 0
+        if head is None:
+            return 
+        i = 0
+        dummy = head
+        curr = dummy
         while curr:
             curr = curr.next
-            count += 1
-        #we get the position of the node
-        position = count - n
-        #we get the node before the node to be removed
-        first = head
+            i += 1
+        if i == 1:
+            return 
+        position = i - n
+        pointer2 = dummy
         for i in range(position-1):
-            first = first.next
-        #we check the Edge case
+            pointer2 = pointer2.next
         if position == 0:
-            return first.next
-        #we skip the node to be removed
-        first.next = first.next.next
-        return head
+            return pointer2.next
+        pointer2.next = pointer2.next.next
+        return dummy
