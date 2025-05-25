27087040f8a318ea51ed19159ebff14c7f3671b3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-25 19:01:42 +0000

    [LeetCode Sync] Sandrin - Reorder List - Runtime - 0 ms (100.00%), Memory - 23.3 MB (81.49%)

--- leetcode-submissions/0143-reorder-list/solution.py
+++ leetcode-submissions/0143-reorder-list/solution.py
@@ -8,28 +8,30 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #We find the mid node using slow and fast pointer
-        slow = head
-        fast = head.next
+
+        # 1. Find the middle element
+        slow = fast = head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-        second = slow.next
-        #We detach the first and second node
-        slow.next = None
-        #we reverse the second node
-        prev = None
-        curr = second
-        while curr:
-            nxt = curr.next
+            slow = slow.next
+
+        # 2. Reverse the second part
+        prev, curr = None, slow
+
+        while curr is not None:
+            temp_next = curr.next
             curr.next = prev
-            prev = curr
-            curr = nxt
-        second = prev
-        #we reorder the linked list
-        while second:
-            temp1, temp2 = head.next, second.next
-            head.next = second
-            second.next = temp1
-            head = temp1
-            second = temp2
+            prev, curr = curr, temp_next
+
+        # 3. Merge the two lists
+        p1, p2 = head, prev
+
+        while p2.next:
+            temp_next_1 = p1.next
+            p1.next = p2
+            p1 = temp_next_1
+
+            temp_next_2 = p2.next
+            p2.next = temp_next_1
+            p2 = temp_next_2
+
