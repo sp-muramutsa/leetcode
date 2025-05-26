f4733481e74f5e31bd3330d30b903ba323b7b9d3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-26 23:28:20 +0000

    [LeetCode Sync] Sandrin - Linked List Cycle - Runtime - 45 ms (75.86%), Memory - 20.3 MB (5.65%)

--- leetcode-submissions/0141-linked-list-cycle/solution.py
+++ leetcode-submissions/0141-linked-list-cycle/solution.py
@@ -4,20 +4,12 @@
 #         self.val = x
 #         self.next = None
 
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-
-        if not head or not head.next:
-            return False
-
-        slow, fast = head, head
-
-        while fast and fast.next is not None:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+        visited = set()
+        while head:
+            if head in visited:
                 return True
-
+            visited.add(head)
+            head = head.next
         return False
-
