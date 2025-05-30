3d492746d0d3460702d03c21f7054e7e951efeeb
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-05-30 19:52:41 +0000

    [LeetCode Sync] Sandrin - Merge k Sorted Lists - Runtime - 15 ms (37.78%), Memory - 20.2 MB (70.48%)

--- leetcode-submissions/0023-merge-k-sorted-lists/solution.py
+++ leetcode-submissions/0023-merge-k-sorted-lists/solution.py
@@ -4,24 +4,40 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        ListNode.__lt__ = lambda self, other: self.val < other.val
 
-        minHeap = []
+    ListNode.__lt__ = lambda node, other: node.val < other.val
 
-        for l in lists:
-            if l:
-                heapq.heappush(minHeap, l)
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        dummy = ListNode()
-        curr = dummy
+        dummy = ListNode(0)
+        tail = dummy
 
-        while minHeap:
-            node = heapq.heappop(minHeap)
-            curr.next = node
-            if node.next:
-                heapq.heappush(minHeap, node.next)
-            curr = curr.next
+        while list1 and list2:
+            if list1 < list2:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
 
+        tail.next = list1 if list1 else list2
         return dummy.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if not lists:
+            return
+
+        while len(lists) > 1:
+
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(self.mergeTwoLists(list1, list2))
+
+            lists = mergedLists
+
+        return lists[0]
 
