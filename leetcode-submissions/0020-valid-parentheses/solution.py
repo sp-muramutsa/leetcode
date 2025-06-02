073f2a19624b5708b612097010fa340ce1814b7e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-06-02 01:25:41 +0000

    [LeetCode Sync] Sandrin - Valid Parentheses - Runtime - 3 ms (22.21%), Memory - 17.8 MB (50.72%)

--- leetcode-submissions/0020-valid-parentheses/solution.py
+++ leetcode-submissions/0020-valid-parentheses/solution.py
@@ -1,23 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        closing_brackets = {
-            ")":"(",
-            "}":"{",
-            "]":"["
+        hashmap = {
+            ")": "(",
+            "]": "[",
+            "}": "{" 
         }
         stack = []
         for i in s:
-            if i in closing_brackets:
-                if stack:
-                    bracket = stack.pop()
-                    if bracket != closing_brackets[i]:
+            if i in hashmap:
+                if len(stack) != 0:
+                    new = stack.pop()
+                    if new != hashmap[i]:
                         return False
                 else:
                     return False
             else:
                 stack.append(i)
-        if not stack:
+        if len(stack) == 0:
             return True
-        else:
-            return False
-
+        return False
