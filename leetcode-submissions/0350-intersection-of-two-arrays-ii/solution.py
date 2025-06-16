d963e40c1fac81eb606592308877951ba4532a5c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-06-16 04:57:25 +0000

    [LeetCode Sync] Sandrin - Intersection of Two Arrays II - Runtime - 4 ms (26.77%), Memory - 18.1 MB (7.46%)

--- leetcode-submissions/0350-intersection-of-two-arrays-ii/solution.py
+++ leetcode-submissions/0350-intersection-of-two-arrays-ii/solution.py
@@ -1,30 +1,11 @@
-from collections import Counter
-class Solution(object):
-    def intersect(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        Intuition: Sort the lists. Iterate through both lists. If we find a common element we append it to our result. Else we try to shift the pointers for small elements to get compared to high ones.
-        """
-        nums1.sort()
-        nums2.sort()
-        length1, length2 = len(nums1), len(nums2)
-        i = j = 0
-        result = []
-
-        while i < length1 and j < length2:
-            if nums1[i] < nums2[j]:
-                i += 1
-            elif nums1[i] > nums2[j]:
-                j += 1
-            else:
-                result.append(nums1[i]) # or result.append(nums2[j])
-                i += 1
-                j += 1
-        return result
-
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count1 = Counter(nums1)
+        res = []
         
-    
+        for i in nums2:
+            if i in count1 and count1[i] > 0:
+                count1[i] -= 1
+                res.append(i)
 
-
+        return res
