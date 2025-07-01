22f38da1671a7236cb1fc09b5da33f93f6e8e74e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-01 17:22:25 +0000

    [LeetCode Sync] Sandrin - Reverse Linked List - Runtime - 0 ms (100.00%), Memory - 18.6 MB (53.57%)

--- leetcode-submissions/0206-reverse-linked-list/solution.py
+++ leetcode-submissions/0206-reverse-linked-list/solution.py
@@ -5,10 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return 
+
         prev, curr = None, head
-        while curr:
-            temp = curr.next
+
+        while curr is not None:
+            temp_next = curr.next
             curr.next = prev
             prev = curr
-            curr = temp
+            curr = temp_next
+          
+        
         return prev
