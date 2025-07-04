f38760c7646437a6ce37adf0e9b00e6534d86d68
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-04 14:34:36 +0000

    [LeetCode Sync] Sandrin - Same Tree - Runtime - 0 ms (100.00%), Memory - 18 MB (20.70%)

--- leetcode-submissions/0100-same-tree/solution.py
+++ leetcode-submissions/0100-same-tree/solution.py
@@ -6,18 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
-        # If both nodes are None, they are identical
+        # Edge cases where one tree can be empty, return False 
+        if (p is None and q is not None) or (p is not None and q is None):
+            return False
         if p is None and q is None:
             return True
-        
-        # If only one is None, they are not identical
-        if p is None or q is None:
+        # If the the values are differrent we return False 
+        if p.val != q.val:
             return False
-        
-        # Recursively check if the left and the right sides of both trees are identical
-        if p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
-            return False # If their values are not identical why even bother.
-
+        # Traverse the left
+        left = self.isSameTree(p.left, q.left)
+        # Traverse the right
+        right = self.isSameTree(p.right, q.right)
+        return (left and right)
