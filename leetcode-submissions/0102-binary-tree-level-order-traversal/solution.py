ee57978110e38f154341c57ed50ab42f1fcebcb7
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-04 17:02:31 +0000

    [LeetCode Sync] Sandrin - Binary Tree Level Order Traversal - Runtime - 1 ms (29.69%), Memory - 18.3 MB (86.90%)

--- leetcode-submissions/0102-binary-tree-level-order-traversal/solution.py
+++ leetcode-submissions/0102-binary-tree-level-order-traversal/solution.py
@@ -4,28 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
-        if not root:
+        # Using BFS to solve this problem
+        # Edge case: the root node might be empty, we return []
+        if root is None:
             return []
-
-        traversal = []
-        q = [root]
-
-        while q:
-            temp = []
-            for node in q:
-                temp.append(node.val)
-            traversal.append(temp)
-
-            children = []
-            for node in q:
+        # Define our result list that stores lists of values at each level of the tree
+        result = []
+        # Define a queue to store nodes
+        queue = deque()
+        # Add the root node
+        queue.append(root)
+        # While queue 
+        while queue:
+            # get the length of the queue
+            length = len(queue)
+            # Define the array for the level
+            arr = []
+            # Iterate the length of level
+            for i in range(length):
+                node = queue.popleft()
+                # Pop the queue and add the val and add the left and then right if available
+                arr.append(node.val)
                 if node.left:
-                    children.append(node.left)
+                    queue.append(node.left)
                 if node.right:
-                    children.append(node.right)
-            q = children
-
-        return traversal
-
+                    queue.append(node.right)
+            result.append(arr)
+        return result
