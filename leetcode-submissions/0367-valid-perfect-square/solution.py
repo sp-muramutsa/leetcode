f6624224221fd88247ec18743a2f35c0942a443d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-05 01:06:37 +0000

    [LeetCode Sync] Sandrin - Valid Perfect Square - Runtime - 0 ms (100.00%), Memory - 17.8 MB (50.92%)

--- leetcode-submissions/0367-valid-perfect-square/solution.py
+++ leetcode-submissions/0367-valid-perfect-square/solution.py
@@ -1,24 +1,19 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        """
-        Binary search
-        Time: O(logn)
-        Space: O(1)
-        Intuition: Use binary search to halve the search space.
-        """
 
-        lo, hi = 0, num
+        l, r = 0, num
 
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            sq = mid * mid
+        while l <= r:
+            mid = l + (r - l) // 2
 
-            if sq == num:
-                return True
-            elif sq > num:
-                hi = mid - 1
+            if mid**2 > num:
+                r = mid - 1
+
+            elif mid**2 < num:
+                l = mid + 1
+
             else:
-                lo = mid + 1
-        return False
+                return True
 
+        return False
 
