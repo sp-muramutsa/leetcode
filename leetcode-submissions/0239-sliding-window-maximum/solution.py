597df412c973de0b24846df8e783771b1e76f36f
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-07 22:42:11 +0000

    [LeetCode Sync] Sandrin - Sliding Window Maximum - Runtime - 155 ms (78.47%), Memory - 31.1 MB (79.58%)

--- leetcode-submissions/0239-sliding-window-maximum/solution.py
+++ leetcode-submissions/0239-sliding-window-maximum/solution.py
@@ -1,31 +1,25 @@
 from collections import deque
 
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
-        dq = deque() # Will store indices
+
+        dq = deque()
         result = []
         n = len(nums)
 
-        # Make the first window
-        for i in range(k):
-            while dq and nums[dq[-1]] < nums[i]:
-                dq.pop()
-            dq.append(i)
-        
-        result.append(nums[dq[0]]) # First window max
+        for i in range(n):
 
-        # Other windows
-        for j in range(k, n): 
-            if j - dq[0] == k:
+            if dq and i - dq[0] >= k:
                 dq.popleft()
 
-            while dq and nums[j] > nums[dq[-1]]:
+            while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
-          
-            dq.append(j)
-            result.append(nums[dq[0]])
-        
+
+            dq.append(i)
+
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+
         return result
 
-          
