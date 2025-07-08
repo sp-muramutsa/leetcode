ce216811db4ebad5f090d2dcfd55dbd5b35b55af
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-08 16:09:17 +0000

    [LeetCode Sync] Sandrin - Last Stone Weight - Runtime - 0 ms (100.00%), Memory - 17.6 MB (96.49%)

--- leetcode-submissions/1127-last-stone-weight/solution.py
+++ leetcode-submissions/1127-last-stone-weight/solution.py
@@ -1,23 +1,29 @@
 import heapq
-
-
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        # Edge case if list is empty return -1
+        # Create a max heap
+        # Repeat this process until the array has 1 element
+            # pop the top element
+            # pop the second element
+            # find the difference
+            # if difference is greater than 1 add it to the list
+        # Return the last element in he list
+
+        # Function to make elements negative to make the heap a max heap
+        def max_heap(li):
+            for i in range(len(li)):
+                li[i] = li[i] * -1
         
-        stones = [-x for x in stones]
+        max_heap(stones)
         heapq.heapify(stones)
-
-        while len(stones) >= 2:
-            heaviest = heapq.heappop(stones)
-            second_heaviest = heapq.heappop(stones)
-
-            if heaviest != second_heaviest:
-                diff = heaviest - second_heaviest
-                heapq.heappush(stones, diff)
-
-        if len(stones) == 1:
-            result = heapq.heappop(stones)
-            return -result
-        else:
-            return 0
+        while len(stones) > 1:
+            y = heapq.heappop(stones)
+            x = heapq.heappop(stones)
+            diff = (y*-1) - (x*-1)
+            if diff > 0:
+                heapq.heappush(stones, (diff*-1))
+        if stones:
+            return (stones[0] * -1)
+        return 0
 
