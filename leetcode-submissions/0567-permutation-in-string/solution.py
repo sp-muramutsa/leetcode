bcd27fb8c3c9e6dab63b1244cfecb2003428d3fe
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-08 00:08:53 +0000

    [LeetCode Sync] Sandrin - Permutation in String - Runtime - 19 ms (66.57%), Memory - 17.8 MB (87.62%)

--- leetcode-submissions/0567-permutation-in-string/solution.py
+++ leetcode-submissions/0567-permutation-in-string/solution.py
@@ -1,14 +1,52 @@
+from collections import Counter
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        hashmap = {}
-        for i in s1:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        l, r = 0, len(s1) - 1
-        while r < len(s2):
-            count = Counter(s2[l:r+1])
-            if count == hashmap:
+
+        k = len(s1)
+        counter_1 = {chr(i + ord("a")): 0 for i in range(26)}
+        counter_2 = {chr(i + ord("a")): 0 for i in range(26)}
+
+        for char in s1:
+            counter_1[char] += 1
+
+        for char in s2[:k]:
+            counter_2[char] += 1
+
+        # First window: O(26)
+        matches = 0
+        for key in counter_1:
+            if counter_1[key] == counter_2[key]:
+                matches += 1
+
+        n = len(s2)
+        l, r = 0, k
+
+        # Main loop
+
+        while r < n:
+            
+            if matches == 26:
                 return True
-            else:
-                r += 1
-                l += 1
-        return False
+        
+            if counter_2[s2[l]] == counter_1[s2[l]]:
+                matches -= 1
+
+            if counter_2[s2[r]] == counter_1[s2[r]]:
+                matches -= 1
+
+            counter_2[s2[l]] -= 1
+            counter_2[s2[r]] += 1
+
+            if counter_2[s2[l]] == counter_1[s2[l]]:
+                matches += 1
+
+            if counter_2[s2[r]] == counter_1[s2[r]]:
+                matches += 1
+
+            l += 1
+            r += 1       
+      
+        return matches == 26
+
