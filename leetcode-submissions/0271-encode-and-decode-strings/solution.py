93ee293b6e9bdca60baa0292fd196a1394b994f1
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-11 02:35:01 +0000

    [LeetCode Sync] Sandrin - Encode and Decode Strings - Runtime - 163 ms (9.40%), Memory - 18.4 MB (22.30%)

--- leetcode-submissions/0271-encode-and-decode-strings/solution.py
+++ leetcode-submissions/0271-encode-and-decode-strings/solution.py
@@ -1,29 +1,37 @@
 class Codec:
     def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
-        res = ""
-        for i in strs:
-            res += str(len(i)) + "#" + i
-        print(res)
-        return res
-        
+        """Encodes a list of strings to a single string."""
+        encoded_string = []
+
+        for word in strs:
+            for char in word:
+                encoded_string.append(char)
+                encoded_string.append(char)
+            encoded_string.append("01")
+
+        return "".join(encoded_string)
 
     def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        res, i = [], 0
-        while i < len(s):
-            j = i
-            while s[j] != "#":
-                j += 1
-            lenght = int(s[i:j])
-            word = s[j+1:j+1+lenght]
-            res.append(word)
-            i = j + 1 + lenght     
-        return res
+        """Decodes a single string to a list of strings."""
+
+        n = len(s)
+        decoded_strings = []
+        string = []
+
+        i = 0
+        while i < n - 1:
+            while s[i] == s[i + 1]:
+                string.append(s[i])
+                i += 2
+
+            decoded_strings.append("".join(string))
+            string = []
+            i += 2
+
+        return decoded_strings
 
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(strs))
+
