27f077c66c52905583a74db9de74aebcbc677c90
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-11 03:00:52 +0000

    [LeetCode Sync] Sandrin - Product of Array Except Self - Runtime - 127 ms (5.14%), Memory - 26.3 MB (26.12%)

--- leetcode-submissions/0238-product-of-array-except-self/solution.py
+++ leetcode-submissions/0238-product-of-array-except-self/solution.py
@@ -1,32 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = []
-        prefix_product = []
-        current_product = 1
-        for i in range(len(nums)):
-            if i == 0:
-                prefix_product.append(1)
-            else:
-                current_product = current_product * nums[i-1]
-                prefix_product.append(current_product)
-        print(prefix_product )
 
+        # Input: nums = [1,2,3,4]
+        # Output: [24,12,8,6]
 
-        suffix_product = []
-        current_product_1 = 1
-        for i in range(len(nums)-1, -1, -1):
-            if i == len(nums)-1:
-                suffix_product.append(1)
-            else:
-                current_product_1 = current_product_1 * nums[i+1]
-                suffix_product.append(current_product_1)
-        suffix_product = suffix_product[-1::-1]
-        print(suffix_product)
+        # L: [1, 1, 2, 6]
+        # R: [24, 12, 4, 1]
+        # O: [24, 12, 8, 6]
 
-        for i in range(len(prefix_product)):
-            res.append(prefix_product[i]*suffix_product[i])
-        return res
-        
-        
+        n = len(nums)
+        L = [1] * n
+        L[0] = 1
 
-         
+        for i in range(1, n):
+            L[i] = nums[i - 1] * L[i - 1]
+
+        R = [1] * n
+        R[n - 1] = 1
+
+        for j in range(n - 2, -1, -1):
+            print(j, j + 1)
+            R[j] = nums[j + 1] * R[j + 1]
+
+        result = [x * y for (x, y) in zip(L, R)]
+
+        return result
+
