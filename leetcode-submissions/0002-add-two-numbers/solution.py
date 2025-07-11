8223b62b9a8b27ccdaf0636e4bf57cb61cbd6d46
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-11 07:59:52 +0000

    [LeetCode Sync] Sandrin - Add Two Numbers - Runtime - 0 ms (100.00%), Memory - 17.9 MB (56.61%)

--- leetcode-submissions/0002-add-two-numbers/solution.py
+++ leetcode-submissions/0002-add-two-numbers/solution.py
@@ -4,33 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-
-        if not l2:
-            return l1
-
-        if not l1:
-            return l2
-
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0)
-        curr = dummy
-        carry = 0
-
-        while l1 or l2 or carry:
-            digit1 = l1.val if l1 else 0
-            digit2 = l2.val if l2 else 0
-
-            value = digit1 + digit2 + carry
-            new_digit = value % 10
-            carry = value // 10
-
-            curr.next = ListNode(new_digit)
-            curr = curr.next
-
+        p1 = dummy
+        holder = 0
+        while l1 or l2:
+            d1 = l1.val if l1 else 0
+            d2 = l2.val if l2 else 0
+            total = d1 + d2 + holder
+            holder = total // 10
+            total = total % 10
+            p1.next = ListNode(total)
+            p1 = p1.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-
+        if holder > 0:
+            p1.next = ListNode(holder)
+        
         return dummy.next
 
