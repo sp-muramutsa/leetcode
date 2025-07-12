39f65d6395e4671543cfef99d6749e16100c3cfd
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-12 20:37:10 +0000

    [LeetCode Sync] Sandrin - Kth Largest Element in a Stream - Runtime - 16 ms (61.07%), Memory - 23.6 MB (83.55%)

--- leetcode-submissions/0789-kth-largest-element-in-a-stream/solution.py
+++ leetcode-submissions/0789-kth-largest-element-in-a-stream/solution.py
@@ -1,23 +1,37 @@
 import heapq
+
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = nums
-        heapq.heapify(self.nums)
-        while len(self.nums) > k:
-            heapq.heappop(self.nums)
+        
+        # Initialize a heap of size k
+        # K-th largest element will be at the root
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
-        if self.k > len(self.nums):
-            heapq.heappush(self.nums, val)
-
-        elif val > self.nums[0]:
-            heapq.heappushpop(self.nums, val)
-
-        return self.nums[0]
         
+        # If the new value is greater than the k-th largest element(root), push it on the heap. 
+
+        # Edge case of when we start with fewer elements than k
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+        elif val > self.min_heap[0]:
+            heapq.heappush(self.min_heap, val)
+
+        # Make sure the heap doesn't exceed size k
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+
+        # Return the k-th largest element i.e. the root
+        return self.min_heap[0]
+
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
