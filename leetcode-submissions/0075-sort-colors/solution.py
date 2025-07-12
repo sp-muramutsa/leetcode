56a9431b6e188939f9fcf79077b23798e3aa9ed6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-12 03:14:04 +0000

    [LeetCode Sync] Sandrin - Sort Colors - Runtime - 0 ms (100.00%), Memory - 17.9 MB (33.70%)

--- leetcode-submissions/0075-sort-colors/solution.py
+++ leetcode-submissions/0075-sort-colors/solution.py
@@ -3,21 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        def swap(i, j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
 
-        l, r, i = 0, len(nums)-1, 0
-        while i <= r:
-            if nums[i] == 0:
-                swap(l, i)
-                l += 1
-            elif nums[i] == 2:
-                swap(r, i)
-                i -= 1
-                r -= 1
-            i += 1
+        n = len(nums)
+        zero, two = 0, n - 1
+        curr = 0
 
-    
+        while curr <= two:
+            if nums[curr] == 0:
+                nums[curr], nums[zero] = nums[zero], nums[curr]
+                zero += 1
+                curr += 1
+
+            elif nums[curr] == 2:
+                nums[curr], nums[two] = nums[two], nums[curr]
+                two -= 1
+
+            else:
+                curr += 1
+
