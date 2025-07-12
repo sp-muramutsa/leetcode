086a56bd4e4226c7b7063ebb71a5577b89532c1c
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-12 09:56:46 +0000

    [LeetCode Sync] Sandrin - Top K Frequent Elements - Runtime - 0 ms (100.00%), Memory - 21.3 MB (61.57%)

--- leetcode-submissions/0347-top-k-frequent-elements/solution.py
+++ leetcode-submissions/0347-top-k-frequent-elements/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        bucket = [[] for i in range(len(nums)+1)]
+
         counter = Counter(nums)
-        for key, value in counter.items():
-            bucket[value].append(key)
-        res = []
-        print(bucket)
-        for i in bucket[::-1]:
-            if len(i) != 0:
-                for j in i:
-                    res.append(j)
-                    if len(res) == k:
-                        return res
+
+        heap = []
+        for element, count in counter.items():
+            heapq.heappush(heap, (count, element))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [element for count, element in heap]
+
