563b2802c9697a8bb849a00cbc062a4edf855d32
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-12 11:18:02 +0000

    [LeetCode Sync] Sandrin - Meeting Rooms - Runtime - 3 ms (75.22%), Memory - 19.8 MB (60.32%)

--- leetcode-submissions/0252-meeting-rooms/solution.py
+++ leetcode-submissions/0252-meeting-rooms/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        #we sort our list
-        #we iterrate over the list
-        #we check if the first number is less than the previous end number
-        #return false if true else True
-        intervals.sort()
-        for i in range(1, len(intervals)):
-            if intervals[i-1][1] > intervals[i][0]:
+
+        # Sort meetings by start time
+        # Loop through the meetings and make sure that each meeting ends before the next one starts.
+
+        intervals.sort(key=lambda x: x[0])
+        n = len(intervals)
+        for i in range(1, n):
+            if intervals[i - 1][1] > intervals[i][0]:
                 return False
+
         return True
+
