c76be0a663a429ad0a347631061d7a7525fa8cc4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-13 23:05:23 +0000

    [LeetCode Sync] Sandrin - Subsets - Runtime - 0 ms (100.00%), Memory - 18 MB (40.07%)

--- leetcode-submissions/0078-subsets/solution.py
+++ leetcode-submissions/0078-subsets/solution.py
@@ -1,16 +1,32 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
 
-        def dfs(i, curr):
-            if i >= len(nums):
-                res.append(curr.copy())
+        n = len(nums)
+        solution, path = [], []
+
+        def backtrack(i):
+           
+            if i == n:
+                solution.append(path.copy())
                 return
             
-            curr.append(nums[i])
-            dfs(i+1, curr)
-            curr.pop()
-            dfs(i+1, curr)
-            return
-        dfs(0, [])
-        return res 
+            # Don't pick nums[i+1]
+            backtrack(i + 1)
+
+            # Pick nums[i]
+            path.append(nums[i])
+            backtrack(i + 1)
+            path.pop()
+        
+        backtrack(0)
+
+        # Time: O(2 ** n)
+        # Space: space for the answer. O(2 ** n)
+        # Recursion depth space: O(n )
+
+        return solution
+
+
+
+
+        
