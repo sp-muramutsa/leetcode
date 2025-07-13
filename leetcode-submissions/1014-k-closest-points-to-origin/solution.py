35b5c7117bdfa03798f92f716ac98ad04f5a02e1
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-13 02:04:34 +0000

    [LeetCode Sync] Sandrin - K Closest Points to Origin - Runtime - 67 ms (62.61%), Memory - 22.3 MB (81.63%)

--- leetcode-submissions/1014-k-closest-points-to-origin/solution.py
+++ leetcode-submissions/1014-k-closest-points-to-origin/solution.py
@@ -1,21 +1,16 @@
-import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Create a min heap and make it a tuple first element in tuple as Euclidian distance
-        if not points:
-            return []
-        heap = []
-        for i in points:
-            distance = sqrt((i[0]**2) + (i[1]**2))
-            heapq.heappush(heap, (distance, i))
 
-        # Create a result list
-        result = []
-        # Pop from the list in the range k
-        i = 0
-        while i < k and heap:
-            result.append(heapq.heappop(heap)[1])
-            i += 1
-            
-        # Return the result
-        return result
+        max_heap = []
+        for point in points:
+            x, y = point
+            distance_to_origin = sqrt(x**2 + y**2)
+
+            heapq.heappush(max_heap, (-distance_to_origin, point))
+
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+
+        k_th_closest_points = [point for dist, point in max_heap]
+        return k_th_closest_points
+
