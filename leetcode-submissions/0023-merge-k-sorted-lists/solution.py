4745153927aff37d9176b2ecb0ec606cefcd54ab
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-13 11:33:29 +0000

    [LeetCode Sync] Sandrin - Merge k Sorted Lists - Runtime - 20 ms (24.90%), Memory - 20.3 MB (36.65%)

--- leetcode-submissions/0023-merge-k-sorted-lists/solution.py
+++ leetcode-submissions/0023-merge-k-sorted-lists/solution.py
@@ -3,41 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
+
 class Solution:
-
-    ListNode.__lt__ = lambda node, other: node.val < other.val
-
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        dummy = ListNode(0)
-        tail = dummy
-
-        while list1 and list2:
-            if list1 < list2:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-
-        tail.next = list1 if list1 else list2
-        return dummy.next
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
-        if not lists:
-            return
+        min_heap = []
+        for head in lists:
+            if head:
+                heapq.heappush(min_heap, HeapNode(head))
 
-        while len(lists) > 1:
+        dummy = ListNode(val=float("-inf"))
+        curr = dummy
 
-            mergedLists = []
-            for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i + 1] if i + 1 < len(lists) else None
-                mergedLists.append(self.mergeTwoLists(list1, list2))
+        while min_heap:
+            smallest = heapq.heappop(min_heap)
+            node = smallest.node
+            curr.next = node
 
-            lists = mergedLists
+            if node.next:
+                heapq.heappush(min_heap, HeapNode(node.next))
 
-        return lists[0]
+            curr = curr.next
+
+        return dummy.next
 
