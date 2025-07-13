3c2f13ad6f86d8155c39d6eb8360f8ca5042a85b
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-13 23:05:36 +0000

    [LeetCode Sync] Sandrin - Permutations - Runtime - 0 ms (100.00%), Memory - 17.8 MB (81.38%)

--- leetcode-submissions/0046-permutations/solution.py
+++ leetcode-submissions/0046-permutations/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        #We first create our result variable
-        res = []
-        #As we are doing all possiible permutation we will use backtracking and extract all possible possiblities, and thiis problem doesn't have any constraints.
-        def backtracking(curr):
-            if len(curr) == len(nums):
-                res.append(curr.copy())
-                return 
-            #Now because we are using permuatation this is where we loop over over our nums.
-            for i in nums:
-                if i in curr:
-                    continue
-                curr.append(i)
-                backtracking(curr)
-                curr.pop()
-            #Once the current i reaches the end that means no more number that wasn't iterated over so we backtrack.
-            return
-        backtracking([])
-        return res
+
+        n = len(nums)
+        solution, path = [], []
+
+        def backtrack(i):
+            if len(path) == n:
+                solution.append(path.copy())
+                return
+
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    backtrack(num)
+                    path.pop()
+
+        backtrack(1)
+        return solution
+
