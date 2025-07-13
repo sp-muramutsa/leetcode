8ea9d217df3f0259156a999918336e579e66f883
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-13 23:08:08 +0000

    [LeetCode Sync] Sandrin - Combination Sum - Runtime - 2733 ms (5.02%), Memory - 18.1 MB (19.47%)

--- leetcode-submissions/0039-combination-sum/solution.py
+++ leetcode-submissions/0039-combination-sum/solution.py
@@ -1,19 +1,36 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        n = len(candidates)
+        candidates.sort()
+        solution, path = [], []
+        
+        def backtrack(i, path):
+
+            if sum(path) == target:
+                sol = Counter(path)
+                if sol not in solution:
+                    solution.append(sol)
+                return    
+            
+            if sum(path) > target:
+                return 
+
+            for num in candidates:     
+                path.append(num)
+                backtrack(i + 1, path)
+                path.pop()
+        
+        
+        backtrack(0, [])
         res = []
-
-        def backtrack(i, curr, total):
-            if i >= len(candidates) or total > target:
-                return
-            if total == target:
-                res.append(curr.copy())
-                return
-
-            curr.append(candidates[i])
-            backtrack(i, curr, total+candidates[i])
-            curr.pop()
-            backtrack(i+1, curr, total)
-
-        backtrack(0, [], 0)
+        for sol in solution:
+            combination = []
+            for key, value in sol.items():
+                for _ in range(value):
+                    combination.append(key)
+            res.append(combination)
+            
         return res
         
+        return []
