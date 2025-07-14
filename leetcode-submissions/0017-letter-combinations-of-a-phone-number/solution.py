57b9c0b2ca42e29252d20769e5cd7176471f2c1f
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-14 01:40:18 +0000

    [LeetCode Sync] Sandrin - Letter Combinations of a Phone Number - Runtime - 0 ms (100.00%), Memory - 18 MB (22.85%)

--- leetcode-submissions/0017-letter-combinations-of-a-phone-number/solution.py
+++ leetcode-submissions/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        #We check the edge case
-        if len(digits) == 0:
-            return []
-        #We create a hashmap to keep track pf our numbers and map them to letters
+
         hashmap = {
+            "1": "",
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -12,17 +10,38 @@ class Solution:
             "6": "mno",
             "7": "pqrs",
             "8": "tuv",
-            "9": "wxyz"
-            }
-        res = []
-        #We operate the dfs and as the string ar eimmutuable we don't need to pop and we can create new strings on every iteration
-        def dfs(i, curr):
-            if i >= len(digits):
-                res.append(curr)
+            "9": "wxyz",
+        }
+
+        solution, path = [], []
+
+        chars = [char for char in digits]
+
+        n = len(chars)
+        k = len(digits)
+
+        def get_neighbors(idx):
+            neighbors = set()
+            for j in range(idx + 1, n):
+                for char in hashmap[chars[j]]:
+                    neighbors.add((j, char))
+
+            return neighbors
+
+        def backtrack(i):
+            if len(path) == k and path:
+                solution.append("".join(path))
                 return
 
-            for c in hashmap[digits[i]]:
-                dfs(i+1, curr+c)
-        temp = ""
-        dfs(0, temp)
-        return res
+            if i == n:
+                return
+
+            for neighbor in get_neighbors(i):
+                idx, char = neighbor
+                path.append(char)
+                backtrack(idx)
+                path.pop()
+
+        backtrack(-1)
+        return solution
+
