100b622ca9b73cc1feca86c22bade4f681a0f057
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-15 06:20:19 +0000

    [LeetCode Sync] Sandrin - Combination Sum II - Runtime - 20 ms (52.96%), Memory - 18.1 MB (13.80%)

--- leetcode-submissions/0040-combination-sum-ii/solution.py
+++ leetcode-submissions/0040-combination-sum-ii/solution.py
@@ -3,34 +3,25 @@ class Solution:
 
         n = len(candidates)
         candidates.sort()
-        solution, path = set(), []
+        solution = []
 
         def backtrack(i, path, curr_sum):
-
             if curr_sum == target:
-                solution.add(tuple(path[:]))
+                solution.append(path[:])
                 return
-
+            
             if curr_sum > target:
                 return
 
-            for j in range(i + 1, n):
+            for j in range(i, n):
                 
-                if curr_sum + candidates[j] > target: 
-                    break
-                
-                if j > i + 1 and candidates[j] == candidates[j - 1]:
+                if j > i and candidates[j] == candidates[j - 1]:
                     continue
-
-
+                 
                 path.append(candidates[j])
-                curr_sum += candidates[j]
-                backtrack(j, path, curr_sum)
-                
-                curr_sum -= candidates[j]
-                path.pop()
+                backtrack(j + 1, path, curr_sum + candidates[j])
+                path.pop()  
 
-        backtrack(-1, path, 0)
-        res = [list(sol) for sol in solution]
-        return res
+        backtrack(0, [], 0) 
+        return solution
 
