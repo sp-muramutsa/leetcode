3e6a21a56251550dbd4d0de4d67dbd026b8113fa
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-18 00:49:05 +0000

    [LeetCode Sync] Sandrin - Palindrome Partitioning - Runtime - 52 ms (43.38%), Memory - 34.4 MB (73.12%)

--- leetcode-submissions/0131-palindrome-partitioning/solution.py
+++ leetcode-submissions/0131-palindrome-partitioning/solution.py
@@ -3,24 +3,22 @@ class Solution:
         return s == s[::-1]
 
     def partition(self, s: str) -> List[List[str]]:
-        
-        solution, path = [], []
+        solution = []
         n = len(s)
 
-        def backtrack(i):
+        def backtrack(i, path):
+
             if i == n:
-                if "".join(path) == s:
-                    ans = path[1:]
-                    if ans:
-                        solution.append(ans)
+                solution.append(path[:])
                 return
 
             for j in range(i + 1, n + 1):
                 if self.is_palindrome(s[i:j]):
                     path.append(s[i:j])
-                    backtrack(j)
+                    backtrack(j, path)
                     path.pop()
+           
 
-        backtrack(-1)
+        backtrack(0, [])
         return solution
 
