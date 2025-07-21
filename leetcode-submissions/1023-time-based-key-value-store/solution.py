6b9a8b24e94545897e17c66fbfe8c69cb1cb00dd
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-21 01:06:18 +0000

    [LeetCode Sync] Sandrin - Time Based Key-Value Store - Runtime - 135 ms (41.45%), Memory - 73.7 MB (88.23%)

--- leetcode-submissions/1023-time-based-key-value-store/solution.py
+++ leetcode-submissions/1023-time-based-key-value-store/solution.py
@@ -1,35 +1,49 @@
 class TimeMap:
 
     def __init__(self):
-        self.map = {}
+        self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.map:
-            self.map[key] = [(timestamp, value)]
-        else:
-            idx = self.bisect_right(self.map[key], timestamp)
-            self.map[key].insert(idx, (timestamp, value))
+        self.store[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if not key in self.map:
-            return ""
+        idx = self.bisect_left(self.store[key], timestamp)
 
-        idx = self.bisect_right(self.map[key], timestamp) - 1
+        if idx > -1:
+            return self.store[key][idx][1]
+        return ""
 
-        return self.map[key][idx][1] if idx >= 0 else ""
-    
-    def bisect_right(self, arr, target):
-        l, r = 0, len(arr) 
+    def bisect_left(self, arr: list, target: int) -> int:
+        l, r = 0, len(arr)
 
         while l < r:
-            mid = (l+r) // 2
+
+            mid = l + (r - l) // 2
             if arr[mid][0] <= target:
                 l = mid + 1
             else:
-                r = mid 
-        
-        return l 
+                r = mid
 
+        return l - 1
+
+
+# key: [(timestamp, value)]
+# Is timestamp <= target?
+# Truth: T, T, T, T, T, F, F, F, F, ..
+# If T
+# we're left. l = mid. This might be the last truth
+# Otherwise
+# we're right. r = mid - 1.
+# At the end, return r - 1.
+# [1, 4] [T, T] target = 4  idx = 1
+# [1, 4] [T, T] target = 5 idx = 1
+# [10, 20] target = 5. [F, F]
+# [10, 20] target = 10. [T, F]
+# [10, 20] target = 15. [T, F]
+# [10, 20] target = 20. [T, T]
+# [10, 20] target = 25. [T, T]
+# Is timestamp > target?
+# T, T, T, T, F -> We want to return the last True if any.
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
