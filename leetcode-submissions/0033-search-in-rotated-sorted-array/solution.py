104bf2d47a7353e7ce10329f97630a6b1a1774f2
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-21 00:13:36 +0000

    [LeetCode Sync] Sandrin - Search in Rotated Sorted Array - Runtime - 3 ms (4.34%), Memory - 18.2 MB (41.53%)

--- leetcode-submissions/0033-search-in-rotated-sorted-array/solution.py
+++ leetcode-submissions/0033-search-in-rotated-sorted-array/solution.py
@@ -1,26 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l+r) // 2
+
+        while l < r:
+            mid = l + (r - l) // 2
+
             if nums[mid] == target:
                 return mid
-
-            if nums[mid] >= nums[l]:
-                if nums[mid] < target:
-                    l = mid + 1
-                elif nums[mid] > target:
-                    if nums[l] <= target:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
             
-            elif nums[mid] <= nums[r]:
-                if nums[mid] > target:
-                    r = mid - 1
-                elif nums[mid] < target:
-                    if nums[r] >= target:
-                        l = mid + 1
-                    else:
-                        r = mid - 1
-        return -1
+            # [4,5,6,7,0,1,2]
+            # Left: sorted part
+            if nums[mid] >= nums[0]:
+                # If target is left, search here
+                if nums[0] <= target < nums[mid]:
+                    r = mid 
+                else:
+                    l = mid + 1
+            
+            # Right: rotated part
+            else:
+                # If target is right, search here
+                if nums[mid] < target <= nums[-1]:
+                    l = mid + 1
+                else:
+                    r = mid 
+        
+        
+        return l if nums[l] == target else -1
