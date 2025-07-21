0e4eed308ef3e4345e53a3e9644907ce21d2bdac
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-21 03:39:12 +0000

    [LeetCode Sync] Sandrin - Median of Two Sorted Arrays - Runtime - 0 ms (100.00%), Memory - 18.3 MB (18.96%)

--- leetcode-submissions/0004-median-of-two-sorted-arrays/solution.py
+++ leetcode-submissions/0004-median-of-two-sorted-arrays/solution.py
@@ -1,34 +1,37 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
-        half = total // 2
 
         if len(A) > len(B):
-            A, B = B, A  # Ensure A is the smaller array
+            B, A = A, B
 
-        l, r = 0, len(A) - 1
+        lenA, lenB = len(A), len(B)
+        total = lenA + lenB
+        half = total // 2
+
+        l, r = 0, lenA - 1
 
         while True:
-            mid1 = (l + r) // 2  # A
-            mid2 = half - mid1 - 2  # B
+            mid1 = l + (r - l) // 2
+            mid2 = half - mid1 - 2
 
-            ALeft = A[mid1] if mid1 >= 0 else float("-infinity")
-            ARight = A[mid1 + 1] if mid1 + 1 < len(A) else float("infinity")
+            AMid = A[mid1] if mid1 >= 0 else float("-inf")
+            APostMid = A[mid1 + 1] if (mid1 + 1) < lenA else float("inf")
 
-            BLeft = B[mid2] if mid2 >= 0 else float("-infinity")
-            BRight = B[mid2 + 1] if mid2 + 1 < len(B) else float("infinity")
+            BMid = B[mid2] if mid2 >= 0 else float("-inf")
+            BPostMid = B[mid2 + 1] if (mid2 + 1) < lenB else float("inf")
 
-            # Partition is correct
-            if ALeft <= BRight and BLeft <= ARight:
+            # Correct partition
+            if AMid <= BPostMid and BMid <= APostMid:
                 # Odd
-                if total % 2 == 0:
-                    return (max(ALeft, BLeft) + min(ARight, BRight)) / 2
-                # Even
+                if total % 2 == 1:
+                    return min(APostMid, BPostMid)
                 else:
-                    return min(ARight, BRight)
+                    return (max(AMid, BMid) + min(APostMid, BPostMid)) / 2
 
-            elif ALeft > BRight:
+            # Too many values from A
+            elif AMid > BPostMid:
                 r = mid1 - 1
             else:
                 l = mid1 + 1
