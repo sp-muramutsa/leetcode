74656505a27594abb8c46348a4c9b8b89eda1af6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-22 02:43:34 +0000

    [LeetCode Sync] Sandrin - First Bad Version - Runtime - 33 ms (80.93%), Memory - 17.9 MB (26.77%)

--- leetcode-submissions/0278-first-bad-version/solution.py
+++ leetcode-submissions/0278-first-bad-version/solution.py
@@ -1,25 +1,23 @@
 # The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return a bool
-# def isBadVersion(version):
+# def isBadVersion(version: int) -> bool:
 
-class Solution(object):
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        Intuition: Binary search. https://www.youtube.com/watch?v=tgVSkMA8joQ&t=426s&ab_channel=mCoding
-        """
-        lo, hi = 0, n
 
-        while lo < hi:
-            mid = lo + (hi-lo) // 2
-            if isBadVersion(mid):
-                hi = mid
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        # [G, G, G, B, B]
+        # We want to return the first Bad version
+        # Bisect left
+
+        l, r = 0, n
+
+        while l < r:
+            mid = l + (r - l) // 2
+
+            if not isBadVersion(mid):
+                l = mid + 1
             else:
-                lo = mid + 1
-                
-        
-        return lo
+                r = mid
 
-        
+        return l
+
