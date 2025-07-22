bff3312f0457455024e7393c343bde1dcedf6a01
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-22 04:13:02 +0000

    [LeetCode Sync] Sandrin - Guess Number Higher or Lower - Runtime - 36 ms (66.57%), Memory - 17.6 MB (71.35%)

--- leetcode-submissions/0374-guess-number-higher-or-lower/solution.py
+++ leetcode-submissions/0374-guess-number-higher-or-lower/solution.py
@@ -8,22 +8,18 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
 
-        """
-        Binary search
-        Time: O(logn)
-        Space: O(1)
-        Intuition: Just like the guessing game, we eliminate a number we     guessed along with either all the numbers greater than it or less.
-        """
         l, r = 0, n
 
         while l <= r:
             mid = l + (r - l) // 2
-            guessed = guess(mid)
-            print(mid, guessed)
-            if guessed == -1:
-                r = mid - 1 
-            elif guessed == 1:
-                l = mid + 1
-            elif guessed == 0:
+            test = guess(mid)
+            
+            if test == 0:
                 return mid
-    
+            
+            elif test == -1:
+                r = mid - 1
+            
+            else:
+                l = mid + 1
+        
