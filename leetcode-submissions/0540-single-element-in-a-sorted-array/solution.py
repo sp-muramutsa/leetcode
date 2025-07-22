eb4aac915213f3dba9af6126b945443a6c895ea1
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-22 02:36:05 +0000

    [LeetCode Sync] Sandrin - Single Element in a Sorted Array - Runtime - 0 ms (100.00%), Memory - 24.8 MB (44.83%)

--- leetcode-submissions/0540-single-element-in-a-sorted-array/solution.py
+++ leetcode-submissions/0540-single-element-in-a-sorted-array/solution.py
@@ -1,33 +1,29 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        """
-        Time: O(n)
-        Space: O(n)
-        Intuition: Use binary search to reduce search space by O(logn). The secret sauce here is that the first and last element of the duplicated numbers are positioned in different ways on left vs right of the single element. e.g. if the single is at index 2: we have 0, 1 vs 3, 4. EVEN-ODD vs ODD-EVEN.
-        """
+
+        # Bisect left
+        # All single elements are at even indices
+        # [2, 2, 3, 3, 7, 7, 10, 11, 11, 12, 12]
+        # Is nums[i] == nums[i + 1]
+        # [T, _, T, _, T, _, F, _, F, _, F] -> Return the first False. 
+        # Only selecting even indices: retuce mid by 1 if it's even and add 2 to left
+        # Left: nums[i - 1] < nums[i] == nums[i + 1]
+
         n = len(nums)
-        l, r = 0, n-1
+        l, r = 0, n - 1
 
-        if n == 1:
-            return nums[0]
-
-        while l <= r:
+        while l < r:
             mid = l + (r - l) // 2
+            if mid % 2 == 1:
+                mid -= 1
 
-            if l == r:
-                return nums[l]
-            
-
-            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid+1]:
-                return nums[mid]
-            
-            # Right
-            elif (mid % 2 == 0 and nums[mid] == nums[mid-1]) or (mid % 2 != 0 and nums[mid] == nums[mid+1]):
-                r = mid - 1
-            
             # Left
-            elif (mid % 2 == 0 and nums[mid] == nums[mid+1]) or (mid % 2 != 0 and nums[mid] == nums[mid-1]):
-                l = mid + 1
-            
-        
-        return 0
+            if nums[mid] == nums[mid + 1]:
+                l = mid + 2
+
+            # Right
+            else:
+                r = mid
+
+        return nums[l]
+
