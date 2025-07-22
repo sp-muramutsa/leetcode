ec564cb6ce0a1043889bcb8e0980ec3b11cd5019
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-22 00:06:42 +0000

    [LeetCode Sync] Sandrin - Find First and Last Position of Element in Sorted Array - Runtime - 0 ms (100.00%), Memory - 19.1 MB (20.37%)

--- leetcode-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ leetcode-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,21 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.BinSearch(nums, True, target)
-        right = self.BinSearch(nums, False, target)
-        return [left, right]
-    def BinSearch(self, nums, left_bias, target):
-        l, r = 0, len(nums) - 1
-        i = -1
-        while l <= r:
-            mid = (l+r) // 2
+
+        # Find the first occurence
+        # Is number less than target?
+        # [T, T, T, T, [F], F, F, F] -> First F
+
+        n = len(nums)
+        l, r = 0, n
+        while l < r:
+            mid = l + (r - l) // 2
+
             if nums[mid] < target:
                 l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
             else:
-                i = mid
-                if left_bias:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-        return i
+                r = mid
+
+        if 0 <= l < n and nums[l] == target:
+            start = l
+        else:
+            return [-1, -1]
+
+        lo, hi = l, n
+
+        # Last first occurence
+        # Is number equal target?
+        # [T, T, T, T, [F], F, F, F] -> First F
+        while lo < hi:
+            middle = lo + (hi - lo) // 2
+
+            if nums[middle] == target:
+                lo = middle + 1
+            else:
+                hi = middle
+
+        if 0 <= lo <= n and nums[lo - 1] == target:
+            print(start)
+            return [start, lo - 1]
+
+        return [-1, -1]
+
