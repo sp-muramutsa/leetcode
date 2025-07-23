b1bc7c30d8ae32d03dd970fc903b2040b3db42ec
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-23 09:40:18 +0000

    [LeetCode Sync] Sandrin - Daily Temperatures - Runtime - 86 ms (82.57%), Memory - 27.3 MB (77.62%)

--- leetcode-submissions/0739-daily-temperatures/solution.py
+++ leetcode-submissions/0739-daily-temperatures/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # We use a stack to keep track of temperatures
-        # We create a result array with zero values
-        stack = []
-        res = [0] * len(temperatures)
 
-        for i, n in enumerate(temperatures):
-            if i == 0:
-                stack.append(i)
-                continue
-            
-            elif n < temperatures[i-1]:
-                stack.append(i)
-                continue
-            
-            while stack and temperatures[stack[-1]] < n:
-                print(stack[-1])
-                res[stack[-1]] = i - stack[-1]
-                stack.pop()
+        n = len(temperatures)
+        stack = []
+        res = [0] * n
+        i = 0
+
+        while i < n:
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                prev_day = stack.pop()
+                res[prev_day] = i - prev_day
             stack.append(i)
-        print(res)
-        return res 
+            i += 1
+
+        return res
+
