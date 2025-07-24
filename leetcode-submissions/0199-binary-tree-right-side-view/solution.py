2659c7bcfb12be887e48f951ba5d822108980920
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-24 16:33:26 +0000

    [LeetCode Sync] Sandrin - Binary Tree Right Side View - Runtime - 0 ms (100.00%), Memory - 17.9 MB (17.19%)

--- leetcode-submissions/0199-binary-tree-right-side-view/solution.py
+++ leetcode-submissions/0199-binary-tree-right-side-view/solution.py
@@ -6,36 +6,24 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # Edge case
-        if root is None:
+
+        if not root:
             return []
-        # Using BFS we will traverse our binary tree
-        # Define the result list
-        result = []
-        # Define the queue
-        queue = deque()
-        # Add the root node to the queue
-        queue.append(root)
-        # While queue
-        while queue:
-            # Get the length of queue
-            length = len(queue) - 1
-            # pop the first node and add it to the result
-            node = queue.popleft()
-            # Add its right node first and then left node if they are not None
-            result.append(node.val)
-            if node.right is not None:
-                queue.append(node.right)
-            if node.left is not None:
-                queue.append(node.left)
-            # Itereate over the other nodes
-            # pop the rest of the queu and adding the children
-            print([i.val for i in queue])
-            print(result)
-            for i in range(length):
-                node = queue.popleft()
-                if node.right:
-                    queue.append(node.right)
-                if node.left:
-                    queue.append(node.left)
-        return result
+
+        mirror_view = []
+
+        def dfs(node, level):
+
+            if not node:
+                return
+
+            if level == len(mirror_view):
+                mirror_view.append(node.val)
+
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+
+        dfs(root, 0)
+
+        return mirror_view
+
