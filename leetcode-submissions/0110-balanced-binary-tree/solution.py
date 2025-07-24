8261c440837badbec67eecb987ea2843f608044d
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-24 22:42:23 +0000

    [LeetCode Sync] Sandrin - Balanced Binary Tree - Runtime - 0 ms (100.00%), Memory - 18.9 MB (27.07%)

--- leetcode-submissions/0110-balanced-binary-tree/solution.py
+++ leetcode-submissions/0110-balanced-binary-tree/solution.py
@@ -9,23 +9,19 @@ class Solution:
 
         self.balance = True
 
-        def helper(node):
-            """
-            Calculates the depth of two subtrees of the same node
-            and checks if they differ by more than one node.
-            """
+        def depth(node):
 
             if not node:
                 return 0
 
-            left_depth = helper(node.left)
-            right_depth = helper(node.right)
+            left_height = depth(node.left)
+            right_height = depth(node.right)
 
-            if abs(left_depth - right_depth) > 1:
+            if abs(left_height - right_height) > 1:
                 self.balance = False
 
-            return 1 + max(left_depth, right_depth)
+            return 1 + max(left_height, right_height)
 
-        helper(root)
+        depth(root)
         return self.balance
 
