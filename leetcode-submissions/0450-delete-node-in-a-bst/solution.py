2bfa52ed07e01c8bb973026d24184d5c9ff944e4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-25 05:24:57 +0000

    [LeetCode Sync] Sandrin - Delete Node in a BST - Runtime - 0 ms (100.00%), Memory - 21.4 MB (74.69%)

--- leetcode-submissions/0450-delete-node-in-a-bst/solution.py
+++ leetcode-submissions/0450-delete-node-in-a-bst/solution.py
@@ -5,29 +5,58 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #using predecessor, largest value smaller than the key root
-    def predecessor(self, root):
-        curr = root.left
-        while curr is not None and curr.right is not None:
+
+    def get_inorder_successor(self, node):
+        """
+        Smallest value on the right
+        """
+        curr = node.right
+
+        while curr.left:
+            curr = curr.left
+        return curr
+
+    def get_inorder_predecessor(self, node):
+        """
+        Largest element on the left
+        """
+        curr = node.left
+
+        while curr.right:
             curr = curr.right
         return curr
 
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if root is None:
-            return None
+
+        # Case 1: target not found
+        if not root:
+            return
+
         if root.val > key:
             root.left = self.deleteNode(root.left, key)
+
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
+
         else:
-            if root.left is None and root.right is None:
+
+            # Case 1: Target node is a leaf node: Simply prune it
+            if not root.left and not root.right:
                 return None
-            if root.left is None and root.right is not None:
+
+            # Case 2: Target node has only one child node: Replace it with its child
+            if not root.left:
                 return root.right
-            if root.right is None and root.left is not None:
+
+            if not root.right:
                 return root.left
+
+            # Case 3: Target node has two children nodes:
+            # Replace target node with inorder successor or predecessor
             else:
-                succ = self.predecessor(root)
-                root.val = succ.val
-                root.left = self.deleteNode(root.left, succ.val)
+                inorder_predecessor = self.get_inorder_predecessor(root)
+                root.val = inorder_predecessor.val
+                root.left = self.deleteNode(root.left, inorder_predecessor.val)
+
         return root
+
