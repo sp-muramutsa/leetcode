795a34ecd2e0cdceac95cde0d18f3f00a5a298a6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-27 21:01:51 +0000

    [LeetCode Sync] Sandrin - Move Zeroes - Runtime - 4 ms (61.42%), Memory - 18.7 MB (94.73%)

--- leetcode-submissions/0283-move-zeroes/solution.py
+++ leetcode-submissions/0283-move-zeroes/solution.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
+        
         n = len(nums)
-        last_non_zero = 0
+        slow, fast = 0, 0
 
-        for i in range(n):
-            if nums[i] != 0:
-                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
-                last_non_zero += 1
-
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
