bf5f47aea5fbcd95ad61616dea46d945ecf1ed91
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-28 01:57:22 +0000

    [LeetCode Sync] Sandrin - Binary Search - Runtime - 0 ms (100.00%), Memory - 18.9 MB (27.49%)

--- leetcode-submissions/0792-binary-search/solution.py
+++ leetcode-submissions/0792-binary-search/solution.py
@@ -1,7 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
-        l, r = 0, len(nums) - 1
+        
+        n = len(nums)
+        l, r = 0, n
 
         while l < r:
             mid = l + (r - l) // 2
@@ -10,7 +11,5 @@ class Solution:
             else:
                 r = mid
 
-        if nums[l] == target:
-            return l
-        return -1
+        return l if l < n and nums[l] == target else -1
 
