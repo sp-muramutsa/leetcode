241cbed019d080ea864182703b9617f5b02ed461
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-28 21:14:27 +0000

    [LeetCode Sync] Sandrin - Design Hit Counter - Runtime - 0 ms (100.00%), Memory - 17.7 MB (89.30%)

--- leetcode-submissions/0362-design-hit-counter/solution.py
+++ leetcode-submissions/0362-design-hit-counter/solution.py
@@ -1,48 +1,24 @@
 class HitCounter:
 
     def __init__(self):
-        self.hits = deque([])
+        self.counter = []
 
     def hit(self, timestamp: int) -> None:
-        self.hits.appendleft(timestamp)
+        self.counter.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        print(self.hits, timestamp)
-        # i = len(self.hits) - 1
-        # count = 0
-
-        # while i >= 0:
-        #     if timestamp - self.hits[i] < 300:
-        #         count += 1
-        #     else:
-        #         return count
-        #     i -= 1
-        
-        # return count
-        n = len(self.hits)
+        n = len(self.counter)
         l, r = 0, n
 
         while l < r:
             mid = l + (r - l) // 2
-
-            # Left
-            if timestamp - self.hits[mid] < 300:
+            if timestamp - self.counter[mid] >= 300:
                 l = mid + 1
             else:
                 r = mid
-        
 
-        return l
+        return len(self.counter[l:n])
 
-            
-        # Is timestamp - nums[mid] < 300?
-        # Return the first False.
-        # [1, 2, 3], 4
-        # [T, T, T] 
-        # [1, 2, 3, 300], 300, return 4
-        #
-        # 1, 2, 3, 300], 301, 
-        # 
 
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
