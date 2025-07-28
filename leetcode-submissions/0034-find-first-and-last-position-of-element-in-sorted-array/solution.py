f22d0f6ba272ddd40b3913c43db93f1b32f5a642
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-28 22:16:36 +0000

    [LeetCode Sync] Sandrin - Find First and Last Position of Element in Sorted Array - Runtime - 0 ms (100.00%), Memory - 18.9 MB (93.45%)

--- leetcode-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ leetcode-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        # Find the first occurence
-        # Is number less than target?
-        # [T, T, T, T, [F], F, F, F] -> First F
-
         n = len(nums)
+
+        # Bisect left
+        # First occurence. Left: nums[mid] < target
+
         l, r = 0, n
         while l < r:
             mid = l + (r - l) // 2
@@ -16,26 +16,23 @@ class Solution:
                 r = mid
 
         if 0 <= l < n and nums[l] == target:
-            start = l
+            left, right = l, n
+            first = l  # First False
         else:
             return [-1, -1]
 
-        lo, hi = l, n
-
-        # Last first occurence
-        # Is number equal target?
-        # [T, T, T, T, [F], F, F, F] -> First F
-        while lo < hi:
-            middle = lo + (hi - lo) // 2
+        # Last occurence. Left: nums[mid] == target
+        while left < right:
+            middle = left + (right - left) // 2
 
             if nums[middle] == target:
-                lo = middle + 1
+                left = middle + 1
             else:
-                hi = middle
+                right = middle
 
-        if 0 <= lo <= n and nums[lo - 1] == target:
-            print(start)
-            return [start, lo - 1]
-
-        return [-1, -1]
+        last = left - 1  # Last True
+        if 0 <= last < n and nums[left - 1] == target:
+            return [first, last]
+        else:
+            return [-1, -1]
 
