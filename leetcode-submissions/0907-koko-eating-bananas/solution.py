0003ddfeb544d2fcc26633c4331f62efd284cc45
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-28 23:50:11 +0000

    [LeetCode Sync] Sandrin - Koko Eating Bananas - Runtime - 139 ms (93.91%), Memory - 19.1 MB (9.92%)

--- leetcode-submissions/0907-koko-eating-bananas/solution.py
+++ leetcode-submissions/0907-koko-eating-bananas/solution.py
@@ -1,24 +1,20 @@
 class Solution:
+    def can_eat_all(self, rate: int, h: int, piles: List[int]) -> bool:      
+        hours = 0
+        for pile in piles:
+            hours += ceil(pile / rate)
+        return hours <= h
+
+
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        minRate, maxRate = 1, max(piles)
 
-        while minRate < maxRate:
-            midRate = minRate + (maxRate - minRate) // 2
-        
-            time = 0
-            for pile in piles:
-                time += math.ceil(pile / midRate) # Take ceiling bc Koko will not eat more bananas during this our (read quesiton carefullt)
-            
-            # Unworkable: we need to increase the rate
-            if time > h:
-                minRate = midRate + 1
-            
-            # Workable: we need to reduce the rate. But cautiously since midRate is valid too! And thus, it might be the first valid value
+        l, r = 1, max(piles)
+
+        while l < r:
+            rate = l + (r - l) // 2  
+            if self.can_eat_all(rate, h, piles):
+                r = rate
             else:
-                maxRate = midRate
-            
-        return minRate
+                l = rate + 1
         
-                
-
-        
+        return l
