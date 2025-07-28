e2c9b779ac1b6f766ab82c1493db46e72f1d019e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-28 02:40:10 +0000

    [LeetCode Sync] Sandrin - Search a 2D Matrix - Runtime - 0 ms (100.00%), Memory - 18.1 MB (71.88%)

--- leetcode-submissions/0074-search-a-2d-matrix/solution.py
+++ leetcode-submissions/0074-search-a-2d-matrix/solution.py
@@ -1,31 +1,47 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-        m = len(matrix)
-        n = len(matrix[0])
+        # Find the right row
+        # leftR, rightR = 0, m
 
-        l, r = 0, m
+        # midR = leftR + rightR
+        # if nums[midR][0] > target:
+        # rightR = mid - 1
+        # else:
+        # if nums[mid][n - 1] < target:
+        # leftR = midR + 1
+        # else:  --- RIGHT ROW
+        # l, r = 0, n
+        # while l < r:
+        #   tout de suite ...
 
-        while l < r:
-            m = (l + r) // 2
+        m, n = len(matrix), len(matrix[0])
 
-            left, right = 0, n
-            while left < right:
-                mid = (left + right) // 2
-                if matrix[m][mid] == target:
-                    return True
-                elif matrix[m][mid] > target:
-                    right = mid
-                else:
-                    left = mid + 1
+        L, R = 0, m - 1
 
-            if matrix[m][0] > target:
-                r = m
-            elif matrix[m][0] < target:
-                l = m + 1
+        while L <= R:
+
+            M = (L + R) // 2
+
+            if matrix[M][0] > target:
+                R = M - 1
             else:
-                return True
+                if matrix[M][n - 1] < target:
+                    L = M + 1
+                else:
 
+                    left, right = 0, n - 1
+                    while left <= right:
+
+                        mid = (left + right) // 2
+
+                        if matrix[M][mid] == target:
+                            return True
+                        elif matrix[M][mid] < target:
+                            left = mid + 1
+                        else:
+                            right = mid - 1
+                    break
 
         return False
 
