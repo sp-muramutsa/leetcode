e0e800e73caac33afb1963187f126d7a54dd18f0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-30 10:50:23 +0000

    [LeetCode Sync] Sandrin - Capacity To Ship Packages Within D Days - Runtime - 293 ms (11.72%), Memory - 22.1 MB (54.97%)

--- leetcode-submissions/1056-capacity-to-ship-packages-within-d-days/solution.py
+++ leetcode-submissions/1056-capacity-to-ship-packages-within-d-days/solution.py
@@ -1,34 +1,35 @@
 class Solution:
+    def can_ship(self, capacity, weights, count, actual_days):
+
+        days = 0
+        i = 0
+        while i < count:
+            # Ship for a day depending on capacity
+            curr_capacity = capacity
+            while curr_capacity > 0 and i < count:
+                if curr_capacity < weights[i]:
+                    break
+                else:
+                    curr_capacity -= weights[i]
+                    i += 1
+            days += 1
+
+            if days > actual_days:
+                return False
+
+        return days <= actual_days
+
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        
+
         n = len(weights)
-        def can_ship(capacity: int) -> bool:
-            i = 0
+        l, r = 1, sum(weights) 
 
-            for day in range(days):
-                cap = capacity    
-                while i < n and cap >= weights[i]:
-                    cap -= weights[i]
-                    i += 1      
-
-            return i >= n
-        
-       
-        
-        # # [T, T, T, T, T, F] -> Return the first False
-
-        # # 1 to total
-        total = sum(weights)
-
-
-        l, r = 0, total
         while l < r:
             mid = l + (r - l) // 2
-            print(mid)
-            if can_ship(mid):
+            if self.can_ship(mid, weights, n, days):
                 r = mid
             else:
                 l = mid + 1
-        
+
         return l
 
