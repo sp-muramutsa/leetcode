9e9fb48cfe89a305798dd6aea13955da7ea2f105
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-07-31 03:11:40 +0000

    [LeetCode Sync] Sandrin - Search in Rotated Sorted Array - Runtime - 0 ms (100.00%), Memory - 18 MB (66.68%)

--- leetcode-submissions/0033-search-in-rotated-sorted-array/solution.py
+++ leetcode-submissions/0033-search-in-rotated-sorted-array/solution.py
@@ -1,29 +1,35 @@
 class Solution:
-
     def search(self, nums: List[int], target: int) -> int:
-
+        
         n = len(nums)
-        l, r = 0, n - 1
+        l, r = 0, n
 
         while l < r:
+            
             mid = l + (r - l) // 2
-
+            
             if nums[mid] == target:
                 return mid
 
-            # Left sorted subarray
-            elif nums[mid] >= nums[0]:
+            # Left sorted array
+            if nums[mid] >= nums[0]:
                 if nums[0] <= target < nums[mid]:
                     r = mid
                 else:
                     l = mid + 1
-
-            # Right sorted
-            else:
+            
+            # Right sorted array
+            elif nums[mid] <= nums[-1]:
                 if nums[mid] < target <= nums[-1]:
                     l = mid + 1
                 else:
                     r = mid
-
-        return l if nums[l] == target else -1
+            
+           
+            
+        
+        return -1
+                
+        
+      
 
