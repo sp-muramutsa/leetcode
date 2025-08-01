a09518ab174a54f8127ae0ef9c21b36e621eedfd
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-01 22:29:34 +0000

    [LeetCode Sync] Sandrin - Find the Length of the Longest Common Prefix - Runtime - 1019 ms (5.02%), Memory - 37.6 MB (12.10%)

--- leetcode-submissions/3329-find-the-length-of-the-longest-common-prefix/solution.py
+++ leetcode-submissions/3329-find-the-length-of-the-longest-common-prefix/solution.py
@@ -1,64 +1,74 @@
 class TrieNode:
     def __init__(self):
-        self.digits = [None] * 10
-        self.end = False
+        self.children = [None] * 10
+        self.is_end = False
 
-    def contains(self, key: chr) -> bool:
-        return self.digits[int(key)] is not None
+    def contains_digit(self, key: str) -> bool:
+        return self.children[ord(key) - ord("0")] is not None
 
-    def put(self, key: chr, node: "TrieNode") -> None:
-        self.digits[int(key)] = node
+    def set_digit(self, key: str, node: "TrieNode") -> None:
+        self.children[ord(key) - ord("0")] = node
 
-    def get(self, key: chr) -> "TrieNode":
-        return self.digits[int(key)]
-
-    def is_end(self) -> bool:
-        return self.end
+    def get_digit(self, key: str) -> None:
+        return self.children[ord(key) - ord("0")]
 
     def set_end(self) -> None:
-        self.end = True
+        self.is_end = True
+
+    def check_end(self) -> bool:
+        return self.is_end
 
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
-    def insert(self, number: int) -> None:
-        number = str(number)
+    def insert_number(self, number: int) -> None:
         curr = self.root
+        number = str(number)
 
-        for char in number:
-            if not curr.contains(char):
-                curr.put(char, TrieNode())
-            curr = curr.get(char)
-
+        for digit in number:
+            if not curr.contains_digit(digit):
+                node = TrieNode()
+                curr.set_digit(digit, node)
+            curr = curr.get_digit(digit)
         curr.set_end()
 
-    def prefix_length(self, number: int) -> int:
-        i = 0
+    def longest_prefix(self, number: int) -> int:
+
         number = str(number)
-        curr = self.root
+        n = len(number)
+        length = 0
 
-        for char in number:
-            if not curr.contains(char):
-                break
-            i += 1
-            curr = curr.get(char)
+        def dfs(node, idx):
+            if not node:
+                return
 
-        return i
+            if idx == n:
+                return
+
+            if node.contains_digit(number[idx]):
+                nonlocal length
+                length += 1
+                child = node.get_digit(number[idx])
+                return dfs(child, idx + 1)
+
+        dfs(self.root, 0)
+        return length
 
 
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
 
         trie = Trie()
-        for x in arr1:
-            trie.insert(x)
+        for num1 in arr1:
+            trie.insert_number(num1)
 
-        longest = 0
-        for y in arr2:
-            longestPrefix = trie.prefix_length(y)
-            longest = max(longest, longestPrefix)
+        longest_prefix = 0
 
-        return longest
+        for num2 in arr2:
+            num_common_prefix = trie.longest_prefix(num2)
+            longest_prefix = max(longest_prefix, num_common_prefix)
+
+        return longest_prefix
 
