1e7ecb45ac5c77b868feea260b9e86d8239bb4fb
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-02 20:33:10 +0000

    [LeetCode Sync] Sandrin - Combination Sum - Runtime - 31 ms (15.88%), Memory - 12.7 MB (16.96%)

--- leetcode-submissions/0039-combination-sum/solution.py
+++ leetcode-submissions/0039-combination-sum/solution.py
@@ -1,28 +1,29 @@
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        # Edge case is when the sum of the numbers in current list is greater than target, if the index is past length of the list
+        # If the sum of the current is equal to the target add it to the result and return
+        # Add the current index
+        # Recursively call the function with same index
+        # After backtracking remove from  the current list and recursively call the next index
+        # After defining the above function and running it return the result list
+        result = []
+        def backtrack(i, curr, total):
+            if total == target:
+                result.append(curr[:])
+                return 
 
-        solution = []
-        n = len(candidates)
-        candidates.sort()
+            if i == len(candidates) or total > target:
+                return 
 
-        def backtrack(i, path, curr_sum):
-
-            if curr_sum > target:
-                return
-
-            if curr_sum == target:
-                solution.append(path[:])
-                return
-
-            for j in range(i, n):
-                
-                if j > i  and candidates[j] == candidates[j - 1]:
-                    continue
-
-                path.append(candidates[j])
-                backtrack(j, path, curr_sum + candidates[j])
-                path.pop()
+            curr.append(candidates[i])
+            backtrack(i, curr, total+candidates[i])
+            curr.pop()
+            backtrack(i+1, curr, total)
 
         backtrack(0, [], 0)
-        return solution
-
+        return result
