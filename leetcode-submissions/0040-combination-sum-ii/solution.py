b5da8e5c228d0a1e408ef9da9700b307a1e6dc56
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-02 22:19:24 +0000

    [LeetCode Sync] Sandrin - Combination Sum II - Runtime - 31 ms (66.22%), Memory - 12.5 MB (38.57%)

--- leetcode-submissions/0040-combination-sum-ii/solution.py
+++ leetcode-submissions/0040-combination-sum-ii/solution.py
@@ -1,27 +1,39 @@
-class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        # Sort the candidates list
+        # Edge case if the index exceeds or reaches the end of the list return 
+        # Identify the result list 
+        # If the sum of the current has been reached add to the result
+        # Create a prev number to skip any repeating values and prevent identical siblings
+        # Create a for loop to create our tree Staring from current position
+        # If the the current value equals previous continue
+        # Recursively call the function
+        # After backtracking pop from curr
+        # Store the prev as current value
 
-        n = len(candidates)
         candidates.sort()
-        solution = []
+        result = []
+        def backtrack(i, curr, total):
+            if total == target:
+                result.append(curr[:])
+                return 
 
-        def backtrack(i, path, curr_sum):
-            if curr_sum == target:
-                solution.append(path[:])
-                return
-            
-            if curr_sum > target:
+            if total > target or i >= len(candidates):
                 return
 
-            for j in range(i, n):
-                
-                if j > i and candidates[j] == candidates[j - 1]:
+            prev = -1
+            for i in range(i, len(candidates)):
+                if prev == candidates[i]:
                     continue
-                 
-                path.append(candidates[j])
-                backtrack(j + 1, path, curr_sum + candidates[j])
-                path.pop()  
+                curr.append(candidates[i])
+                backtrack(i+1, curr, total + candidates[i])
+                curr.pop()
+                prev = candidates[i]
 
-        backtrack(0, [], 0) 
-        return solution
-
+        backtrack(0, [], 0)
+        return result
