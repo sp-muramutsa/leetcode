44994cade6aa2f537b67c9eb1be2c644561bfaa5
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-02 03:06:46 +0000

    [LeetCode Sync] Sandrin - Search Suggestions System - Runtime - 14 ms (66.62%), Memory - 20.5 MB (75.31%)

--- leetcode-submissions/1397-search-suggestions-system/solution.py
+++ leetcode-submissions/1397-search-suggestions-system/solution.py
@@ -1,42 +1,31 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
 
-        # cut the searchWord into prefixes
-        prefixes = []
-        word_length = len(searchWord)
-        i = 1
-        while i < word_length + 1:
-            prefixes.append(searchWord[:i])
-            i += 1
-        
-        
-        # for each prefix: create a list of related words
-        search_suggestion = []
-        for prefix in prefixes:
-            prefix_length = len(prefix)
-            prefix_product = []
-            for product in products:
-                if product[:prefix_length] == prefix:
-                    prefix_product.append(product)
+        products.sort()
 
-            # sort lexicographically
-            prefix_product = sorted(prefix_product)
+        n = len(searchWord)
+        m = len(products)
+        suggestions = []
 
-            # return at most the top three words
-            try:
-                search_suggestion.append(prefix_product[:3])
-            except IndexError:
-                search_suggestion.append(prefix_product)
-            
-        return search_suggestion
+        for i in range(n):
+            prefix = searchWord[: i + 1]
+            top_3 = []
 
+            l, r = 0, m
 
+            while l < r:
+                mid = l + (r - l) // 2
+                if products[mid] < prefix:
+                    l = mid + 1
+                else:
+                    r = mid
 
-        
-        
-            
-                    
+            end = min(l + 3, m)
+            for i in range(l, end):
+                if products[i].startswith(prefix):
+                    top_3.append(products[i])
 
+            suggestions.append(top_3)
 
-    
-        
+        return suggestions
+
