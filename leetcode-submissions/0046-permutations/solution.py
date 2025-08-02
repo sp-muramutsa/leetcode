35e89ebe23887466c8406b0b207f2421d6c5cef5
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-02 23:44:25 +0000

    [LeetCode Sync] Sandrin - Permutations - Runtime - 0 ms (100.00%), Memory - 18.2 MB (16.74%)

--- leetcode-submissions/0046-permutations/solution.py
+++ leetcode-submissions/0046-permutations/solution.py
@@ -1,22 +1,32 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        n = len(nums)
-        solution= []
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # Edge case if the length of curr is equal to the lenght of nums add curr to result and return
+        # Create a for loop 
+        # If the number is not eqaul to the current number and not in curr list
+        # Add it to curr and reculsively call the function
+        # After backtracking pop from curr and 
+        # return 
+        result = []
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                result.append(curr[:])
+                return 
 
-        def backtrack(nums_used, path):
-            if nums_used == n:
-                solution.append(path[:])
-                return
-
-            for j in range(n):
-                if nums[j] in path:
+            for i in range(len(nums)):
+                if nums[i] in curr:
                     continue
-                
-                path.append(nums[j])
-                backtrack(nums_used + 1, path)
-                path.pop()
 
-        backtrack(0, [])
-        return solution
+                curr.append(nums[i])
+                backtrack(curr)
+                curr.pop()
 
+
+            return 
+
+        backtrack([])
+        return result
