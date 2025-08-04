085c972ef59bbbc5f782fce417f41c96358364a6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-04 20:56:47 +0000

    [LeetCode Sync] Sandrin - Subsets II - Runtime - 3 ms (25.60%), Memory - 18.2 MB (15.62%)

--- leetcode-submissions/0090-subsets-ii/solution.py
+++ leetcode-submissions/0090-subsets-ii/solution.py
@@ -1,26 +1,20 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-
-        n = len(nums)
-        solution, path = [], []
         nums.sort()
+        length = len(nums)
+        result = []
+        def backtrack(starting_index, path):
+            result.append(path.copy())
 
-        def backtrack(i, path):
-            print(i, path)
-            solution.append(path[:])
-            if i == n:
+            if starting_index == length:
                 return
 
-            for j in range(i + 1, n):
-
-                # Prune for duplicates
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for i in range(starting_index, length):
+                if i > starting_index and nums[i] == nums[i - 1]:
                     continue
-
-                path.append(nums[j])
-                backtrack(j, path)
+                path.append(nums[i])
+                backtrack(i+1, path)
                 path.pop()
 
-        backtrack(-1, path)
-        return solution
-
+        backtrack(0, [])
+        return result
