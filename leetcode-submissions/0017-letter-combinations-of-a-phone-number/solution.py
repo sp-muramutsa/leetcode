1b6fdfd306b900505d3874608154a8f8e59aaa50
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-04 23:49:25 +0000

    [LeetCode Sync] Sandrin - Letter Combinations of a Phone Number - Runtime - 0 ms (100.00%), Memory - 17.8 MB (42.47%)

--- leetcode-submissions/0017-letter-combinations-of-a-phone-number/solution.py
+++ leetcode-submissions/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,5 +1,14 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # Create a hashmap that stores list of all possible letters
+        # Create the result variable
+        # Identify the dfs function 
+        # If the Starting_index has reached the end add the path to the result
+        # Create a for loop for all possible paths at current index
+        # Add them to the path and recursively call the function
+        # After backtracking pop from the list
+        if len(digits) == 0:
+            return []
         hashmap = {
             "1": "",
             "2": "abc",
@@ -11,22 +20,17 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
-
-        solution = []
-        k = len(digits)
-        if k == 0:
-            return []
-
-        def backtrack(i, path):
-            if i == k:
-                solution.append("".join(path))
+        result = []
+        n = len(digits)
+        def dfs(starting_index, path):
+            if starting_index == n:
+                result.append("".join(path.copy()))
                 return
 
-            for char in hashmap[digits[i]]:
-                path.append(char)
-                backtrack(i + 1, path)
+            for i in hashmap[digits[starting_index]]:
+                path.append(i)
+                dfs(starting_index+1, path)
                 path.pop()
-
-        backtrack(0, [])
-        return solution
-
+        
+        dfs(0, [])
+        return result
