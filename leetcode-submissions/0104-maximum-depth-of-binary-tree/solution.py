57c41dba2d03f21437785dc62e0472520b7549dd
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-10 14:39:14 +0000

    [LeetCode Sync] Sandrin - Maximum Depth of Binary Tree - Runtime - 0 ms (100.00%), Memory - 19.2 MB (29.91%)

--- leetcode-submissions/0104-maximum-depth-of-binary-tree/solution.py
+++ leetcode-submissions/0104-maximum-depth-of-binary-tree/solution.py
@@ -4,26 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-        if not root:
+        # Edge case if root is None return 0
+        if root is None:
             return 0
-
-        stack = [root]
-        depth = 0
-        curr = root
-
-        while stack:
-
-            level = []
-            for node in stack:
-                if node.left:
-                    level.append(node.left)
-                if node.right:
-                    level.append(node.right)
-            stack = level
-            depth += 1
-
-        return depth
-
+        # Using BFS 
+        length = 0
+        # Identify the BFS function
+        def BFS(root):
+            # Identify your queue
+            queue = deque()
+            queue.append(root)
+            # Identify your length
+            length = 0
+            # While the queue is not empty
+            while queue:
+                l = len(queue)
+                # Update the length
+                length += 1
+                # Pop from the queue and add the children if available
+                for i in range(l):
+                    node = queue.popleft()
+                    if node.right is not None:
+                        queue.append(node.right)
+                    if node.left is not None:
+                        queue.append(node.left)
+            # Return the length
+            return length
+        return BFS(root)
