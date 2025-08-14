74458bf674dfb6e2dfc975ff0ab0338b6be98cee
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-14 07:37:23 +0000

    [LeetCode Sync] Sandrin - Walls and Gates - Runtime - 71 ms (26.40%), Memory - 25 MB (24.94%)

--- leetcode-submissions/0286-walls-and-gates/solution.py
+++ leetcode-submissions/0286-walls-and-gates/solution.py
@@ -1,26 +1,45 @@
+from collections import deque
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
+        # Keep a queue with the gates
+        # While queue
+        # Get the neighbors 
+        # Increament the current level
+        # Add them to the visited data structure
+        queue = deque()
+        visited = set()
 
-        # Multi-source shortest path bfs. Start from the gate. Move inwards into the rooms.
-        m, n = len(rooms), len(rooms[0])
-        q = deque()
-        for r in range(m):
-            for c in range(n):
-                if rooms[r][c] == 0:
-                    q.append((0, r, c))
+        def get_neighbors(grid, position):
+            new_row = [1, 0, -1, 0]
+            new_col = [0, 1, 0, -1]
+            moves = []
+            for i in range(4):
+                r = position[0] + new_row[i]
+                c = position[1] + new_col[i]
 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] != -1 and grid[r][c] != 0:
+                    moves.append((r, c))
 
-        while q:
+            return moves
 
-            dist, row, col = q.popleft()
-
-            for dx, dy in directions:
-                x, y = row + dx, col + dy
-                if 0 <= x < m and 0 <= y < n and rooms[x][y] == 2147483647:
-                    rooms[x][y] = dist + 1
-                    q.append((dist + 1, x, y))
-
+        for i in range(len(rooms)):
+            for j in range(len(rooms[0])):
+                if rooms[i][j] == 0:
+                    queue.append((i, j))
+                    visited.add((i, j))
+         
+        distance = 0
+        while queue:
+            l = len(queue)
+            for _ in range(l):
+                n = queue.popleft()
+                rooms[n[0]][n[1]] = distance
+                for p in get_neighbors(rooms, n):
+                    if p in visited:
+                        continue
+                    visited.add((p[0], p[1]))
+                    queue.append((p[0], p[1]))
+            distance += 1
