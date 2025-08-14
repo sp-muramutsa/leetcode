2bfd8488a7966c3fe264d8ca20066403f1977e9e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-14 02:52:31 +0000

    [LeetCode Sync] Sandrin - Find the Town Judge - Runtime - 0 ms (100.00%), Memory - 20.7 MB (95.00%)

--- leetcode-submissions/1039-find-the-town-judge/solution.py
+++ leetcode-submissions/1039-find-the-town-judge/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
 
-        trust_counts = [0] * (n + 1)  # Initialize trust counts for each person
-        
+        trusted_people = [[] for _ in range(n + 1)]
+        trusted_count = {i: 0 for i in range(n + 1)}
+
         for a, b in trust:
-            trust_counts[a] -= 1  # Decrease trust count for person a
-            trust_counts[b] += 1  # Increase trust count for person b
+            trusted_people[a].append(b)
+            trusted_count[b] += 1
 
-        for i in range(1, n + 1):
-            if trust_counts[i] == n - 1:  # Check if person i is trusted by everyone except themselves
-                return i
+        for person in range(1, n + 1):
+            if not trusted_people[person] and trusted_count[person] == n - 1:
+                return person
 
-        return -1  # No judge found
+        return -1
 
-            
-        
-        
-        
