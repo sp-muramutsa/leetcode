c97d40786994d0c78b307114ce762380ebfc76d0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-16 23:52:37 +0000

    [LeetCode Sync] Sandrin - Meeting Rooms II - Runtime - 3 ms (87.16%), Memory - 19.8 MB (76.81%)

--- leetcode-submissions/0253-meeting-rooms-ii/solution.py
+++ leetcode-submissions/0253-meeting-rooms-ii/solution.py
@@ -1,18 +1,13 @@
-import heapq
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 
-        n = len(intervals)
-        intervals.sort() # Sort by start time.
+        intervals.sort()
         heap = [intervals[0][1]]
 
-        for i in range(1, n):      
-            if intervals[i][0] >= heap[0]:
+        for start, end in intervals[1:]:
+            if start >= heap[0]:
                 heapq.heappop(heap)
-            heapq.heappush(heap, intervals[i][1])        
-            
-        return len(heap)
+            heapq.heappush(heap, end)
 
-       
+        return len(heap)
 
