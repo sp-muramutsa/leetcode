4674d9fd86a8d290e1e62f77f6847dbc19482e07
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-19 02:29:04 +0000

    [LeetCode Sync] Sandrin - House Robber - Runtime - 0 ms (100.00%), Memory - 17.9 MB (33.91%)

--- leetcode-submissions/0198-house-robber/solution.py
+++ leetcode-submissions/0198-house-robber/solution.py
@@ -2,16 +2,17 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
 
         n = len(nums)
+
         if n == 1:
             return nums[0]
 
         prev = nums[0]
-        curr = max(prev, nums[1])
+        curr = max(nums[0], nums[1])
+        res = curr
 
         for i in range(2, n):
-            best = max(curr, nums[i] + prev)
+            res = max(nums[i] + prev, curr)
+            prev, curr = curr, res
 
-            prev, curr = curr, best
-
-        return curr
+        return res
 
