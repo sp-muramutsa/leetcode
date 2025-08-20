a866ca865e1f0b5350cea3ee3e2d31bd5bef8aa2
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-20 01:52:26 +0000

    [LeetCode Sync] Sandrin - Car Fleet - Runtime - 114 ms (98.03%), Memory - 38.4 MB (65.59%)

--- leetcode-submissions/0883-car-fleet/solution.py
+++ leetcode-submissions/0883-car-fleet/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = zip(position, speed)
+        position = sorted(cars, key=lambda x: x[0], reverse=True)
 
-        cars = sorted(zip(position, speed), reverse=True)
-        arrival_times = [(target - pos) / speed for pos, speed in cars]
 
         stack = []
 
-        for t in arrival_times:
-            stack.append(t)
+        for d, s in position:
+            time = (target - d) / s
+            stack.append(time)
 
             if len(stack) >= 2 and stack[-2] >= stack[-1]:
                 stack.pop()
 
         return len(stack)
-
