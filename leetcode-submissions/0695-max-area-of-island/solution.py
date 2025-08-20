f082d113402557469f5d76de05f283021d525ff4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-20 19:55:36 +0000

    [LeetCode Sync] Sandrin - Max Area of Island - Runtime - 38 ms (15.25%), Memory - 18 MB (87.45%)

--- leetcode-submissions/0695-max-area-of-island/solution.py
+++ leetcode-submissions/0695-max-area-of-island/solution.py
@@ -1,65 +1,60 @@
-class UnionFind:
-    def __init__(self, grid: List[List[int]], nodes: List[tuple]) -> None:
-        self.parents = {}
-        self.sizes = {}
-        self.grid = grid
-
-        for node in nodes:
-            print(node)
-            self.parents[node] = node
-
-            r, c = node
-            if self.grid[r][c] == 1:
-                self.sizes[node] = 1
-            else:
-                self.sizes[node] = 0
-
-    def find(self, child: tuple) -> tuple:
-        """
-        Find with path compression
-        """
-
-        parent = self.parents[child]
-        if parent != child:
-            self.parents[child] = self.find(parent)
-
-        return self.parents[child]
-
-    def union(self, node1: tuple, node2: tuple) -> None:
-        """
-        Union with size heuristic
-        """
-
-        parent1 = self.find(node1)
-        parent2 = self.find(node2)
-
-        if parent1 == parent2:
-            return
-
-        if self.sizes[parent1] > self.sizes[parent2]:
-            self.parents[parent2] = parent1
-            self.sizes[parent1] += self.sizes[parent2]
-        else:
-            self.parents[parent1] = parent2
-            self.sizes[parent2] += self.sizes[parent1]
-
-
+from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # Using BFS we will iterate over each island
+        # Create a get neighbor function
+        # Create a result variable
+        # BFS inplementation
+        # Iterate through each value
+        # Create a visited data structure
+        # If the value is 0 skip
+        # Create a queue
+        # Create a temp area
+        # Add the specific cell
+        # While queue
+        # Pop from the queue
+        # Get the neighbors
+        # If the neighbor is in visited we skip
+        # Increament the temp value and store the cell in visited
+        # Update the result with temp area
+        # Return result
 
-        m, n = len(grid), len(grid[0])
+        def get_neighbor(cell, grid):
+            new_row = [1, 0, -1, 0]
+            new_col = [0, 1, 0, -1]
+            moves = []
 
-        cells = [(row, col) for row in range(m) for col in range(n)]
-        uf = UnionFind(grid, cells)
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for i in range(4):
+                r = cell[0] + new_row[i]
+                c = cell[1] + new_col[i]
 
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1:
-                    for dx, dy in directions:
-                        x, y = row + dx, col + dy
-                        if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
-                            uf.union((row, col), (x, y))
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 1:
+                    moves.append((r, c))
+            return moves
 
-        return max(uf.sizes.values())
+        def bfs(grid):
+            visited = set()
+            res = 0
+            for i in range(len(grid)):
+                for j in range(len(grid[0])):
+                    if grid[i][j] == 0 or (i, j) in visited:
+                        continue
+
+                    queue = deque([(i, j)])
+                    tmp = 1
+                    visited.add((i, j))
+                    while queue:
+                        n = queue.popleft()
+                        for neighbor in get_neighbor((n[0], n[1]), grid):
+                            if (neighbor[0], neighbor[1]) in visited:
+                                continue
+                            visited.add((neighbor[0], neighbor[1]))
+                            queue.append((neighbor[0], neighbor[1]))
+                            tmp += 1
+
+                    res = max(res, tmp)
+
+            return res
+
+        return bfs(grid)
 
