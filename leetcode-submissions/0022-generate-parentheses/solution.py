7acdab7e311dc165bdee47616294faa9c3faecf2
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-20 00:54:27 +0000

    [LeetCode Sync] Sandrin - Generate Parentheses - Runtime - 3 ms (29.53%), Memory - 18.2 MB (25.46%)

--- leetcode-submissions/0022-generate-parentheses/solution.py
+++ leetcode-submissions/0022-generate-parentheses/solution.py
@@ -1,27 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # We have two options either we add ( or we add )
-        # If the end of the path is reached we add the path to the result
-        # If the opening is less than the required opening
-        # Add the ( to path and dfs
-        # Backtrack and pop
-        # If the closing is less than the expeced closing 
-        # dfs backtracka and pop
         result = []
-        def dfs(starting_index, closing, opening, path):
-            if starting_index == 2 * n:
+        def backtrack(path, starting_index):
+            if starting_index / 2 == n:
                 result.append("".join(path.copy()))
                 return
 
-            if opening < n:
-                path.append('(')
-                dfs(starting_index+1, closing, opening+1, path)
+            if path.count("(") < n:
+                path.append("(")
+                backtrack(path, starting_index+1)
                 path.pop()
 
-            if closing < opening:
-                path.append(')')
-                dfs(starting_index+1, closing+1, opening, path)
+            if path.count(")") < path.count("("):
+                path.append(")")
+                backtrack(path, starting_index+1)
                 path.pop()
-
-        dfs(0, 0, 0, [])
+        backtrack([], 0)
         return result
