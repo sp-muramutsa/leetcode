55066c569aef3733082ea0f90b23ae5873529b15
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-21 21:41:12 +0000

    [LeetCode Sync] Sandrin - Redundant Connection - Runtime - 4 ms (31.13%), Memory - 18.1 MB (77.00%)

--- leetcode-submissions/0684-redundant-connection/solution.py
+++ leetcode-submissions/0684-redundant-connection/solution.py
@@ -1,51 +1,36 @@
-class UnionFind:
-    def __init__(self, n: int):
-        self.parents = {i: i for i in range(1, n + 1)}
-        self.sizes = {j: 1 for j in range(1, n + 1)}
-
-    def find(self, node: int) -> int:
-        """
-        Find with path compression
-        """
-
-        parent = self.parents[node]
-        if parent != node:
-            self.parents[node] = self.find(parent)
-
-        return self.parents[node]
-
-    def union(self, node1: int, node2: int) -> Optional[List[int]]:
-        """
-        Union with size heuristic
-        """
-
-        parent1 = self.find(node1)
-        parent2 = self.find(node2)
-
-        # Connected component
-        if parent1 == parent2:
-            return [node1, node2]
-
-        # Otherwise, the component is unconnected. Just union.
-        if self.sizes[parent1] > self.sizes[parent2]:
-            self.parents[parent2] = parent1
-            self.sizes[parent1] += self.sizes[parent2]
-        else:
-            self.parents[parent1] = parent2
-            self.sizes[parent2] += self.sizes[parent1]
-
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-
+        # Get the number of nodes
         n = len(edges)
-        uf = UnionFind(n)
-        redundant_connection = []
+        parent = [i for i in range(n+1)]
+        rank = [1] * (n+1)
 
-        for src, dest in edges:
-            temp = uf.union(src, dest)
-            if temp:
-                redundant_connection = temp
+        # Find
+        def find(n):
+            if parent[n] == n:
+                return n
 
-        return redundant_connection
+            return find(parent[n])
 
+        # Union
+        def union(n1, n2):
+            e1 = find(n1)
+            e2 = find(n2)
+
+            if e1 == e2:
+                return False
+
+            rank[e2] += 1
+            if rank[e1] > rank[e2]:
+                parent[e2] = e1
+                rank[e1] += rank[e2]
+
+            else:
+                parent[e1] = e2
+                rank[e2] += rank[e1]
+
+            return True
+
+        for edge in edges:
+            if not union(edge[0], edge[1]):
+                return edge
