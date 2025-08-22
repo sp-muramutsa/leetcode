999396db105ef5b54e745f00dcea1d770e8ee3f8
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-22 04:17:24 +0000

    [LeetCode Sync] Sandrin - Product of Array Except Self - Runtime - 27 ms (46.24%), Memory - 25 MB (36.32%)

--- leetcode-submissions/0238-product-of-array-except-self/solution.py
+++ leetcode-submissions/0238-product-of-array-except-self/solution.py
@@ -1,28 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
-        # Input: nums = [1,2,3,4]
-        # Output: [24,12,8,6]
-
-        # L: [1, 1, 2, 6]
-        # R: [24, 12, 4, 1]
-        # O: [24, 12, 8, 6]
-
-        n = len(nums)
-        L = [1] * n
-        L[0] = 1
-
-        for i in range(1, n):
-            L[i] = nums[i - 1] * L[i - 1]
-
-        R = [1] * n
-        R[n - 1] = 1
-
-        for j in range(n - 2, -1, -1):
-            print(j, j + 1)
-            R[j] = nums[j + 1] * R[j + 1]
-
-        result = [x * y for (x, y) in zip(L, R)]
-
-        return result
-
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
+        
+        # Iterate from the first element and then multiply from the front
+        pre = 1
+        for i in range(1, len(nums)):
+            pre *= nums[i-1]
+            prefix[i] = pre
+            
+        su = 1
+        for i in range(len(nums)-2, -1, -1):
+            su *= nums[i+1]
+            suffix[i] = su
+            
+        for i in range(len(suffix)):
+            prefix[i] = prefix[i] * suffix[i]
+            
+        return prefix
