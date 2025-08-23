a1e7e727b0616f60a7e7de53dfa02d4453ec02b7
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-23 17:16:25 +0000

    [LeetCode Sync] Sandrin - Binary Tree Inorder Traversal - Runtime - 0 ms (100.00%), Memory - 17.9 MB (12.40%)

--- leetcode-submissions/0094-binary-tree-inorder-traversal/solution.py
+++ leetcode-submissions/0094-binary-tree-inorder-traversal/solution.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        p = root
         result = []
+        while p or stack:
+            while p:
+                stack.append(p)
+                p = p.left
+            n = stack.pop()
+            result.append(n.val)
+            p = n.right
 
-        def recursive(root):
-            if root is None:
-                return
-            recursive(root.left)
-            result.append(root.val)
-            recursive(root.right)
-
-        recursive(root)
         return result
