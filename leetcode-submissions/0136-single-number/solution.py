82c8dff3039bd5dd33423a56ca07c23698cc138f
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-08-24 06:26:35 +0000

    [LeetCode Sync] Sandrin - Single Number - Runtime - 7 ms (31.30%), Memory - 19.6 MB (76.77%)

--- leetcode-submissions/0136-single-number/solution.py
+++ leetcode-submissions/0136-single-number/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashmap = {}
-        for i in nums:
-            hashmap[i] = hashmap.get(i, 0) + 1
-        for key, item in hashmap.items():
-            if item == 1:
-                return key
+        
+        a = 0
+        for x in nums:
+            a ^= x
+        return a
         
