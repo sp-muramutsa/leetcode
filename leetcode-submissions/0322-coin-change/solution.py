203f71adf86199edda83a5650fb7402505208097
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-02 03:37:10 +0000

    [LeetCode Sync] Sandrin - Coin Change - Runtime - 780 ms (47.75%), Memory - 18.4 MB (43.03%)

---
+++ leetcode-submissions/0322-coin-change/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+
+        for curr_amount in range(1, amount + 1):
+            for coin in coins:
+                if curr_amount - coin < 0:
+                    continue
+                dp[curr_amount] = min(dp[curr_amount], 1 + dp[curr_amount - coin])
+
+        return dp[amount] if dp[amount] != float("inf") else -1
+
