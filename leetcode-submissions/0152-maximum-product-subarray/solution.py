01e9cb545c6335617ef5366f686735f48debcac6
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-03 00:14:09 +0000

    [LeetCode Sync] Sandrin - Maximum Product Subarray - Runtime - 11 ms (49.17%), Memory - 18.2 MB (96.49%)

--- leetcode-submissions/0152-maximum-product-subarray/solution.py
+++ leetcode-submissions/0152-maximum-product-subarray/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        negative, positive, ans = nums[0], nums[0], nums[0]
-        length = len(nums)
-        for i in range(1, length):
-            temp = max(nums[i], positive * nums[i], negative * nums[i])
-            negative = min(nums[i], negative*nums[i], positive*nums[i])
-            positive = temp
-            ans = max(ans, positive)
 
-        return ans
-            
+        minn = maxx = max_product = nums[0]
+        n = len(nums)
+
+        for i in range(1, n):
+            x = nums[i]
+            prev_max, prev_min = maxx, minn
+            maxx = max(x, prev_max * x, prev_min * x)
+            minn = min(x, prev_max * x, prev_min * x)
+            max_product = max(maxx, max_product)
+
+        return max_product
 
