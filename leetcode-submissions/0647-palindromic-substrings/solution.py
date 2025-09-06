c7702daf1ca83de2add5569ddbe923f046b8c96a
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-06 04:50:20 +0000

    [LeetCode Sync] Sandrin - Palindromic Substrings - Runtime - 104 ms (91.69%), Memory - 17.7 MB (66.52%)

--- leetcode-submissions/0647-palindromic-substrings/solution.py
+++ leetcode-submissions/0647-palindromic-substrings/solution.py
@@ -1,36 +1,29 @@
-from typing import List
-
-
 class Solution:
-
-    def manacher_radii(self, s: str) -> List[int]:
-
-        t = "^#" + "#".join(s) + "#$"
-        n = len(t)
-        radii = [0] * n
-
-        center, radius = 0, 0
-
-        for right in range(1, n - 1):
-            if radius > right:
-                mirror = 2 * center - right
-                radii[right] = min(radius - right, radii[mirror])
-
-            while t[right + (1 + radii[right])] == t[right - (1 + radii[right])]:
-                radii[right] += 1
-
-            if right + radii[right] > radius:
-                center, radius = right, right + radii[right]
-
-        return radii
-
     def countSubstrings(self, s: str) -> int:
 
-        radii = self.manacher_radii(s)
+        # 3: Expanding from the centers: O(n ** 2) time and O(n) space
 
-        palindromes = 0
-        for radius in radii: 
-            palindromes += (radius + 1) // 2
-            
+        n = len(s)
+        palindromes = n
+
+        for c in range(n):
+            l, r = c - 1, c + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                palindromes += 1
+                l -= 1
+                r += 1
+
+        for c1 in range(n - 1):
+            c2 = c1 + 1
+            if s[c1] == s[c2]:
+                palindromes += 1
+
+                l, r = c1 - 1, c2 + 1
+
+                while l >= 0 and r < n and s[l] == s[r]:
+                    palindromes += 1
+                    l -= 1
+                    r += 1
+
         return palindromes
 
