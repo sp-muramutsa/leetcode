dadcac26938c82bfcbc2c814304c69bf38e8f219
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-07 11:08:08 +0000

    [LeetCode Sync] Sandrin - Permutations - Runtime - 4 ms (11.77%), Memory - 17.8 MB (82.41%)

--- leetcode-submissions/0046-permutations/solution.py
+++ leetcode-submissions/0046-permutations/solution.py
@@ -1,29 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # Create a list for visited
-        # Create a result list
-        # Define the dfs function
-        # If the length of the path is length of the nums add the path to result
-        # Create a for loop for possible decisions
-        # If the current number is not visited
-        # Turn it to visited
-        # Dfs 
-        # After backtracking pop from the path
-        # Turn visited index back to False
-        visited_decisions = [False] * len(nums)
-        result = []
-        length = len(nums)
+        # Create a res variable
+        res = []
+        # Implement a dfs function
         def dfs(path):
-            if len(path) == length:
-                result.append(path.copy())
-                return
+            # Edge case if the length of the path is the same as the length of the nums add to result
+            if len(path) == len(nums):
+                res.append(path.copy())
+            # Create a for loop for all nums
+            for i in range(len(nums)):
+                # If current number is in path skip it
+                if nums[i] in path:
+                    continue
+                # After backtracking pop from the path
+                path.append(nums[i])
+                dfs(path)
+                path.pop()
 
-            for i, n in enumerate(nums):
-                if visited_decisions[i] == False:
-                    visited_decisions[i] = True
-                    path.append(n)
-                    dfs(path)
-                    visited_decisions[i] = False
-                    path.pop()
         dfs([])
-        return result
+        return res
