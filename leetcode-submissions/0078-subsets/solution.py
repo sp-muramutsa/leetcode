20f49bf409790cfb4818d4de393d8d48cc1d5d58
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-07 10:29:25 +0000

    [LeetCode Sync] Sandrin - Subsets - Runtime - 0 ms (100.00%), Memory - 18.1 MB (21.68%)

--- leetcode-submissions/0078-subsets/solution.py
+++ leetcode-submissions/0078-subsets/solution.py
@@ -1,19 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # If the counter reaches the kength of the list add result to result list
-        # Add the number to the lsit and keep running the function
-        # After the function backtracks skip the current number and go to the next
-        # Return the result
-        def backtrack(num, res, nums, curr):
-            if num == len(nums):
-                res.append(curr.copy())
-                return 
+        res = []
 
-            curr.append(nums[num])
-            backtrack(num+1, res, nums, curr)
-            curr.pop()
-            backtrack(num+1, res, nums, curr)
+        def dfs(path, starting_index):
+            res.append(path.copy())
 
-        result = []
-        backtrack(0, result, nums, [])
-        return result
+            for i in range(starting_index, len(nums)):
+                path.append(nums[i])
+                dfs(path, i+1)
+                path.pop()
+
+        dfs([], 0)
+        return res
