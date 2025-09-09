8abf537bf6a04b069e53f00cf0c58654772b3d76
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-09 22:30:00 +0000

    [LeetCode Sync] Sandrin - Decode Ways - Runtime - 0 ms (100.00%), Memory - 17.8 MB (68.76%)

--- leetcode-submissions/0091-decode-ways/solution.py
+++ leetcode-submissions/0091-decode-ways/solution.py
@@ -1,26 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        if s[0] == "0":
-            return 0
-
         n = len(s)
-        one_digit = 1
         two_digits = 1
+        one_digit = 1 if s[0] != "0" else 0
 
-        # string ending at i
         for i in range(1, n):
 
+            # One digit can't be decoded if it is 0
             temp = 0
-            # Single character
             if s[i] != "0":
-                temp = one_digit
+                temp += one_digit
 
-            # Check if we can decode using a double-digit
-            if 0 < int(s[i-1]) <= 2 and 0 < int(s[i - 1: i + 1]) <= 26:
+            # 1 <= x <= 26
+            if s[i - 1] != "0" and int(s[i - 1 : i + 1]) <= 26:
                 temp += two_digits
 
             two_digits = one_digit
             one_digit = temp
 
         return one_digit
+
