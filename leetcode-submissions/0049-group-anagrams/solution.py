68d2119827e7621bf41728b79923895bb9c9f21b
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-09 17:48:30 +0000

    [LeetCode Sync] Sandrin - Group Anagrams - Runtime - 27 ms (11.30%), Memory - 22.7 MB (11.80%)

--- leetcode-submissions/0049-group-anagrams/solution.py
+++ leetcode-submissions/0049-group-anagrams/solution.py
@@ -1,13 +1,24 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Create a hashmap where key is the tuple of asci value of the word and value is a list of group anagrams
+        # Iterate over the list
+        # Create a key using the array oof 26 letters where each index represents count of a letter
+        # If the key is in hashmap add the string to the array 
+        hashmap = defaultdict(list)
+        def get_key(string):
+            chars = [0] * 26
+            for i in string:
+                num = ord(i) - ord("a")
+                chars[num] += 1
 
-        anagrams = defaultdict(list)
+            return tuple(chars)
 
-        for word in strs:
-            pattern = [0] * 26
-            for char in word:
-                pattern[ord(char) - ord("a")] += 1
-            anagrams[tuple(pattern)].append(word)
-
-        return list(anagrams.values())
+        for i in strs:
+            key = get_key(i)
+            hashmap[key].append(i)
+        res = []
+        for i in hashmap:
+            res.append(hashmap[i])
+        return res
 
