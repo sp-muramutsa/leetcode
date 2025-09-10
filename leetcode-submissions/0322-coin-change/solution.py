8c6aedc1136924ac1e0d6c8a84c09dcc9903c7aa
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-10 03:51:59 +0000

    [LeetCode Sync] Sandrin - Coin Change - Runtime - 291 ms (98.90%), Memory - 18.8 MB (32.89%)

--- leetcode-submissions/0322-coin-change/solution.py
+++ leetcode-submissions/0322-coin-change/solution.py
@@ -1,14 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
+        # BFS: Shortest path
 
-        for curr_amount in range(1, amount + 1):
+        q = deque([(0, 0)])
+        visited = set()
+
+        while q:
+
+            amount_so_far, coins_used = q.popleft()
+
+            if amount_so_far == amount:
+                return coins_used
+
             for coin in coins:
-                if curr_amount - coin < 0:
-                    continue
-                dp[curr_amount] = min(dp[curr_amount], 1 + dp[curr_amount - coin])
 
-        return dp[amount] if dp[amount] != float("inf") else -1
+                new_amount_so_far = amount_so_far + coin
+
+                if new_amount_so_far in visited or new_amount_so_far > amount:
+                    continue
+                
+                visited.add(new_amount_so_far)
+                q.append((new_amount_so_far, 1 + coins_used))
+
+        return -1
 
