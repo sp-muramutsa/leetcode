d318eb04b2e8639b53cdee2ee9c49f1a6badf529
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-14 13:04:43 +0000

    [LeetCode Sync] Sandrin - Basic Calculator II - Runtime - 63 ms (71.02%), Memory - 21.6 MB (60.09%)

--- leetcode-submissions/0227-basic-calculator-ii/solution.py
+++ leetcode-submissions/0227-basic-calculator-ii/solution.py
@@ -1,38 +1,38 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        """
-        Intuition: Store individual numbers, products, and quotients. Add them up at the end.
-        Dangers: enumerate, parse properly, do an operation at the end, truncate towards 0, update operation and current number
-        """
-        # Initialize a stack to store what to be summed
-        stack = []
-        current_number = 0
-        operation = '+'
-        s = s.replace(" ", "")  # Remove any spaces in the string
-
-        # Iterate over the string
-        for i, char in enumerate(s):
-            # Parse the digits into an int
-            if char.isdigit():
-                current_number = current_number * 10 + int(char)
-            
-            if not char.isdigit() or i == len(s) - 1: # You have to do an operation at the end
-                # + or -: append a number or its inverse
-                if operation == '+':
-                    stack.append(current_number)
-                elif operation == '-':
-                    stack.append(-current_number)
-
-                # * or /: multiply or divide what's at the top of the stack
-                elif operation == '*':
-                    stack[-1] = stack[-1] * current_number
-                elif operation == '/':
-                    # Perform integer division (truncated towards zero)
-                    stack[-1] = int(stack[-1] / current_number)
-                
-                # Update operation and reset current number
-                operation = char
-                current_number = 0
+        # Using the BODMAS
+        # Bracket Order Division Multiplication Addition Subtraction
+        #5
+        #[3]
+        s = s.replace(" ", "")
+        length = len(s)
+        if length == 0:
+            return 0
         
-        # Return the sum of all values in the stack
+        num = 0
+        i = 0
+        stack = []
+        op = '+'
+
+        while i < length:
+            curr = s[i]
+
+            if curr.isdigit():
+                num = num * 10 + ord(curr) - ord('0')
+
+            if (not curr.isdigit()) or i == length - 1:
+                if op == '+':
+                    stack.append(num)
+                elif op == '-':
+                    stack.append(-num)
+                elif op == '*':
+                    stack.append(stack.pop() * num)
+                elif op == '/':
+                    temp = stack.pop()
+                    stack.append(int(temp/num))
+                op = curr
+                num = 0
+            i += 1
         return sum(stack)
+            
+
