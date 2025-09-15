97a7d32b0d05d41ae6ee79fcc19bc7999825368e
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-15 11:35:33 +0000

    [LeetCode Sync] Sandrin - Minimum Path Sum - Runtime - 11 ms (91.07%), Memory - 20.1 MB (65.99%)

--- leetcode-submissions/0064-minimum-path-sum/solution.py
+++ leetcode-submissions/0064-minimum-path-sum/solution.py
@@ -1,20 +1,22 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        # First row
+        for i in range(1, ROWS):
+            grid[i][0] += grid[i - 1][0]
+        
+        # First column
+        for j in range(1, COLS):
+            grid[0][j] += grid[0][j - 1]
 
-        # Minimum path sum for the first row
-        for c in range(n):
-            dp[0][c] = dp[0][c - 1] + grid[0][c] if c else grid[0][c]
+        # Other cells
+        for row in range(1, ROWS):
+            for col in range(1, COLS):
+                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
+        
+     
+        return grid[-1][-1]
+                
 
-        # Minimum path sum for the first column
-        for r in range(m):
-            dp[r][0] = dp[r - 1][0] + grid[r][0] if r else grid[r][0]
-
-        # Calculate minimum path sum for other cells
-        for r in range(1, m):
-            for c in range(1, n):
-                dp[r][c] = min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c]
-
-        return dp[-1][-1]
