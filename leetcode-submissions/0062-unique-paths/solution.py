4f28f41aab50c2424e52797e7b6dbe5e5d788b3b
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-15 00:16:46 +0000

    [LeetCode Sync] Sandrin - Unique Paths - Runtime - 3 ms (21.64%), Memory - 17.9 MB (46.21%)

--- leetcode-submissions/0062-unique-paths/solution.py
+++ leetcode-submissions/0062-unique-paths/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # Using iterative method bottom-up we make our way to the destination
-        # We can only move in twoo ways which signals dp, also we are counting ways to reach the goal
-        # Create the Matrix with 0 inputs
-        dp = [[0 for i in range(n)] for j in range(m)]
-        # As the first row and first column only have one way to get rached on we mark them with 1
-        for i in range(n):
-            dp[0][i] = 1
 
-        for j in range(m):
-            dp[j][0] = 1
-        # Iterate over the cells 
-        # The other cells you find the number of ways to reach them by adding the upper cell and left cell paths
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
-        # Return the final cell
+        dp = [[0] * n for _ in range(m)]
+        directions = [(0, 1), (1, 0)]
+        dp[0][0] = 1
+
+        for row in range(m):
+            for col in range(n):
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if not (0 <= nr < m and 0 <= nc < n):
+                        continue
+                    dp[nr][nc] += dp[row][col]
+
         return dp[-1][-1]
 
-        #Time comlexity O(m*n)
-        # Space complexity O(m*n)
