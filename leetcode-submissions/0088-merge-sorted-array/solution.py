79354c84bc57ffcaf3efb666a25b831643e1b141
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-22 17:45:12 +0000

    [LeetCode Sync] Sandrin - Merge Sorted Array - Runtime - 0 ms (100.00%), Memory - 18 MB (26.21%)

--- leetcode-submissions/0088-merge-sorted-array/solution.py
+++ leetcode-submissions/0088-merge-sorted-array/solution.py
@@ -1,14 +1,11 @@
-from collections import deque
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Starting from position m nums1 
-        # Replace the values with nums2 starting from back to n
-        # Sort the list
-        start = len(nums2) - n
+        c = 0
         for i in range(m, len(nums1)):
-            nums1[i] = nums2[start]
-            start += 1
-        nums1.sort()
+            nums1[i] = nums2[c]
+            c += 1
+
+        return nums1.sort()
