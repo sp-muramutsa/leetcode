4e378cb5f45bba67f746ac6d8c236df6933df9a4
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-22 18:43:21 +0000

    [LeetCode Sync] Sandrin - Move Zeroes - Runtime - 11 ms (21.32%), Memory - 18.9 MB (49.66%)

--- leetcode-submissions/0283-move-zeroes/solution.py
+++ leetcode-submissions/0283-move-zeroes/solution.py
@@ -3,12 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        n = len(nums)
-        slow, fast = 0, 0
+        # [1,3,0,0,0,12]
 
-        while fast < n:
-            if nums[fast] != 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
-            fast += 1
+        # Create pointers left and right
+        # Edge case; length of array less than 2 return array
+        # Move until r is at the end
+        # If right is a number and left is 0 swap the two and shift both by 1
+        # If none of them is 0 move both by 1
+        # If both are 0 shift just the right
+
+        l, r = 0, 1
+
+        while r < len(nums):
+            if nums[r] != 0 and nums[l] == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r += 1
+                continue
+            elif (nums[r] != 0 and nums[l] != 0) or (nums[r] == 0 and nums[l] != 0):
+                l += 1
+                r += 1
+                continue
+                
+            else:
+                r += 1
+
+        return nums
+
+        
