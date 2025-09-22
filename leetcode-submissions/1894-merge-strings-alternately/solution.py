6ec1621cee466557200e902dacd572a7fee726b2
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-22 08:46:18 +0000

    [LeetCode Sync] Sandrin - Merge Strings Alternately - Runtime - 42 ms (27.71%), Memory - 17.8 MB (33.72%)

--- leetcode-submissions/1894-merge-strings-alternately/solution.py
+++ leetcode-submissions/1894-merge-strings-alternately/solution.py
@@ -1,23 +1,33 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        """
-        """
+        # Create a result list 
+        # A count if odd add word if even add word2
+        # Iterate over the string if all indices aren't at the end
+        # Afer iterating over both strings, check if all pointers are at the end
+        # if not add the rest
+        res = []
+        count = 1
+        p1 = 0
+        p2 = 0
+        while p1 < len(word1) and p2 < len(word2):
+            if count % 2 != 0:
+                res.append(word1[p1])
+                p1 += 1
+                count += 1
+                continue
 
-        n1 = len(word1)
-        n2 = len(word2)
-        n = min(n1, n2)
-        merged_string = ""
-        a, b = 0, 0
+            if count % 2 == 0:
+                res.append(word2[p2])
+                p2 += 1
+                count += 1
+                continue
 
+        if p1 < len(word1):
+            for i in range(p1, len(word1)):
+                res.append(word1[i])
 
-        while a < n:
-            merged_string += word1[a] + word2[a] 
-            a += 1
+        if p2 < len(word2):
+            for j in range(p2, len(word2)):
+                res.append(word2[j])
 
-        difference = abs(n1 - n2)
-        if difference > 0:
-            merged_string += (word1[-difference:] if n1 > n2 else word2[-difference:])
-        return merged_string 
-
-
-
+        return "".join(res)
