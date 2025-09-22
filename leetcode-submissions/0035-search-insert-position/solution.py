d6d72c28bc35959b74c2c8a9069d8c5abd92e511
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-22 20:33:33 +0000

    [LeetCode Sync] Sandrin - Search Insert Position - Runtime - 0 ms (100.00%), Memory - 18.4 MB (73.29%)

--- leetcode-submissions/0035-search-insert-position/solution.py
+++ leetcode-submissions/0035-search-insert-position/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        lo, hi = 0, len(nums)
+        # Using binary search
+        # Perform binary search 
+        # If not found return the index it should be in
 
-        while lo < hi:
-            mid = lo + (hi - lo)//2
+        l, r = 0, len(nums)-1
 
-            if nums[mid] < target:
-                lo = mid + 1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
             else:
-                hi = mid
-        return lo
-        
-    
+                return mid
 
-
-        
+        return l
