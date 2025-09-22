1c9e7e64d96f5dc9fd51fec870fb1f32a36e9e65
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-22 19:44:18 +0000

    [LeetCode Sync] Sandrin - Remove Duplicates from Sorted Array - Runtime - 5 ms (17.21%), Memory - 18.9 MB (74.43%)

--- leetcode-submissions/0026-remove-duplicates-from-sorted-array/solution.py
+++ leetcode-submissions/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,22 +1,28 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # Create a left and right pointer
+        # [0,1,2,1,1,1,2,3,3,4]
+        # Starting from the second value yoouo will shift the right pointer until the value one step back from left is less than
+        # Swap the numbers
+        # Shif both left and right by 1
+        if len(nums) == 0:
+            return 0
 
-        n = len(nums)
 
-        if n == 1:
-            return
+        l, r = 1, 1
+        res = 0
 
-        l, r = 0, 1
-        curr = nums[0]
-
-        while r < n:
-            if nums[r] == curr:
-                r += 1
-            else:
-                nums[l + 1] = nums[r]
-                curr = nums[l + 1]
+        while r < len(nums):
+            if nums[l] <= nums[l-1]:
+                while r < len(nums) and nums[r] <= nums[l-1]:
+                    r += 1
+                
+                if r < len(nums):
+                    nums[l], nums[r] = nums[r], nums[l]
+            if r < len(nums):
                 l += 1
                 r += 1
 
-        return l + 1
+        return l
 
+        
