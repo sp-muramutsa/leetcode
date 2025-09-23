d46aad0c169b03c4cdea85cc4d88334732775209
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-23 15:38:46 +0000

    [LeetCode Sync] Sandrin - Add Two Numbers - Runtime - 1 ms (81.79%), Memory - 18 MB (14.59%)

--- leetcode-submissions/0002-add-two-numbers/solution.py
+++ leetcode-submissions/0002-add-two-numbers/solution.py
@@ -5,21 +5,34 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy node
+        # Itearate over both linked lists
+        # Keep a carrier
+        # Add the current values of both nodes if they exist, else add 0
+        # Update the carrier if necessary
+        # Move the pointers
         dummy = ListNode(0)
-        p1 = dummy
-        holder = 0
-        while l1 or l2:
-            d1 = l1.val if l1 else 0
-            d2 = l2.val if l2 else 0
-            total = d1 + d2 + holder
-            holder = total // 10
-            total = total % 10
-            p1.next = ListNode(total)
-            p1 = p1.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        if holder > 0:
-            p1.next = ListNode(holder)
-        
-        return dummy.next
+        p = dummy
+        p1 = l1
+        p2 = l2
+        carrier = 0
+        while p1 or p2:
+            n1 = p1.val if p1 else 0
+            n2 = p2.val if p2 else 0
+            tot = n1 + n2 + carrier
+            if tot >= 10:
+                stay = tot % 10 
+                carrier = tot // 10
+                p.next = ListNode(stay)
+                p = p.next
+            else:
+                carrier = 0
+                p.next = ListNode(tot)
+                p = p.next
+            p1 = p1.next if p1 else None
+            p2 = p2.next if p2 else None
+        if carrier != 0:
+            p.next = ListNode(carrier)
 
+        return dummy.next
+        
