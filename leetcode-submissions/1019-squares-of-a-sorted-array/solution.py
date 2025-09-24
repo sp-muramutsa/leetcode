cb2a65c70b421e665266410450797c2ae15d99a0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-24 18:51:27 +0000

    [LeetCode Sync] Sandrin - Squares of a Sorted Array - Runtime - 11 ms (51.90%), Memory - 19.6 MB (50.30%)

--- leetcode-submissions/1019-squares-of-a-sorted-array/solution.py
+++ leetcode-submissions/1019-squares-of-a-sorted-array/solution.py
@@ -1,29 +1,19 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        """
-        Two pointer approach
-        Time: O(n)
-        Space: O(n)
-        Intuition: Use two pointers. Comparing the two values at the extremes and determining which one will fill in a certain position.
-        """
-
-        length = len(nums)
-        result = [0] * length
-
-        l, r = 0, length - 1
-        i = r
-
-        while i >= 0:
-            l_square = nums[l] ** 2
-            r_square = nums[r] ** 2
-
-            if l_square > r_square:
-                result[i] = l_square
-                l += 1
+        i = 0
+        j = len(nums) - 1
+        new_list = [0] * len(nums)
+        
+        counter = len(new_list) - 1
+        
+        while i <= j :
+            if ((nums[j] * nums[j]) > (nums[i] *nums[i])):
+                new_list[counter] = (nums[j] * nums[j])
+                j -= 1
+                
             else:
-                result[i] = r_square
-                r -= 1
-
-            i -= 1
-        return result
- 
+                new_list[counter] = (nums[i] * nums[i])
+                i += 1
+            counter -= 1
+            
+        return new_list
