f09cbd400b92e5e4d0955a919161eab6e45445f8
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-27 14:37:34 +0000

    [LeetCode Sync] Sandrin - Perfect Squares - Runtime - 2627 ms (46.85%), Memory - 18.1 MB (49.60%)

--- leetcode-submissions/0279-perfect-squares/solution.py
+++ leetcode-submissions/0279-perfect-squares/solution.py
@@ -1,21 +1,16 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [i*i for i in range(1, int(math.sqrt(n)) + 1)]
-        cache = {}
+        # Create a dp array
+        # Each index represents minimum number of sq to add up to it
+        # Iterate over the array
+        # Find from the back the minimum number to add to it
+        dp = [n] * (n+1)
+        dp[0] = 0
 
-        def dfs(rem):
-            if rem == 0:
-                return 0
-            if rem in cache:
-                return cache[rem]
-
-            res = float("inf")
-            for i in squares:
-                if i <= rem:
-                    res = min(res, 1 + dfs(rem - i))
-
-            cache[rem] = res
-
-            return res
-
-        return dfs(n)
+        for trg in range(1, n+1):
+            for sq in range(1, trg+1):
+                square = sq*sq
+                if square > trg:
+                    break
+                dp[trg] = min(dp[trg], 1 + dp[trg-square])
+        return dp[-1]
