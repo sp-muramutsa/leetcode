4f475568235abc1a0dbfb055abdd6cbd142bfed5
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-27 19:03:44 +0000

    [LeetCode Sync] Sandrin - Palindrome Partitioning - Runtime - 50 ms (53.71%), Memory - 34.6 MB (19.61%)

--- leetcode-submissions/0131-palindrome-partitioning/solution.py
+++ leetcode-submissions/0131-palindrome-partitioning/solution.py
@@ -1,24 +1,19 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        # If the starting index has reached add the path and then end return
-        # Create a loop for the index
-        # If the prefix is not a palindrome continue
-        # If it is add to the path and operate dfs function
-        result = []
-        n = len(s)
-        def is_palindrome(l):
-            return l == l[::-1]
-        def dfs(starting_index, path):
-            if starting_index == n:
-                result.append(path.copy())
+        res = []
+
+        def dfs(idx, path):
+            if idx == len(s):
+                res.append(path.copy())
                 return
 
-            for i in range(starting_index+1, n+1):
-                prefix = s[starting_index:i]
-                if is_palindrome(prefix):
+            for i in range(idx, len(s)):
+                prefix = s[idx:i+1]
+                if prefix[:] == prefix[::-1]:
                     path.append(prefix)
-                    dfs(i, path)
+                    dfs(i+1, path)
                     path.pop()
-                
+
         dfs(0, [])
-        return result
+        return res
+
