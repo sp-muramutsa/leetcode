0cf4ae8d55d9e4e3df449b61450985e6eb0d9f44
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-29 06:21:10 +0000

    [LeetCode Sync] Sandrin - House Robber - Runtime - 0 ms (100.00%), Memory - 17.7 MB (53.95%)

--- leetcode-submissions/0198-house-robber/solution.py
+++ leetcode-submissions/0198-house-robber/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Create a dp of that store maximum money at position index
+        # Starting from 3rd last position update the index
+        # Skip adjacent index and update the sum of money with max 
+        # Return max money from dp array
 
-        n = len(nums)
+        dp = [n for n in nums]
 
-        if n == 1:
-            return nums[0]
+        for i in range(len(nums)-3, -1, -1):
+            n = dp[i]
+            for j in range(i+2, len(nums)):
+                n = max(n, (dp[i] + dp[j]))
+            dp[i] = n
 
-        prev = nums[0]
-        curr = max(nums[0], nums[1])
-        res = curr
-
-        for i in range(2, n):
-            res = max(nums[i] + prev, curr)
-            prev, curr = curr, res
-
-        return res
-
+        return max(dp)
