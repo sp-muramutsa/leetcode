512424d6a22466c0fbdb2eec2f6eb7a363301656
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-29 19:46:19 +0000

    [LeetCode Sync] Sandrin - Number of Islands - Runtime - 380 ms (5.06%), Memory - 35 MB (5.79%)

--- leetcode-submissions/0200-number-of-islands/solution.py
+++ leetcode-submissions/0200-number-of-islands/solution.py
@@ -1,35 +1,58 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        """
-        DFS
-        """
-
+class DSU:
+    def __init__(self, grid):
         ROWS, COLS = len(grid), len(grid[0])
-        visited = set()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        self.parents = {}
+        self.sizes = {}
+        self.num_islands = 0
 
-        def dfs_make_island(row, col):
-
-            visited.add((row, col))
-
-            for dr, dc in directions:
-                nr, nc = row + dr, col + dc
-
-                if not (0 <= nr < ROWS and 0 <= nc < COLS):
-                    continue
-
-                if (nr, nc) in visited:
-                    continue
-
-                if grid[nr][nc] == "1":
-                    dfs_make_island(nr, nc)
-
-        num_islands = 0
         for row in range(ROWS):
             for col in range(COLS):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    dfs_make_island(row, col)
-                    num_islands += 1
+                if grid[row][col] == "1":
+                    self.parents[(row, col)] = (row, col)
+                    self.sizes[(row, col)] = 1
+                    self.num_islands += 1
 
-        return num_islands
+    def find(self, x):
+
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+
+        return self.parents[x]
+
+    def union(self, x, y):
+
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+
+        if parent_x == parent_y:
+            return
+
+        if self.sizes[parent_x] > self.sizes[parent_y]:
+            self.parents[parent_y] = parent_x
+            self.sizes[parent_x] += self.sizes[parent_y]
+        else:
+            self.parents[parent_x] = parent_y
+            self.sizes[parent_y] = self.sizes[parent_x]
+
+        self.num_islands -= 1
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        ROWS, COLS = len(grid), len(grid[0])
+        dsu = DSU(grid)
+        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "1":
+                    for dx, dy in directions:
+                        nr, nc = row + dx, col + dy
+                        if not (0 <= nr < ROWS and 0 <= nc < COLS):
+                            continue
+                        if grid[nr][nc] == "1":
+                            dsu.union((row, col), (nr, nc))
+
+        return dsu.num_islands
 
