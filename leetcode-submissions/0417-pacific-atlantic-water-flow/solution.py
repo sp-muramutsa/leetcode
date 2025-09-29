e88a3029eadeff699a05b09406f10a3a63b1b999
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-29 19:01:24 +0000

    [LeetCode Sync] Sandrin - Pacific Atlantic Water Flow - Runtime - 67 ms (12.21%), Memory - 19.5 MB (22.71%)

--- leetcode-submissions/0417-pacific-atlantic-water-flow/solution.py
+++ leetcode-submissions/0417-pacific-atlantic-water-flow/solution.py
@@ -1,55 +1,68 @@
-from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # Get the cells tha are adjacent to the Pacific
-        adj_pacific_cells = []
-        for j in range(len(heights[0])):
-            adj_pacific_cells.append((0, j))
 
-        for i in range(len(heights)):
-            adj_pacific_cells.append((i, 0))
-        # Get the cells that area adjacent to the atlantic
-        adj_atlantic_cells = []
-        for j in range(len(heights[0])):
-            adj_atlantic_cells.append((len(heights)-1, j))
+        ROWS, COLS = len(heights), len(heights[0])
+        result = []
 
-        for i in range(len(heights)):
-            adj_atlantic_cells.append((i, len(heights[0])-1))
+        q = deque()
+        for i in range(COLS):
+            q.append((0, i))
 
-        def get_neighbors(cell, grid):
-            new_r = [1, 0, -1, 0]
-            new_c = [0, 1, 0, -1]
-            moves = []
+        for j in range(ROWS):
+            q.append((j, 0))
 
-            for i in range(4):
-                r = new_r[i] + cell[0]
-                c = new_c[i] + cell[1]
+        pacific = set(q)
+        visited = set()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] >= grid[cell[0]][cell[1]]:
-                    moves.append((r, c))
+        while q:
 
-            return moves
-        # Us bfs to get the cells where cells have a greater height
-        def bfs(grid, adj):
-            visited = set()
-            queue = deque()
-            for i in adj:
-                visited.add(i)
-                queue.append(i)
-            while queue:
-                n = queue.popleft()
-                for cell in get_neighbors(n, grid):
-                    if cell in visited:
-                        continue
+            row, col = q.popleft()
+            visited.add((row, col))
 
-                    visited.add(cell)
-                    queue.append(cell)
-            return visited
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
 
-        # Add those cells to Pacific list
-        res_pacific = bfs(heights, adj_pacific_cells)
-        # Do the same for those in Atlantic list
-        res_atlantic = bfs(heights, adj_atlantic_cells)
+                if not (0 <= nr < ROWS and 0 <= nc < COLS):
+                    continue
 
-        # Return the intersection
-        return list(res_pacific & res_atlantic)
+                if (nr, nc) in visited:
+                    continue
+
+                if heights[row][col] <= heights[nr][nc]:
+                    pacific.add((nr, nc))
+                    q.append((nr, nc))
+
+        print(pacific)
+        q = deque()
+        for k in range(COLS):
+            q.append((ROWS - 1, k))
+
+        for l in range(ROWS):
+            q.append((l, COLS - 1))
+
+        visited = set()
+        atlantic = set(q)
+
+        while q:
+
+            row, col = q.popleft()
+            visited.add((row, col))
+
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+
+                if not (0 <= nr < ROWS and 0 <= nc < COLS):
+                    continue
+
+                if (nr, nc) in visited:
+                    continue
+
+                if heights[row][col] <= heights[nr][nc]:
+                    atlantic.add((nr, nc))
+                    q.append((nr, nc))
+
+        print(atlantic)
+        result = pacific.intersection(atlantic)
+        return list(result)
+
