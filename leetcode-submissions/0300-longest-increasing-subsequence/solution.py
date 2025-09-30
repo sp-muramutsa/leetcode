1efb743d1574c4806b98ee20529d35092309bf01
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-30 14:05:32 +0000

    [LeetCode Sync] Sandrin - Longest Increasing Subsequence - Runtime - 1781 ms (36.08%), Memory - 17.9 MB (80.78%)

--- leetcode-submissions/0300-longest-increasing-subsequence/solution.py
+++ leetcode-submissions/0300-longest-increasing-subsequence/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-
-        n = len(nums)
-        dp = [1] * n
-
-        for start in range(n):
-            for end in range(start + 1, n):
-                if nums[start] < nums[end]:
-                    dp[end] = max(dp[end], dp[start] + 1)
-
+        # Create a dp of max sequence
+        dp = [1] * len(nums)
+        # Starting from the second last element
+        for i in range(len(nums)-2, -1, -1):
+            # Ierate from current index and update dp with max lenght if number is greater than current index value
+            for j in range(i+1, len(nums)):
+                if nums[j] > nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        # Return the max dp value
         return max(dp)
-
