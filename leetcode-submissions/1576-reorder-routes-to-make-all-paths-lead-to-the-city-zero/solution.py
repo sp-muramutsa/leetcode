5ff0d9195ebbf330f5ccc126c8fd3f6f1a43999f
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-09-30 01:28:41 +0000

    [LeetCode Sync] Sandrin - Reorder Routes to Make All Paths Lead to the City Zero - Runtime - 318 ms (5.97%), Memory - 54 MB (11.36%)

--- leetcode-submissions/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/solution.py
+++ leetcode-submissions/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/solution.py
@@ -1,30 +1,34 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
 
-        adj = [[] for _ in range(n)]
-        artificial = [[] for _ in range(n)]
+        reorder = 0
 
-        for src, dest in connections:
-            adj[dest].append(src)
-            artificial[dest].append(src)
-            artificial[src].append(dest)
+        adj = defaultdict(list)
+        alt = defaultdict(list)
+
+        for u, v in connections:
+            adj[u].append(v)
+            alt[u].append(v)
+            alt[v].append(u)
 
         visited = set()
-        visited.add(0)
-        reorder = 0
-        q = deque([0])
+        print(alt)
+        print(adj)
 
-        while q:
+        def dfs(node):
 
-            curr = q.popleft()
+            visited.add(node)
 
-            for child in artificial[curr]:
-                if child not in visited:
-                    # There is an edge from child to parent
-                    if curr in adj[child]:
-                        reorder += 1
-                    visited.add(child)
-                    q.append(child)
+            for neighbor in alt[node]:
+                if neighbor in visited:
+                    continue
 
+                if neighbor in adj[node]:
+                    nonlocal reorder
+                    reorder += 1
+
+                dfs(neighbor)
+
+        dfs(0)
         return reorder
 
