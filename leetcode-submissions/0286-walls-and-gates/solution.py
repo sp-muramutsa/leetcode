580fdbf886c6c3a50a81aaf144d49d2dbb7616a0
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-01 13:35:25 +0000

    [LeetCode Sync] Sandrin - Walls and Gates - Runtime - 150 ms (13.01%), Memory - 20.6 MB (95.90%)

--- leetcode-submissions/0286-walls-and-gates/solution.py
+++ leetcode-submissions/0286-walls-and-gates/solution.py
@@ -1,45 +1,40 @@
-from collections import deque
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        # Keep a queue with the gates
-        # While queue
-        # Get the neighbors 
-        # Increament the current level
-        # Add them to the visited data structure
-        queue = deque()
-        visited = set()
 
-        def get_neighbors(grid, position):
-            new_row = [1, 0, -1, 0]
-            new_col = [0, 1, 0, -1]
-            moves = []
-            for i in range(4):
-                r = position[0] + new_row[i]
-                c = position[1] + new_col[i]
+        # Multi-source BFS
+        # Start from the gates
 
-                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] != -1 and grid[r][c] != 0:
-                    moves.append((r, c))
+        ROWS, COLS = len(rooms), len(rooms[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-            return moves
+        def bfs(source):
+    
+            q = deque([(source, 0)])
+            visited = set()
 
-        for i in range(len(rooms)):
-            for j in range(len(rooms[0])):
-                if rooms[i][j] == 0:
-                    queue.append((i, j))
-                    visited.add((i, j))
-         
-        distance = 0
-        while queue:
-            l = len(queue)
-            for _ in range(l):
-                n = queue.popleft()
-                rooms[n[0]][n[1]] = distance
-                for p in get_neighbors(rooms, n):
-                    if p in visited:
-                        continue
-                    visited.add((p[0], p[1]))
-                    queue.append((p[0], p[1]))
-            distance += 1
+            while q:
+
+                curr, dist = q.popleft()
+                r, c = curr
+           
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+
+                    if (0 <= nr < ROWS and 0 <= nc < COLS) \
+                    and rooms[nr][nc] != -1 and rooms[nr][nc] != 0 and (nr, nc) not in visited:
+                        rooms[nr][nc] = min(dist + 1, rooms[nr][nc])
+                        q.append(((nr, nc), dist + 1))
+                        visited.add((nr, nc))
+
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if rooms[row][col] == 0:
+                    rooms[row][col] == -2 # Mark visited
+                    bfs((row, col)) # Explore neighbors
+
+        
+        
