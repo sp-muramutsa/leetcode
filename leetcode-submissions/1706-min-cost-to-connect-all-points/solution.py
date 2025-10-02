858a5c55cc00049746b0b8d998c961756c7016c3
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-02 22:54:50 +0000

    [LeetCode Sync] Sandrin - Min Cost to Connect All Points - Runtime - 2753 ms (21.63%), Memory - 278.6 MB (5.01%)

--- leetcode-submissions/1706-min-cost-to-connect-all-points/solution.py
+++ leetcode-submissions/1706-min-cost-to-connect-all-points/solution.py
@@ -1,68 +1,54 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        # Prim's
 
-        # Approach 1: Prim's Algorithm (Eager)
-        # O((N ** 2) )
+        # Greedily pick cheap edges
+        # Use a min heap to get them by min cost
+
+        # MST
+            # Undirected graph with min edge cost
 
         n = len(points)
-        costs = defaultdict(int)
-        edges = defaultdict(list)
+        edges_count = n - 1
+        graph = defaultdict(list)
+        costs = {}
 
-        for i in range(n):
-            for j in range(i + 1, n):
-
-                p1 = tuple(points[i])
-                p2 = tuple(points[j])
-
-                x_val = abs(points[i][0] - points[j][0])
-                y_val = abs(points[i][1] - points[j][1])
-
-                manhattan = x_val + y_val
-                edges[p1].append(p2)
-                edges[p2].append(p1)
-                costs[(p1, p2)] = manhattan
-                costs[(p2, p1)] = manhattan
-
-        # Choose a source
-        source = tuple(points[0])
-        n = len(costs)
-        m = n - 1
-
-        mst_cost, edges_count = 0, 0
-        visited = set()
-        min_cost = {tuple(node): float("inf") for node in points}
-        parents = {tuple(node): None for node in points}
-
+        # Make the graph
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if i == j:
+                    continue
+                costs[(i, j)] = abs(points[i-1][0] - points[j-1][0]) + abs(points[i-1][1] - points[j-1][1])
+                graph[i].append(j)
+                graph[j].append(i)
+        
+        
+        source = 1
+        chosen = 0
+        visited = set([1])
+        min_cost = 0
         heap = []
-        heapq.heappush(heap, (0, source))
 
-        # Prim's Algorithm(lazy)
-        while heap and edges_count < m:
+        for neighbor in graph[source]:
+            heapq.heappush(heap, (costs[source, neighbor], neighbor))
+        
+       
+        while heap and chosen < edges_count:
+          
+            cost, curr = heapq.heappop(heap)
 
-            cost, node = heapq.heappop(heap)
-
-            # Skip outdated entry
-            if cost > min_cost[node]:
+            if curr in visited:
                 continue
 
-            # Visited node
-            if node in visited:
-                continue
+            visited.add(curr)
+            min_cost += cost
+            chosen += 1
 
-            visited.add(node)
-
-            if parents[node] is not None:
-                mst_cost += cost
-                edges_count += 1
-
-            for dest in edges[node]:
-                if dest not in visited:
-                    edge_cost = costs[(node, dest)]
-
-                    if edge_cost < min_cost[tuple(dest)]:
-                        min_cost[dest] = edge_cost
-                        parents[dest] = node
-                        heapq.heappush(heap, (edge_cost, dest))
-
-        return mst_cost
+            for neighbor in graph[curr]:
+                if neighbor not in visited:
+                    heapq.heappush(heap, (costs[curr, neighbor], neighbor))
+        
+        return min_cost
+             
 
