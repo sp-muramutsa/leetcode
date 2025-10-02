0495621c3ee547a8669903dd77de3a04c7e4885b
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-02 23:16:00 +0000

    [LeetCode Sync] Sandrin - Swim in Rising Water - Runtime - 24 ms (69.00%), Memory - 18.7 MB (25.88%)

--- leetcode-submissions/0794-swim-in-rising-water/solution.py
+++ leetcode-submissions/0794-swim-in-rising-water/solution.py
@@ -1,32 +1,33 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-
-        time = 0
-        heap = [(grid[0][0], 0, 0)]
-        visited = set([(0, 0)])
+        
         ROWS, COLS = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
+        heap = [(grid[0][0], 0, 0)]
+        visited = set([(0, 0)])
+
+
+        # BFS 
 
         while heap:
 
-            height, row, col = heapq.heappop(heap)
-            
-            # Goal 
-            if row == ROWS - 1 and col == COLS - 1:
-                return height
+            t, row, col = heapq.heappop(heap)
 
-            
+            if row == ROWS - 1 and col == COLS - 1:
+                return t
+
             for dr, dc in directions:
                 nr, nc = row + dr, col + dc
 
-                if (nr, nc) in visited:
+                if not (0 <= nr < ROWS and 0 <= nc < COLS) or (nr, nc) in visited:
                     continue
 
-                if not (0 <= nr < ROWS and 0 <= nc < COLS):
-                    continue
-                
                 visited.add((nr, nc))
-                heapq.heappush(heap, (max(grid[nr][nc], height), nr, nc))
-
+                new_t = max(t, grid[nr][nc])
+                heapq.heappush(heap, (new_t, nr, nc))
         
+
+
+
+                
+
