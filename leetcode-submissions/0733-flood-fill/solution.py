b01d7f3d7f057860181f627cd769eef3d6491d87
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-02 15:34:02 +0000

    [LeetCode Sync] Sandrin - Flood Fill - Runtime - 1 ms (25.18%), Memory - 17.9 MB (68.28%)

--- leetcode-submissions/0733-flood-fill/solution.py
+++ leetcode-submissions/0733-flood-fill/solution.py
@@ -1,27 +1,29 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-
+        
         ROWS, COLS = len(image), len(image[0])
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        init_color = image[sr][sc]
-        stack = deque([(sr, sc)])
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
-        # Iterative DFS
-        while stack:
+        q = deque([(sr, sc)])
+        original_color = image[sr][sc]
+        image[sr][sc] = color
+        visited = set()
 
-            row, col = stack.pop()
+        while q:
 
-            image[row][col] = color
+            row, col = q.popleft()
+            visited.add((row, col))
 
             for dr, dc in directions:
                 nr, nc = row + dr, col + dc
 
-                if not (0 <= nr < ROWS and 0 <= nc < COLS):
+                if (nr, nc) in visited:
                     continue
 
-                if image[nr][nc] == init_color and image[nr][nc] != color:
-                    stack.append((nr, nc))
-
+                if 0 <= nr < ROWS and 0 <= nc < COLS and image[nr][nc] == original_color:
+                    image[nr][nc] = color
+                    q.append((nr, nc))
+        
         return image
 
-       
+
