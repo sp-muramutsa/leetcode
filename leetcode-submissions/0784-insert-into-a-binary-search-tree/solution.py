7c914e4ef6641bdd6d3d6b7ecced5a0cfa74cbc1
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-04 20:04:08 +0000

    [LeetCode Sync] Sandrin - Insert into a Binary Search Tree - Runtime - 3 ms (34.06%), Memory - 19.5 MB (21.73%)

--- leetcode-submissions/0784-insert-into-a-binary-search-tree/solution.py
+++ leetcode-submissions/0784-insert-into-a-binary-search-tree/solution.py
@@ -6,17 +6,15 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        
         if not root:
             return TreeNode(val)
-
-        if root.val > val:
-            root.left = self.insertIntoBST(root.left, val)
         
-        else:
+        if root.val < val:
             root.right = self.insertIntoBST(root.right, val)
-        
+            
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left,val)
+            
         return root
-
-      
-     
+            
+        
