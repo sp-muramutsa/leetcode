46489496851f5517c94752eafefad0a04a1f30c7
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-05 21:02:55 +0000

    [LeetCode Sync] Sandrin - Number of Islands - Runtime - 314 ms (9.92%), Memory - 25.6 MB (15.57%)

--- leetcode-submissions/0200-number-of-islands/solution.py
+++ leetcode-submissions/0200-number-of-islands/solution.py
@@ -1,58 +1,42 @@
-class DSU:
-    def __init__(self, grid):
-        ROWS, COLS = len(grid), len(grid[0])
-        self.parents = {}
-        self.sizes = {}
-        self.num_islands = 0
-
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == "1":
-                    self.parents[(row, col)] = (row, col)
-                    self.sizes[(row, col)] = 1
-                    self.num_islands += 1
-
-    def find(self, x):
-
-        if self.parents[x] != x:
-            self.parents[x] = self.find(self.parents[x])
-
-        return self.parents[x]
-
-    def union(self, x, y):
-
-        parent_x = self.find(x)
-        parent_y = self.find(y)
-
-        if parent_x == parent_y:
-            return
-
-        if self.sizes[parent_x] > self.sizes[parent_y]:
-            self.parents[parent_y] = parent_x
-            self.sizes[parent_x] += self.sizes[parent_y]
-        else:
-            self.parents[parent_x] = parent_y
-            self.sizes[parent_y] = self.sizes[parent_x]
-
-        self.num_islands -= 1
-
-
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # base case
+        if len(grid) == 0:
+            return 0
 
-        ROWS, COLS = len(grid), len(grid[0])
-        dsu = DSU(grid)
-        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        rows, columns = len(grid), len(grid[0])
+        seen = set()
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
 
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == "1":
-                    for dx, dy in directions:
-                        nr, nc = row + dx, col + dy
-                        if not (0 <= nr < ROWS and 0 <= nc < COLS):
-                            continue
-                        if grid[nr][nc] == "1":
-                            dsu.union((row, col), (nr, nc))
+        answer = 0
 
-        return dsu.num_islands
+        def bfs(row, column):
+            seen.add((row,column))
+            queue = deque()
+            queue.append((row,column))
+            
+            while queue:
+                r, c = queue.popleft()
 
+                for x, y in directions:
+                    current_row = r + x
+                    current_column = c + y
+
+                    if ( (current_row in range(rows)) and
+                         (current_column in range(columns)) and
+                         ((current_row,current_column) not in seen) and
+                         ( grid[current_row][current_column] == "1")
+                        ):
+                        seen.add((current_row,current_column))
+                        queue.append((current_row,current_column))
+
+
+        for row in range(rows):
+            for column in range(columns):
+
+                if (((row,column) not in seen) and (grid[row][column] == "1")):
+                    bfs(row,column)
+                    answer += 1
+
+        return answer
