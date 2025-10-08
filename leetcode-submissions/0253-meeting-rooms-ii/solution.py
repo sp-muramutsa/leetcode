83f36172e799f42bcb19944a6d9a091df028ea99
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-08 22:36:55 +0000

    [LeetCode Sync] Sandrin - Meeting Rooms II - Runtime - 3 ms (87.68%), Memory - 19.9 MB (76.94%)

--- leetcode-submissions/0253-meeting-rooms-ii/solution.py
+++ leetcode-submissions/0253-meeting-rooms-ii/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        """
+        - sort by start time
+        - last
+        """
 
         intervals.sort()
         heap = [intervals[0][1]]
 
         for start, end in intervals[1:]:
+            # If the start is later than the end of the top meeting, free up a room
             if start >= heap[0]:
                 heapq.heappop(heap)
             heapq.heappush(heap, end)
