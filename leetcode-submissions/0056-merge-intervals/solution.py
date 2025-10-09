d0afa74eca39bde7e0771f38cfdc6de7d5819665
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-09 17:00:14 +0000

    [LeetCode Sync] Sandrin - Merge Intervals - Runtime - 8 ms (48.79%), Memory - 21.6 MB (32.44%)

--- leetcode-submissions/0056-merge-intervals/solution.py
+++ leetcode-submissions/0056-merge-intervals/solution.py
@@ -2,11 +2,10 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
         intervals.sort()
-
         result = [intervals[0]]
-        n = len(intervals)
 
         for start, end in intervals[1:]:
+
             if start <= result[-1][1]:
                 result[-1][1] = max(end, result[-1][1])
             else:
