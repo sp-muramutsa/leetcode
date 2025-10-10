8cb1775c28baf19e2f0c41da61624b70d0455a34
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-10 16:45:28 +0000

    [LeetCode Sync] Sandrin - Partition Equal Subset Sum - Runtime - 720 ms (49.42%), Memory - 18 MB (74.29%)

--- leetcode-submissions/0416-partition-equal-subset-sum/solution.py
+++ leetcode-submissions/0416-partition-equal-subset-sum/solution.py
@@ -1,20 +1,21 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
-        # Optimized Knapsack
+        # Constant space knapsack solution
         n = len(nums)
-
         total = sum(nums)
+
         if total % 2 == 1:
             return False
 
         half = total // 2
+
         dp = [False] * (half + 1)
         dp[0] = True
 
         for i in range(n):
             for curr_sum in range(half, nums[i] - 1, -1):
-                dp[curr_sum] = dp[curr_sum] or dp[curr_sum - nums[i]]
+                dp[curr_sum] = dp[curr_sum - nums[i]] or dp[curr_sum]
 
-        return dp[-1]
+        return dp[half]
 
