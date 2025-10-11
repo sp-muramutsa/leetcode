963587fda8234338a8167d0b1ad19461269206ac
Sandrin Muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2025-10-11 15:26:47 +0000

    [LeetCode Sync] Sandrin - Implement Trie (Prefix Tree) - Runtime - 87 ms (9.80%), Memory - 35.4 MB (6.41%)

--- leetcode-submissions/0208-implement-trie-prefix-tree/solution.py
+++ leetcode-submissions/0208-implement-trie-prefix-tree/solution.py
@@ -1,23 +1,22 @@
 class TrieNode:
-    def __init__(self) -> None:
+    def __init__(self) -> "TrieNode":
         self.children = [None] * 26
         self.is_end = False
 
-    def set_child(self, key: chr, node: "TrieNode") -> None:
-        self.children[ord(key) - ord("a")] = node
+    def contains(self, key: chr) -> bool:
+        return self.children[ord("a") - ord(key)] is not None
 
-    def get_child(self, key: chr) -> "TrieNode":
-        return self.children[ord(key) - ord("a")]
+    def put(self, key: chr, node: "TrieNode") -> None:
+        self.children[ord("a") - ord(key)] = node
 
-    def contains_child(self, key: chr) -> bool:
-        return self.children[ord(key) - ord("a")] is not None
+    def get(self, key: chr) -> "TrieNode":
+        return self.children[ord("a") - ord(key)]
+
+    def set_end(self) -> None:
+        self.is_end = True
 
     def check_end(self) -> bool:
         return self.is_end
-
-    def set_end(self) -> bool:
-        self.is_end = True
-
 
 class Trie:
 
@@ -25,31 +24,38 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
+
         curr = self.root
+
         for char in word:
-            if not curr.contains_child(char):
+            if not curr.contains(char):
                 node = TrieNode()
-                curr.set_child(char, node)
-            curr = curr.get_child(char)
+                curr.put(char, node)
+            curr = curr.get(char)
+        
         curr.set_end()
+        
 
     def search(self, word: str) -> bool:
+        
+        curr = self.root 
 
-        curr = self.root
         for char in word:
-            if not curr.contains_child(char):
+            if not curr.contains(char):
                 return False
-            curr = curr.get_child(char)
-
+            curr = curr.get(char)
+        
         return curr.check_end()
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.root
-        for char in prefix:
-            if not curr.contains_child(char):
-                return False
-            curr = curr.get_child(char)
 
+        curr = self.root
+
+        for char in prefix:
+            if not curr.contains(char):
+                return False
+            curr = curr.get(char)
+        
         return True
 
 
@@ -58,4 +64,3 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
-
